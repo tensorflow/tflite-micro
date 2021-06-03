725775df1d10713e91d356cdf470a18d50e2d40a
bhanu prakash bandaru venkata <bhanup@cadence.com>
2021-06-03 12:17:50 +0530

    Integrated HiFi 5 NN Library kernels for below operators   svdf(int8),   quantize(int8),

--- tensorflow/lite/micro/kernels/xtensa/xtensa.h
+++ tensorflow/lite/micro/kernels/xtensa/xtensa.h
@@ -1,24 +1,3 @@
-/******************************************************************************
- * Copyright (C) 2021 Cadence Design Systems, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to use this Software with Cadence processor cores only and
- * not with any other processors and platforms, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- ******************************************************************************/
-
 /* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -43,11 +22,6 @@ limitations under the License.
 #include "include/nnlib/xa_nnlib_api.h"
 #include "include/nnlib/xa_nnlib_standards.h"
 
-#define CHECK_ERR_HIFI_NNLIB_KER(ret, err_msg) \
-  if (ret != 0) {                              \
-    TF_LITE_KERNEL_LOG(context, err_msg);      \
-    return kTfLiteError;                       \
-  }
 
 #define ALIGNED_SIZE(x, bytes) (((x) + (bytes - 1)) & (~(bytes - 1)))
 #define ALIGN_PTR(x, bytes) ((((unsigned)(x)) + (bytes - 1)) & (~(bytes - 1)))
