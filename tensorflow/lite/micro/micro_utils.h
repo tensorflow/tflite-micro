9ecc25b2319ac58add8be3ee622928c565749c5b
Nat Jeffries <njeff@google.com>
2021-06-22 17:31:46 -0700

    Add comment explaining int64 in micro_utils.h

--- tensorflow/lite/micro/micro_utils.h
+++ tensorflow/lite/micro/micro_utils.h
@@ -44,6 +44,8 @@ T FloatToQuantizedType(const float value, const float scale, int zero_point) {
 
 template <typename T>
 T FloatToSymmetricQuantizedType(const float value, const float scale) {
+  // 64-bit values are required since 8x16 conv accumulates to int64, meaning
+  // an int64 bias is required.
   std::int64_t result = round(value / scale);
   result = std::max(
       static_cast<std::int64_t>(std::numeric_limits<T>::min() + 1), result);
