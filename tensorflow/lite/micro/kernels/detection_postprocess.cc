177e23548d0a8f59a3189bf5ac184def64f3f8bb
Advait Jain <advaitjain@google.com>
2021-07-09 12:52:07 -0700

    Fix the build.

--- tensorflow/lite/micro/kernels/detection_postprocess.cc
+++ tensorflow/lite/micro/kernels/detection_postprocess.cc
@@ -15,6 +15,8 @@ limitations under the License.
 
 #include <numeric>
 
+#include "flatbuffers/flatbuffers.h"
+#include "flatbuffers/flexbuffers.h"
 #include "tensorflow/lite/c/builtin_op_data.h"
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/kernels/internal/common.h"
