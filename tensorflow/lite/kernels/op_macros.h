da7b777020b955c429056287b55cb8bf2de85e7e
Advait Jain <advaitjain@google.com>
2021-07-13 12:22:41 -0700

    Have a TFLM-specific version of lite/kernels/op_macros.h
     * After this change, we can remove the TFLM-specific code from upstream
       Tensorflow.
     * Both the TfLite and TFLM implementations of op_macros.h will be
       simplified.
    
    BUG=http://b/187728891
    NO_CHECK_TFLITE_FILE=lite/kernels/kernel_util.cc will be modified in upstream as well

--- tensorflow/lite/kernels/op_macros.h
+++ tensorflow/lite/kernels/op_macros.h
@@ -15,69 +15,23 @@ limitations under the License.
 #ifndef TENSORFLOW_LITE_KERNELS_OP_MACROS_H_
 #define TENSORFLOW_LITE_KERNELS_OP_MACROS_H_
 
-// If we're on a platform without standard IO functions, fall back to a
-// non-portable function.
-#ifdef TF_LITE_MCU_DEBUG_LOG
-
 #include "tensorflow/lite/micro/debug_log.h"
 
-#define DEBUG_LOG(x) \
-  do {               \
-    DebugLog(x);     \
-  } while (0)
-
-inline void InfiniteLoop() {
-  DEBUG_LOG("HALTED\n");
-  while (1) {
-  }
-}
-
-#define TFLITE_ABORT InfiniteLoop();
-
-#else  // TF_LITE_MCU_DEBUG_LOG
-
-#include <cstdio>
+#if !defined(TF_LITE_MCU_DEBUG_LOG)
 #include <cstdlib>
-
-#define DEBUG_LOG(x)            \
-  do {                          \
-    fprintf(stderr, "%s", (x)); \
-  } while (0)
-
-// Report Error for unsupported type by op 'op_name' and returns kTfLiteError.
-#define TF_LITE_UNSUPPORTED_TYPE(context, type, op_name)                    \
-  do {                                                                      \
-    TF_LITE_KERNEL_LOG((context), "%s:%d Type %s is unsupported by op %s.", \
-                       __FILE__, __LINE__, TfLiteTypeGetName(type),         \
-                       (op_name));                                          \
-    return kTfLiteError;                                                    \
-  } while (0)
-
 #define TFLITE_ABORT abort()
-
-#endif  // TF_LITE_MCU_DEBUG_LOG
+#else
+#define TFLITE_ABORT          \
+  do {                        \
+    DebugLog("HALTED\n");     \
+    while (1);                \
+  } while (0)
+#endif
 
 #if defined(NDEBUG) || defined(ARDUINO)
 #define TFLITE_ASSERT_FALSE (static_cast<void>(0))
 #else
 #define TFLITE_ASSERT_FALSE TFLITE_ABORT
 #endif
-
-#define TF_LITE_FATAL(msg)  \
-  do {                      \
-    DEBUG_LOG(msg);         \
-    DEBUG_LOG("\nFATAL\n"); \
-    TFLITE_ABORT;           \
-  } while (0)
-
-#define TF_LITE_ASSERT(x)        \
-  do {                           \
-    if (!(x)) TF_LITE_FATAL(#x); \
-  } while (0)
-
-#define TF_LITE_ASSERT_EQ(x, y)                            \
-  do {                                                     \
-    if ((x) != (y)) TF_LITE_FATAL(#x " didn't equal " #y); \
-  } while (0)
 
 #endif  // TENSORFLOW_LITE_KERNELS_OP_MACROS_H_
