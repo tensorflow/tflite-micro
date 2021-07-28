c4ecd9230d9a6c09e740bc799e8971263321a2e7
Nat Jeffries <natmjeffries@gmail.com>
2021-07-28 12:36:47 -0700

    Change python script to handle multiple sources.
    Python script now handles generating multiple cc sources at once, moving complexity out of the makefile.

--- tensorflow/lite/micro/tools/make/generate_cc_arrays.py
+++ tensorflow/lite/micro/tools/make/generate_cc_arrays.py
@@ -25,15 +25,13 @@ import io
 import os
 
 
-def write_array_to_file(out_fname, array_name, out_string, size):
+def generate_cc_file(out_fname, array_name, out_string, size):
     ''' Write the out string containing an array of values to a variable in a cc
         file, and create a header file defining the same array. '''
     os.makedirs(os.path.dirname(out_fname), exist_ok=True)
     out_cc_file = open(out_fname + '.cc', 'w')
-    out_hdr_file = open(out_fname + '.h', 'w')
-    out_hdr_file.write('extern const unsigned char ' + array_name + '[];\n')
-    out_hdr_file.write('extern const unsigned int ' + array_name + '_size' +
-                       ';')
+    print(
+        out_cc_file.name)  # Log cc file name for Make to include in the build.
     out_cc_file.write('#include "' + out_fname + '.h"\n\n')
     out_cc_file.write('const unsigned char ' + array_name + '[] = {')
     out_cc_file.write(out_string)
@@ -41,27 +39,40 @@ def write_array_to_file(out_fname, array_name, out_string, size):
     out_cc_file.write('const unsigned int ' + array_name + '_size = ' +
                       str(size) + ';')
     out_cc_file.close()
+
+
+def generate_hdr_file(out_fname, array_name):
+    ''' Write the out string containing an array of values to a variable in a cc
+        file, and create a header file defining the same array. '''
+    os.makedirs(os.path.dirname(out_fname), exist_ok=True)
+    out_hdr_file = open(out_fname + '.h', 'w')
+    out_hdr_file.write('extern const unsigned char ' + array_name + '[];\n')
+    out_hdr_file.write('extern const unsigned int ' + array_name + '_size' +
+                       ';')
     out_hdr_file.close()
 
 
-def tflite_to_cc(tflite_fname, out_fname):
+def tflite_to_cc(tflite_fname, out_fname, generate_cc, generate_hdr):
     ''' Serialize a tflite file to a header and cc file for use with TFLM. '''
     with open(tflite_fname, 'rb') as tflite_file:
-        out_string = ""
+        out_string = ''
         byte = tflite_file.read(1)
         size = 0
-        while (byte != b""):
-            out_string += "0x" + byte.hex() + ","
+        while (byte != b''):
+            out_string += '0x' + byte.hex() + ','
             byte = tflite_file.read(1)
             size += 1
         # Array name is 'g_<model name without path or extension>_model_data'
         array_name = 'g_' + tflite_fname.split('.')[0].split(
             '/')[-1] + '_model_data'
         out_fname = out_fname + '_model_data'
-        write_array_to_file(out_fname, array_name, out_string, size)
+        if generate_hdr:
+            generate_hdr_file(out_fname, array_name)
+        if generate_cc:
+            generate_cc_file(out_fname, array_name, out_string, size)
 
 
-def bmp_to_cc(bmp_fname, out_fname):
+def bmp_to_cc(bmp_fname, out_fname, generate_cc, generate_hdr):
     ''' Serialize an image to a header and cc file for use with TFLM.'''
     img = Image.open(bmp_fname, mode='r')
     image_bytes = img.tobytes()
@@ -71,29 +82,54 @@ def bmp_to_cc(bmp_fname, out_fname):
     # Array name is 'g_<file name without path or extension>_image_data'
     array_name = 'g_' + bmp_fname.split('.')[0].split('/')[-1] + '_image_data'
     out_fname = out_fname + '_image_data'
-    write_array_to_file(out_fname, array_name, out_string, len(image_bytes))
+    if generate_hdr:
+        generate_hdr_file(out_fname, array_name)
+    if generate_cc:
+        generate_cc_file(out_fname, array_name, out_string, len(image_bytes))
 
 
-def main(unparsed_args):
-    """Control the rewriting of source files."""
-    input_file = unparsed_args[0]
-    output_file = unparsed_args[1].split(".")[0] # Remove file extension
-    print(output_file[0:output_file.rfind('/')]) # Print output directory
-    if input_file.endswith(".tflite"):
-        tflite_to_cc(input_file, output_file)
-    elif input_file.endswith(".bmp"):
-        bmp_to_cc(input_file, output_file)
-    else:
-        raise ValueError("input file must be .tflite, .bmp")
+def generate_cc_and_hdrs(generator_output,
+                         input_files,
+                         cc_only=False,
+                         hdr_only=False):
+    generate_cc = True
+    generate_hdr = True
+    if hdr_only:
+        generate_cc = False
+    if cc_only:
+        generate_hdr = False
+
+    for input_file in input_files:
+        if os.path.isdir(generator_output):
+          output_dir = os.path.join(generator_output,
+                                  input_file.split('.')[0])
+        else:
+          output_dir = generator_output
+        if input_file.endswith('.tflite'):
+            tflite_to_cc(input_file, output_dir, generate_cc, generate_hdr)
+        elif input_file.endswith('.bmp'):
+            bmp_to_cc(input_file, output_dir, generate_cc, generate_hdr)
+        else:
+            raise ValueError('input file must be .tflite, .bmp')
 
 
-def parse_args():
-    """Converts the raw arguments into accessible flags."""
+def main():
+    '''Create cc sources with c arrays with data from each .tflite or .bmp.'''
     parser = argparse.ArgumentParser()
-    _, unparsed_args = parser.parse_known_args()
-
-    main(unparsed_args)
+    parser.add_argument(
+        'generator_output_dir',
+        help=
+        'location where generated files are saved. Generated files will be saved to <output_dir>/<input file without file extension>.cc/.h'
+    )
+    parser.add_argument('inputs',
+                        nargs='+',
+                        help='input bmp or tflite files to convert')
+    parser.add_argument('--hdr_only', help='generate only a single header file')
+    parser.add_argument('--cc_only', help='generate only a single cc file')
+    args = parser.parse_args()
+    generate_cc_and_hdrs(args.generator_output_dir, args.inputs, args.cc_only,
+                         args.hdr_only)
 
 
 if __name__ == '__main__':
-    parse_args()
+    main()
