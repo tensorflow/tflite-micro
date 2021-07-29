d324a07ef3eba261104a3331feb5e4b32e3e7457
Nat Jeffries <natmjeffries@gmail.com>
2021-07-28 17:54:02 -0700

    Format and add copyright headers.

--- tensorflow/lite/micro/tools/make/generate_cc_arrays.py
+++ tensorflow/lite/micro/tools/make/generate_cc_arrays.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Converts .csv, .tflite, .wav and .bmp files to cc arrays"""
+"""Library for converting .tflite and .bmp files to cc arrays"""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -28,49 +28,52 @@ def generate_file(out_fname, array_name, out_string, size):
         file, and create a header file defining the same array. '''
     os.makedirs(os.path.dirname(out_fname), exist_ok=True)
     if out_fname.endswith('.cc'):
-      out_cc_file = open(out_fname, 'w')
-      # Log cc file name for Make to include in the build.
-      out_cc_file.write('#include "' + out_fname.replace('.cc', '.h') + '"\n\n')
-      out_cc_file.write('const unsigned char ' + array_name + '[] = {')
-      out_cc_file.write(out_string)
-      out_cc_file.write('};\n')
-      out_cc_file.write('const unsigned int ' + array_name + '_size = ' +
-                        str(size) + ';')
-      out_cc_file.close()
+        out_cc_file = open(out_fname, 'w')
+        # Log cc file name for Make to include in the build.
+        out_cc_file.write('#include "' + out_fname.replace('.cc', '.h') +
+                          '"\n\n')
+        out_cc_file.write('const unsigned char ' + array_name + '[] = {')
+        out_cc_file.write(out_string)
+        out_cc_file.write('};\n')
+        out_cc_file.write('const unsigned int ' + array_name + '_size = ' +
+                          str(size) + ';')
+        out_cc_file.close()
     elif out_fname.endswith('.h'):
-      out_hdr_file = open(out_fname, 'w')
-      out_hdr_file.write('extern const unsigned char ' + array_name + '[];\n')
-      out_hdr_file.write('extern const unsigned int ' + array_name + '_size' + ';')
-      out_hdr_file.close()
+        out_hdr_file = open(out_fname, 'w')
+        out_hdr_file.write('extern const unsigned char ' + array_name + '[];\n')
+        out_hdr_file.write('extern const unsigned int ' + array_name + '_size' +
+                           ';')
+        out_hdr_file.close()
     else:
-      raise ValueError('input file must be .tflite, .bmp')
+        raise ValueError('input file must be .tflite, .bmp')
 
 
 def generate_array(input_fname):
     ''' Return array size and string containing an array of data from the input file. '''
     if input_fname.endswith('.tflite'):
-      with open(input_fname, 'rb') as input_file:
-          out_string = ''
-          byte = input_file.read(1)
-          size = 0
-          while (byte != b''):
-              out_string += '0x' + byte.hex() + ','
-              byte = input_file.read(1)
-              size += 1
-          return [size, out_string]
+        with open(input_fname, 'rb') as input_file:
+            out_string = ''
+            byte = input_file.read(1)
+            size = 0
+            while (byte != b''):
+                out_string += '0x' + byte.hex() + ','
+                byte = input_file.read(1)
+                size += 1
+            return [size, out_string]
     elif input_fname.endswith('.bmp'):
-      img = Image.open(input_fname, mode='r')
-      image_bytes = img.tobytes()
-      out_string = ""
-      for byte in image_bytes:
-          out_string += hex(byte) + ","
-      return [len(image_bytes), out_string]
+        img = Image.open(input_fname, mode='r')
+        image_bytes = img.tobytes()
+        out_string = ""
+        for byte in image_bytes:
+            out_string += hex(byte) + ","
+        return [len(image_bytes), out_string]
     else:
-      raise ValueError('input file must be .tflite, .bmp')
+        raise ValueError('input file must be .tflite, .bmp')
+
 
 def array_name(input_fname):
-  base_array_name = 'g_' + input_fname.split('.')[0].split('/')[-1] 
-  if input_fname.endswith('.tflite'):
-    return base_array_name + '_model_data'
-  elif input_fname.endswith('.bmp'):
-    return base_array_name + '_image_data'
+    base_array_name = 'g_' + input_fname.split('.')[0].split('/')[-1]
+    if input_fname.endswith('.tflite'):
+        return base_array_name + '_model_data'
+    elif input_fname.endswith('.bmp'):
+        return base_array_name + '_image_data'
