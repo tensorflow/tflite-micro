751b36c54695485146b85f9fa7946f49d9d8d7ac
deqiangc <86809673+deqiangc@users.noreply.github.com>
2021-08-06 17:01:38 -0700

    Add a new test case for conv operator (#329) (#377)
    * Add a new test case for conv operator (#329)
    
    This new test case is based on issue #329 to increase
    coverage on optimized kernel's data precision for conv operator.
    In this test, input, output and filter are all 8 bits and filter
    tensor is of dimension 8x3x3x3 with different scales per output channel.
    
    TESTED= local test with x86 and HiFi4.
    
    * Move large test data variable into its own folder
    and use one flat top namespace instead of nested namespace.
    
    BUG=195779890

---
+++ tensorflow/lite/micro/kernels/testdata/conv_test_data.h
@@ -0,0 +1,28 @@
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_LITE_MICRO_KERNELS_CONV_TEST_DATA_H_
+#define TENSORFLOW_LITE_MICRO_KERNELS_CONV_TEST_DATA_H_
+
+#include "tensorflow/lite/c/common.h"
+
+namespace tflite {
+extern const int8_t kConvInput1x32x32x3[];
+extern const int8_t kConvFilter8x3x3x3[];
+extern const int32_t kConvBiasQuantized8[];
+extern const int8_t kConvGoldenOutput1x16x16x8[];
+}  // namespace tflite
+
+#endif  // TENSORFLOW_LITE_MICRO_KERNELS_CONV_TEST_DATA_H_
