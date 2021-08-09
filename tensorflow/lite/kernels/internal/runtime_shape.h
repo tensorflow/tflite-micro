bd20a675920e208a4f18f9abc4951069a595bda4
Nat Jeffries <natmjeffries@gmail.com>
2021-08-09 16:33:46 -0700

    properly update size in RuntimeShape.

--- tensorflow/lite/kernels/internal/runtime_shape.h
+++ tensorflow/lite/kernels/internal/runtime_shape.h
@@ -70,6 +70,7 @@ class RuntimeShape {
   const int32_t* DimsData() const { return dims_; }
 
   void ReplaceWith(int dimensions_count, const int32_t* dims_data) {
+    size_ = dimensions_count;
     int32_t* dst_dims = DimsData();
     std::memcpy(dst_dims, dims_data, dimensions_count * sizeof(int32_t));
   }
