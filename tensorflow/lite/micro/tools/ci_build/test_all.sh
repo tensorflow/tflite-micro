fdc88436a1928b2314caf5e1445fdf16ccd83d68
Advait Jain <advaitjain@users.noreply.github.com>
2021-08-11 13:35:56 -0700

    Delete (most of the) Arduino-specific code.
    Moving forward, TFLM support for Arduino is going to be maintained in a
    stand-alone repository at:
    https://github.com/tensorflow/tflite-micro-arduino-examples
    
    BUG=http://b/193823889

--- tensorflow/lite/micro/tools/ci_build/test_all.sh
+++ tensorflow/lite/micro/tools/ci_build/test_all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -109,9 +109,6 @@ tensorflow/lite/micro/tools/ci_build/test_sparkfun.sh
 
 echo "Running stm32f4 tests at `date`"
 tensorflow/lite/micro/tools/ci_build/test_stm32f4.sh PRESUBMIT
-
-echo "Running Arduino tests at `date`"
-tensorflow/lite/micro/tools/ci_build/test_arduino.sh
 
 echo "Running cortex_m_generic tests at `date`"
 tensorflow/lite/micro/tools/ci_build/test_cortex_m_generic.sh
