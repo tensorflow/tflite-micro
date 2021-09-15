dc8790fb86425ce1a46e5934c197f13ea76f7de0
Advait Jain <advaitjain@google.com>
2021-09-15 13:41:49 -0700

    Manual fixes to get the presubmit checks to pass.

--- tensorflow/lite/kernels/kernel_util.h
+++ tensorflow/lite/kernels/kernel_util.h
@@ -142,6 +142,12 @@ const TfLiteTensor* GetIntermediates(TfLiteContext* context,
 TfLiteStatus GetIntermediatesSafe(const TfLiteContext* context,
                                   const TfLiteNode* node, int index,
                                   TfLiteTensor** tensor);
+
+// Gets the output shape from the input tensor.
+TfLiteStatus GetOutputShapeFromInput(TfLiteContext* context,
+                                     const TfLiteTensor* input,
+                                     TfLiteIntArray** output_shape);
+
 #endif  // TF_LITE_STATIC_MEMORY
 
 inline int NumDimensions(const TfLiteTensor* t) { return t->dims->size; }
@@ -270,11 +276,6 @@ void CalculateActivationRange(TfLiteFusedActivation activation,
 
 // Return true if the given tensors have the same shape.
 bool HaveSameShapes(const TfLiteTensor* input1, const TfLiteTensor* input2);
-
-// Gets the output shape from the input tensor.
-TfLiteStatus GetOutputShapeFromInput(TfLiteContext* context,
-                                     const TfLiteTensor* input,
-                                     TfLiteIntArray** output_shape);
 
 // Calculates the output_shape that is necessary for element-wise operations
 // with broadcasting involving the two input tensors.
