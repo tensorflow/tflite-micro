tensorflow/tflite-micro

commit 0beb99eb2aed1407e68440b7ed02c49e5c1f54ea

Automated sync from github.com/tensorflow/tensorflow (#629)

tensorflow/lite/micro/tools/ci_build/test_code_style.sh
@@ -34,17 +34,17 @@ set +e
 # --fix_formatting to let the script fix both code and build file format error.
 FIX_FORMAT_FLAG=${1}
 
-pushd tensorflow/lite/
 ############################################################
 # License Check
 ############################################################
-micro/tools/make/downloads/pigweed/pw_presubmit/py/pw_presubmit/pigweed_presubmit.py \
-  kernels/internal/reference/ \
-  micro/ \
-  ../../third_party/ \
+tensorflow/lite/micro/tools/make/downloads/pigweed/pw_presubmit/py/pw_presubmit/pigweed_presubmit.py \
+  tensorflow/lite/kernels/internal/reference/ \
+  tensorflow/lite/micro/ \
+  third_party/ \
   -p copyright_notice \
   -e kernels/internal/reference/integer_ops/ \
   -e kernels/internal/reference/reference_ops.h \
+  -e python/schema_py_generated.py \
   -e tools/make/downloads \
   -e tools/make/targets/ecm3531 \
   -e BUILD\
@@ -76,13 +76,19 @@ else
   FIX_FORMAT_OPTIONS=""
 fi
 
-micro/tools/make/downloads/pigweed/pw_presubmit/py/pw_presubmit/format_code.py \
+tensorflow/lite/micro/tools/make/downloads/pigweed/pw_presubmit/py/pw_presubmit/format_code.py \
   ${FIX_FORMAT_OPTIONS} \
+  -e .github \
+  -e third_party/hexagon \
+  -e third_party/xtensa \
+  -e ci \
   -e c/common.c \
   -e core/api/error_reporter.cc \
   -e kernels/internal/reference/integer_ops/ \
   -e kernels/internal/reference/reference_ops.h \
   -e kernels/internal/types.h \
+  -e lite/python \
+  -e lite/tools \
   -e experimental \
   -e schema/schema_generated.h \
   -e schema/schema_utils.h \
@@ -109,6 +115,7 @@ BUILD_FORMAT_RESULT=$?
 #############################################################################
 # Avoided specific-code snippets for TFLM
 #############################################################################
+pushd tensorflow/lite/
 
 CHECK_CONTENTS_PATHSPEC=\
 "micro"\
@@ -141,11 +148,12 @@ check_contents "\<assert\>" "${ASSERT_PATHSPEC}" \
   "assert should not be used in TFLM code.."
 ASSERT_RESULT=$?
 
+popd
+
 ###########################################################################
 # All checks are complete, clean up.
 ###########################################################################
 
-popd
 
 # Re-enable exit on error now that we are done with the temporary git repo.
 set -e

tensorflow/lite/tools/flatbuffer_utils.py
@@ -1,0 +1,228 @@
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Utility functions for FlatBuffers.
+
+All functions that are commonly used to work with FlatBuffers.
+
+Refer to the tensorflow lite flatbuffer schema here:
+tensorflow/lite/schema/schema.fbs
+
+"""
+
+import copy
+import random
+import re
+
+import flatbuffers
+from tensorflow.lite.python import schema_py_generated as schema_fb
+from tensorflow.python.platform import gfile
+
+_TFLITE_FILE_IDENTIFIER = b'TFL3'
+
+
+def convert_bytearray_to_object(model_bytearray):
+  """Converts a tflite model from a bytearray to an object for parsing."""
+  model_object = schema_fb.Model.GetRootAsModel(model_bytearray, 0)
+  return schema_fb.ModelT.InitFromObj(model_object)
+
+
+def read_model(input_tflite_file):
+  """Reads a tflite model as a python object.
+
+  Args:
+    input_tflite_file: Full path name to the input tflite file
+
+  Raises:
+    RuntimeError: If input_tflite_file path is invalid.
+    IOError: If input_tflite_file cannot be opened.
+
+  Returns:
+    A python object corresponding to the input tflite file.
+  """
+  if not gfile.Exists(input_tflite_file):
+    raise RuntimeError('Input file not found at %r\n' % input_tflite_file)
+  with gfile.GFile(input_tflite_file, 'rb') as input_file_handle:
+    model_bytearray = bytearray(input_file_handle.read())
+  return convert_bytearray_to_object(model_bytearray)
+
+
+def read_model_with_mutable_tensors(input_tflite_file):
+  """Reads a tflite model as a python object with mutable tensors.
+
+  Similar to read_model() with the addition that the returned object has
+  mutable tensors (read_model() returns an object with immutable tensors).
+
+  Args:
+    input_tflite_file: Full path name to the input tflite file
+
+  Raises:
+    RuntimeError: If input_tflite_file path is invalid.
+    IOError: If input_tflite_file cannot be opened.
+
+  Returns:
+    A mutable python object corresponding to the input tflite file.
+  """
+  return copy.deepcopy(read_model(input_tflite_file))
+
+
+def convert_object_to_bytearray(model_object):
+  """Converts a tflite model from an object to a immutable bytearray."""
+  # Initial size of the buffer, which will grow automatically if needed
+  builder = flatbuffers.Builder(1024)
+  model_offset = model_object.Pack(builder)
+  builder.Finish(model_offset, file_identifier=_TFLITE_FILE_IDENTIFIER)
+  model_bytearray = bytes(builder.Output())
+  return model_bytearray
+
+
+def write_model(model_object, output_tflite_file):
+  """Writes the tflite model, a python object, into the output file.
+
+  Args:
+    model_object: A tflite model as a python object
+    output_tflite_file: Full path name to the output tflite file.
+
+  Raises:
+    IOError: If output_tflite_file path is invalid or cannot be opened.
+  """
+  model_bytearray = convert_object_to_bytearray(model_object)
+  with gfile.GFile(output_tflite_file, 'wb') as output_file_handle:
+    output_file_handle.write(model_bytearray)
+
+
+def strip_strings(model):
+  """Strips all nonessential strings from the model to reduce model size.
+
+  We remove the following strings:
+  (find strings by searching ":string" in the tensorflow lite flatbuffer schema)
+  1. Model description
+  2. SubGraph name
+  3. Tensor names
+  We retain OperatorCode custom_code and Metadata name.
+
+  Args:
+    model: The model from which to remove nonessential strings.
+  """
+
+  model.description = None
+  for subgraph in model.subgraphs:
+    subgraph.name = None
+    for tensor in subgraph.tensors:
+      tensor.name = None
+  # We clear all signature_def structure, since without names it is useless.
+  model.signatureDefs = None
+
+
+def randomize_weights(model, random_seed=0, buffers_to_skip=None):
+  """Randomize weights in a model.
+
+  Args:
+    model: The model in which to randomize weights.
+    random_seed: The input to the random number generator (default value is 0).
+    buffers_to_skip: The list of buffer indices to skip. The weights in these
+                     buffers are left unmodified.
+  """
+
+  # The input to the random seed generator. The default value is 0.
+  random.seed(random_seed)
+
+  # Parse model buffers which store the model weights
+  buffers = model.buffers
+  buffer_ids = range(1, len(buffers))  # ignore index 0 as it's always None
+  if buffers_to_skip is not None:
+    buffer_ids = [idx for idx in buffer_ids if idx not in buffers_to_skip]
+
+  for i in buffer_ids:
+    buffer_i_data = buffers[i].data
+    buffer_i_size = 0 if buffer_i_data is None else buffer_i_data.size
+
+    # Raw data buffers are of type ubyte (or uint8) whose values lie in the
+    # range [0, 255]. Those ubytes (or unint8s) are the underlying
+    # representation of each datatype. For example, a bias tensor of type
+    # int32 appears as a buffer 4 times it's length of type ubyte (or uint8).
+    # TODO(b/152324470): This does not work for float as randomized weights may
+    # end up as denormalized or NaN/Inf floating point numbers.
+    for j in range(buffer_i_size):
+      buffer_i_data[j] = random.randint(0, 255)
+
+
+def rename_custom_ops(model, map_custom_op_renames):
+  """Rename custom ops so they use the same naming style as builtin ops.
+
+  Args:
+    model: The input tflite model.
+    map_custom_op_renames: A mapping from old to new custom op names.
+  """
+  for op_code in model.operatorCodes:
+    if op_code.customCode:
+      op_code_str = op_code.customCode.decode('ascii')
+      if op_code_str in map_custom_op_renames:
+        op_code.customCode = map_custom_op_renames[op_code_str].encode('ascii')
+
+
+def xxd_output_to_bytes(input_cc_file):
+  """Converts xxd output C++ source file to bytes (immutable).
+
+  Args:
+    input_cc_file: Full path name to th C++ source file dumped by xxd
+
+  Raises:
+    RuntimeError: If input_cc_file path is invalid.
+    IOError: If input_cc_file cannot be opened.
+
+  Returns:
+    A bytearray corresponding to the input cc file array.
+  """
+  # Match hex values in the string with comma as separator
+  pattern = re.compile(r'\W*(0x[0-9a-fA-F,x ]+).*')
+
+  model_bytearray = bytearray()
+
+  with open(input_cc_file) as file_handle:
+    for line in file_handle:
+      values_match = pattern.match(line)
+
+      if values_match is None:
+        continue
+
+      # Match in the parentheses (hex array only)
+      list_text = values_match.group(1)
+
+      # Extract hex values (text) from the line
+      # e.g. 0x1c, 0x00, 0x00, 0x00, 0x54, 0x46, 0x4c,
+      values_text = filter(None, list_text.split(','))
+
+      # Convert to hex
+      values = [int(x, base=16) for x in values_text]
+      model_bytearray.extend(values)
+
+  return bytes(model_bytearray)
+
+
+def xxd_output_to_object(input_cc_file):
+  """Converts xxd output C++ source file to object.
+
+  Args:
+    input_cc_file: Full path name to th C++ source file dumped by xxd
+
+  Raises:
+    RuntimeError: If input_cc_file path is invalid.
+    IOError: If input_cc_file cannot be opened.
+
+  Returns:
+    A python object corresponding to the input tflite file.
+  """
+  model_bytes = xxd_output_to_bytes(input_cc_file)
+  return convert_bytearray_to_object(model_bytes)

tensorflow/lite/tools/flatbuffer_utils_test.py
@@ -1,0 +1,234 @@
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for flatbuffer_utils.py."""
+import copy
+import os
+import subprocess
+
+from tensorflow.lite.tools import flatbuffer_utils
+from tensorflow.lite.tools import test_utils
+from tensorflow.python.framework import test_util
+from tensorflow.python.platform import test
+
+_SKIPPED_BUFFER_INDEX = 1
+
+
+class WriteReadModelTest(test_util.TensorFlowTestCase):
+
+  def testWriteReadModel(self):
+    # 1. SETUP
+    # Define the initial model
+    initial_model = test_utils.build_mock_model()
+    # Define temporary files
+    tmp_dir = self.get_temp_dir()
+    model_filename = os.path.join(tmp_dir, 'model.tflite')
+
+    # 2. INVOKE
+    # Invoke the write_model and read_model functions
+    flatbuffer_utils.write_model(initial_model, model_filename)
+    final_model = flatbuffer_utils.read_model(model_filename)
+
+    # 3. VALIDATE
+    # Validate that the initial and final models are the same
+    # Validate the description
+    self.assertEqual(initial_model.description, final_model.description)
+    # Validate the main subgraph's name, inputs, outputs, operators and tensors
+    initial_subgraph = initial_model.subgraphs[0]
+    final_subgraph = final_model.subgraphs[0]
+    self.assertEqual(initial_subgraph.name, final_subgraph.name)
+    for i in range(len(initial_subgraph.inputs)):
+      self.assertEqual(initial_subgraph.inputs[i], final_subgraph.inputs[i])
+    for i in range(len(initial_subgraph.outputs)):
+      self.assertEqual(initial_subgraph.outputs[i], final_subgraph.outputs[i])
+    for i in range(len(initial_subgraph.operators)):
+      self.assertEqual(initial_subgraph.operators[i].opcodeIndex,
+                       final_subgraph.operators[i].opcodeIndex)
+    initial_tensors = initial_subgraph.tensors
+    final_tensors = final_subgraph.tensors
+    for i in range(len(initial_tensors)):
+      self.assertEqual(initial_tensors[i].name, final_tensors[i].name)
+      self.assertEqual(initial_tensors[i].type, final_tensors[i].type)
+      self.assertEqual(initial_tensors[i].buffer, final_tensors[i].buffer)
+      for j in range(len(initial_tensors[i].shape)):
+        self.assertEqual(initial_tensors[i].shape[j], final_tensors[i].shape[j])
+    # Validate the first valid buffer (index 0 is always None)
+    initial_buffer = initial_model.buffers[1].data
+    final_buffer = final_model.buffers[1].data
+    for i in range(initial_buffer.size):
+      self.assertEqual(initial_buffer.data[i], final_buffer.data[i])
+
+
+class StripStringsTest(test_util.TensorFlowTestCase):
+
+  def testStripStrings(self):
+    # 1. SETUP
+    # Define the initial model
+    initial_model = test_utils.build_mock_model()
+    final_model = copy.deepcopy(initial_model)
+
+    # 2. INVOKE
+    # Invoke the strip_strings function
+    flatbuffer_utils.strip_strings(final_model)
+
+    # 3. VALIDATE
+    # Validate that the initial and final models are the same except strings
+    # Validate the description
+    self.assertIsNotNone(initial_model.description)
+    self.assertIsNone(final_model.description)
+    self.assertIsNotNone(initial_model.signatureDefs)
+    self.assertIsNone(final_model.signatureDefs)
+
+    # Validate the main subgraph's name, inputs, outputs, operators and tensors
+    initial_subgraph = initial_model.subgraphs[0]
+    final_subgraph = final_model.subgraphs[0]
+    self.assertIsNotNone(initial_model.subgraphs[0].name)
+    self.assertIsNone(final_model.subgraphs[0].name)
+    for i in range(len(initial_subgraph.inputs)):
+      self.assertEqual(initial_subgraph.inputs[i], final_subgraph.inputs[i])
+    for i in range(len(initial_subgraph.outputs)):
+      self.assertEqual(initial_subgraph.outputs[i], final_subgraph.outputs[i])
+    for i in range(len(initial_subgraph.operators)):
+      self.assertEqual(initial_subgraph.operators[i].opcodeIndex,
+                       final_subgraph.operators[i].opcodeIndex)
+    initial_tensors = initial_subgraph.tensors
+    final_tensors = final_subgraph.tensors
+    for i in range(len(initial_tensors)):
+      self.assertIsNotNone(initial_tensors[i].name)
+      self.assertIsNone(final_tensors[i].name)
+      self.assertEqual(initial_tensors[i].type, final_tensors[i].type)
+      self.assertEqual(initial_tensors[i].buffer, final_tensors[i].buffer)
+      for j in range(len(initial_tensors[i].shape)):
+        self.assertEqual(initial_tensors[i].shape[j], final_tensors[i].shape[j])
+    # Validate the first valid buffer (index 0 is always None)
+    initial_buffer = initial_model.buffers[1].data
+    final_buffer = final_model.buffers[1].data
+    for i in range(initial_buffer.size):
+      self.assertEqual(initial_buffer.data[i], final_buffer.data[i])
+
+
+class RandomizeWeightsTest(test_util.TensorFlowTestCase):
+
+  def testRandomizeWeights(self):
+    # 1. SETUP
+    # Define the initial model
+    initial_model = test_utils.build_mock_model()
+    final_model = copy.deepcopy(initial_model)
+
+    # 2. INVOKE
+    # Invoke the randomize_weights function
+    flatbuffer_utils.randomize_weights(final_model)
+
+    # 3. VALIDATE
+    # Validate that the initial and final models are the same, except that
+    # the weights in the model buffer have been modified (i.e, randomized)
+    # Validate the description
+    self.assertEqual(initial_model.description, final_model.description)
+    # Validate the main subgraph's name, inputs, outputs, operators and tensors
+    initial_subgraph = initial_model.subgraphs[0]
+    final_subgraph = final_model.subgraphs[0]
+    self.assertEqual(initial_subgraph.name, final_subgraph.name)
+    for i in range(len(initial_subgraph.inputs)):
+      self.assertEqual(initial_subgraph.inputs[i], final_subgraph.inputs[i])
+    for i in range(len(initial_subgraph.outputs)):
+      self.assertEqual(initial_subgraph.outputs[i], final_subgraph.outputs[i])
+    for i in range(len(initial_subgraph.operators)):
+      self.assertEqual(initial_subgraph.operators[i].opcodeIndex,
+                       final_subgraph.operators[i].opcodeIndex)
+    initial_tensors = initial_subgraph.tensors
+    final_tensors = final_subgraph.tensors
+    for i in range(len(initial_tensors)):
+      self.assertEqual(initial_tensors[i].name, final_tensors[i].name)
+      self.assertEqual(initial_tensors[i].type, final_tensors[i].type)
+      self.assertEqual(initial_tensors[i].buffer, final_tensors[i].buffer)
+      for j in range(len(initial_tensors[i].shape)):
+        self.assertEqual(initial_tensors[i].shape[j], final_tensors[i].shape[j])
+    # Validate the first valid buffer (index 0 is always None)
+    initial_buffer = initial_model.buffers[1].data
+    final_buffer = final_model.buffers[1].data
+    for j in range(initial_buffer.size):
+      self.assertNotEqual(initial_buffer.data[j], final_buffer.data[j])
+
+  def testRandomizeSomeWeights(self):
+    # 1. SETUP
+    # Define the initial model
+    initial_model = test_utils.build_mock_model()
+    final_model = copy.deepcopy(initial_model)
+
+    # 2. INVOKE
+    # Invoke the randomize_weights function, but skip the first buffer
+    flatbuffer_utils.randomize_weights(
+        final_model, buffers_to_skip=[_SKIPPED_BUFFER_INDEX])
+
+    # 3. VALIDATE
+    # Validate that the initial and final models are the same, except that
+    # the weights in the model buffer have been modified (i.e, randomized)
+    # Validate the description
+    self.assertEqual(initial_model.description, final_model.description)
+    # Validate the main subgraph's name, inputs, outputs, operators and tensors
+    initial_subgraph = initial_model.subgraphs[0]
+    final_subgraph = final_model.subgraphs[0]
+    self.assertEqual(initial_subgraph.name, final_subgraph.name)
+    for i, _ in enumerate(initial_subgraph.inputs):
+      self.assertEqual(initial_subgraph.inputs[i], final_subgraph.inputs[i])
+    for i, _ in enumerate(initial_subgraph.outputs):
+      self.assertEqual(initial_subgraph.outputs[i], final_subgraph.outputs[i])
+    for i, _ in enumerate(initial_subgraph.operators):
+      self.assertEqual(initial_subgraph.operators[i].opcodeIndex,
+                       final_subgraph.operators[i].opcodeIndex)
+    initial_tensors = initial_subgraph.tensors
+    final_tensors = final_subgraph.tensors
+    for i, _ in enumerate(initial_tensors):
+      self.assertEqual(initial_tensors[i].name, final_tensors[i].name)
+      self.assertEqual(initial_tensors[i].type, final_tensors[i].type)
+      self.assertEqual(initial_tensors[i].buffer, final_tensors[i].buffer)
+      for j in range(len(initial_tensors[i].shape)):
+        self.assertEqual(initial_tensors[i].shape[j], final_tensors[i].shape[j])
+    # Validate that the skipped buffer is unchanged.
+    initial_buffer = initial_model.buffers[_SKIPPED_BUFFER_INDEX].data
+    final_buffer = final_model.buffers[_SKIPPED_BUFFER_INDEX].data
+    for j in range(initial_buffer.size):
+      self.assertEqual(initial_buffer.data[j], final_buffer.data[j])
+
+
+class XxdOutputToBytesTest(test_util.TensorFlowTestCase):
+
+  def testXxdOutputToBytes(self):
+    # 1. SETUP
+    # Define the initial model
+    initial_model = test_utils.build_mock_model()
+    initial_bytes = flatbuffer_utils.convert_object_to_bytearray(initial_model)
+
+    # Define temporary files
+    tmp_dir = self.get_temp_dir()
+    model_filename = os.path.join(tmp_dir, 'model.tflite')
+
+    # 2. Write model to temporary file (will be used as input for xxd)
+    flatbuffer_utils.write_model(initial_model, model_filename)
+
+    # 3. DUMP WITH xxd
+    input_cc_file = os.path.join(tmp_dir, 'model.cc')
+
+    command = 'xxd -i {} > {}'.format(model_filename, input_cc_file)
+    subprocess.call(command, shell=True)
+
+    # 4. VALIDATE
+    final_bytes = flatbuffer_utils.xxd_output_to_bytes(input_cc_file)
+
+    # Validate that the initial and final bytearray are the same
+    self.assertEqual(initial_bytes, final_bytes)
+
+
+if __name__ == '__main__':
+  test.main()

commit b74f90b0390d399e5f9fc675bb82db7b4a7a2d83

Make code format check work again (and fix some formatting errors). (#663)
Prior to this change, non of the files were getting tested.

i.e. the following command:
```
tensorflow/lite/micro/tools/ci_build/test_code_style.sh
```

Would have the output:
```
Formatting files in the tflite_micro repo that do not match 16 patterns (.github, third_party/hexagon, third_party/xtensa, ci, c/common.c, core/api/error_reporter.cc, kernels/internal/reference/integer_ops/, kernels/internal/reference/reference_ops.h, kernels/internal/types.h, lite/python, lite/tools, experimental, schema/schema_generated.h, schema/schema_utils.h, \.inc, \.md)
Checking formatting for 0 files
```

With this change, the output is now:
```
Formatting files in the tflite_micro repo that do not match 16 patterns (\.github, third_party/hexagon, third_party/xtensa, ci, c/common.c, core/api/error_reporter.cc, kernels/internal/reference/integer_ops/, kernels/internal/reference/reference_ops.h, kernels/internal/types.h, lite/python, lite/tools, experimental, schema/schema_generated.h, schema/schema_utils.h, \.inc, \.md)
Checking formatting for 773 files
./            7 files
tensorflow/   752 files (344 .cc, 186 .h, 44 .csv, 178 other)
third_party/  14 files (5 .bzl, 2 .bazel, 1 .system, 6 other)
```

This regression was likely introduced with #629.

 * #629 was manually patched to exclude lite/python and a few additional
   folders from the Python style check.
 * For some (currently unknown) reason, adding -e .github meant that all
   the paths were getting excluded.
 * -e "\.github" fixes the issue.

BUG=fix regression in CI checks.

tensorflow/lite/micro/integration_tests/seanet/conv/integration_tests.cc
@@ -133,63 +133,73 @@ void RunModel(const uint8_t* model, const int16_t* input,
 TF_LITE_MICRO_TESTS_BEGIN
 
 TF_LITE_MICRO_TEST(conv0_test) {
-  tflite::micro::RunModel(
-      g_conv0_model_data, g_conv0_input_int16_test_data, g_conv0_input_int16_test_data_size,
-      g_conv0_golden_int16_test_data, g_conv0_golden_int16_test_data_size, "conv0 test");
+  tflite::micro::RunModel(g_conv0_model_data, g_conv0_input_int16_test_data,
+                          g_conv0_input_int16_test_data_size,
+                          g_conv0_golden_int16_test_data,
+                          g_conv0_golden_int16_test_data_size, "conv0 test");
 }
 
 TF_LITE_MICRO_TEST(conv1_test) {
-  tflite::micro::RunModel(
-      g_conv1_model_data, g_conv1_input_int16_test_data, g_conv1_input_int16_test_data_size,
-      g_conv1_golden_int16_test_data, g_conv1_golden_int16_test_data_size, "conv1 test");
+  tflite::micro::RunModel(g_conv1_model_data, g_conv1_input_int16_test_data,
+                          g_conv1_input_int16_test_data_size,
+                          g_conv1_golden_int16_test_data,
+                          g_conv1_golden_int16_test_data_size, "conv1 test");
 }
 
 TF_LITE_MICRO_TEST(conv2_test) {
-  tflite::micro::RunModel(
-      g_conv2_model_data, g_conv2_input_int16_test_data, g_conv2_input_int16_test_data_size,
-      g_conv2_golden_int16_test_data, g_conv2_golden_int16_test_data_size, "conv2 test");
+  tflite::micro::RunModel(g_conv2_model_data, g_conv2_input_int16_test_data,
+                          g_conv2_input_int16_test_data_size,
+                          g_conv2_golden_int16_test_data,
+                          g_conv2_golden_int16_test_data_size, "conv2 test");
 }
 
 TF_LITE_MICRO_TEST(conv3_test) {
-  tflite::micro::RunModel(
-      g_conv3_model_data, g_conv3_input_int16_test_data, g_conv3_input_int16_test_data_size,
-      g_conv3_golden_int16_test_data, g_conv3_golden_int16_test_data_size, "conv3 test");
+  tflite::micro::RunModel(g_conv3_model_data, g_conv3_input_int16_test_data,
+                          g_conv3_input_int16_test_data_size,
+                          g_conv3_golden_int16_test_data,
+                          g_conv3_golden_int16_test_data_size, "conv3 test");
 }
 
 TF_LITE_MICRO_TEST(conv4_test) {
-  tflite::micro::RunModel(
-      g_conv4_model_data, g_conv4_input_int16_test_data, g_conv4_input_int16_test_data_size,
-      g_conv4_golden_int16_test_data, g_conv4_golden_int16_test_data_size, "conv4 test");
+  tflite::micro::RunModel(g_conv4_model_data, g_conv4_input_int16_test_data,
+                          g_conv4_input_int16_test_data_size,
+                          g_conv4_golden_int16_test_data,
+                          g_conv4_golden_int16_test_data_size, "conv4 test");
 }
 
 TF_LITE_MICRO_TEST(conv5_test) {
-  tflite::micro::RunModel(
-      g_conv5_model_data, g_conv5_input_int16_test_data, g_conv5_input_int16_test_data_size,
-      g_conv5_golden_int16_test_data, g_conv5_golden_int16_test_data_size, "conv5 test");
+  tflite::micro::RunModel(g_conv5_model_data, g_conv5_input_int16_test_data,
+                          g_conv5_input_int16_test_data_size,
+                          g_conv5_golden_int16_test_data,
+                          g_conv5_golden_int16_test_data_size, "conv5 test");
 }
 
 TF_LITE_MICRO_TEST(conv6_test) {
-  tflite::micro::RunModel(
-      g_conv6_model_data, g_conv6_input_int16_test_data, g_conv6_input_int16_test_data_size,
-      g_conv6_golden_int16_test_data, g_conv6_golden_int16_test_data_size, "conv6 test");
+  tflite::micro::RunModel(g_conv6_model_data, g_conv6_input_int16_test_data,
+                          g_conv6_input_int16_test_data_size,
+                          g_conv6_golden_int16_test_data,
+                          g_conv6_golden_int16_test_data_size, "conv6 test");
 }
 
 TF_LITE_MICRO_TEST(conv7_test) {
-  tflite::micro::RunModel(
-      g_conv7_model_data, g_conv7_input_int16_test_data, g_conv7_input_int16_test_data_size,
-      g_conv7_golden_int16_test_data, g_conv7_golden_int16_test_data_size, "conv7 test");
+  tflite::micro::RunModel(g_conv7_model_data, g_conv7_input_int16_test_data,
+                          g_conv7_input_int16_test_data_size,
+                          g_conv7_golden_int16_test_data,
+                          g_conv7_golden_int16_test_data_size, "conv7 test");
 }
 
 TF_LITE_MICRO_TEST(conv8_test) {
-  tflite::micro::RunModel(
-      g_conv8_model_data, g_conv8_input_int16_test_data, g_conv8_input_int16_test_data_size,
-      g_conv8_golden_int16_test_data, g_conv8_golden_int16_test_data_size, "conv8 test");
+  tflite::micro::RunModel(g_conv8_model_data, g_conv8_input_int16_test_data,
+                          g_conv8_input_int16_test_data_size,
+                          g_conv8_golden_int16_test_data,
+                          g_conv8_golden_int16_test_data_size, "conv8 test");
 }
 
 TF_LITE_MICRO_TEST(conv9_test) {
-  tflite::micro::RunModel(
-      g_conv9_model_data, g_conv9_input_int16_test_data, g_conv9_input_int16_test_data_size,
-      g_conv9_golden_int16_test_data, g_conv9_golden_int16_test_data_size, "conv9 test");
+  tflite::micro::RunModel(g_conv9_model_data, g_conv9_input_int16_test_data,
+                          g_conv9_input_int16_test_data_size,
+                          g_conv9_golden_int16_test_data,
+                          g_conv9_golden_int16_test_data_size, "conv9 test");
 }
 
 TF_LITE_MICRO_TEST(conv10_test) {

tensorflow/lite/micro/tools/ci_build/test_code_style.sh
@@ -78,7 +78,7 @@ fi
 
 tensorflow/lite/micro/tools/make/downloads/pigweed/pw_presubmit/py/pw_presubmit/format_code.py \
   ${FIX_FORMAT_OPTIONS} \
-  -e .github \
+  -e "\.github" \
   -e third_party/hexagon \
   -e third_party/xtensa \
   -e ci \