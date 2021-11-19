56c0c96091a97bfdeaf7a464db2177626b3cc0eb
Advait Jain <advaitjain@google.com>
2021-11-19 15:14:43 -0800

    format the code.

--- tensorflow/lite/micro/kernels/kernel_util.cc
+++ tensorflow/lite/micro/kernels/kernel_util.cc
@@ -13,8 +13,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/micro/kernels/kernel_util.h"
+
+#include "tensorflow/lite/c/common.h"
 
 namespace tflite {
 namespace micro {
