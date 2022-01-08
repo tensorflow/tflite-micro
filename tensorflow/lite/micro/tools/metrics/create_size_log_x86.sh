e6659c2fff7a551611bf84c5ffc6f8fab55a007b
Deqiang Chen <deqiangc@google.com>
2022-01-07 17:57:22 -0800

    Generate log per target binary and move them to their own directory

--- tensorflow/lite/micro/tools/metrics/create_size_log_x86.sh
+++ tensorflow/lite/micro/tools/metrics/create_size_log_x86.sh
@@ -38,7 +38,7 @@ function profile_a_binary() {
   local binary=${1}
   local log=${2}
 
-  raw_size=$(size ${GEN_FILES_DIR}/${binary})
+  raw_size=$(size ${binary})
   # Skip the title row
   sizes=$(echo "${raw_size}" | sed -n '2 p')
   text_size=$(echo "$sizes" | awk '{print $1}')
@@ -46,7 +46,7 @@ function profile_a_binary() {
   bss_size=$(echo "$sizes" | awk '{print $3}')
   total_size=$(echo "$sizes" | awk '{print $4}')
 
-  echo "${binary}, ${text_size}, ${data_size}, ${bss_size}, ${total_size}" >> ${log}
+  echo "${BUILD_TIME}, ${HEAD_SHA}, ${text_size}, ${data_size}, ${bss_size}, ${total_size}" >> ${log}
 }
 
 # Parameters:
@@ -54,39 +54,28 @@ function profile_a_binary() {
 function start_size_report() {
   local log=${1}  
 
-  echo "" >> ${log}
-  echo "${START_HEADER}"  >> ${log}
-  date >> ${log}
-  echo "Profiled at ${HEAD_SHA}" >> ${log}
-  echo "binary name, text, data, bss, total" >> ${log}
-
-  readable_run make -f tensorflow/lite/micro/tools/make/Makefile third_party_downloads
+  if [[ ! -f ${log} ]]
+  then
+    echo "${CSV_HEADER}" >> ${log}
+  fi
 }
 
 # Parameters:
-# ${1} - size log file name
-function end_size_report() {
-  local log=${1}  
-
-  echo "${END_HEADER}"  >> ${log}
-  echo "" >> ${log}
-}
-
-# Parameters:
-# ${1} - (size) log file name
-# ${2} - target binary name such as keyword_benchmark
+# ${1}: binary target name such as keyworkd_benchmark
+# ${2}: build type such as RELEASE, DEFAULT
+# ${3}: target such as linux
+# ${4}: target architecture such as x86_64
 function report_size() {
-  local log=${1}
-  local binary=${2}
+  local binary=$1
+  local build_type=$2
+  local target=$3
+  local target_arch=$4
+  local log="${LOG_ROOT_DIR}/${binary}.csv"
 
-  local target="linux"
-  local target_arch="x86_64"
-  local build_type="release"
+  start_size_report ${log}
 
   build_target ${binary} ${build_type} ${target} ${target_arch}
   local build_result=$?
-  local binary_path="${target}_${target_arch}_${build_type}/bin/${binary}"
-
 
   if [[ ${build_result} != 0 ]]
   then
@@ -97,6 +86,7 @@ function report_size() {
   fi
 
   # If build is successful, profile the size.
+  local binary_path="${GEN_FILES_DIR}/${target}_${target_arch}_${build_type}/bin/${binary}"
   profile_a_binary ${binary_path} ${log}
 }
 
@@ -106,28 +96,30 @@ function report_size() {
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR=${SCRIPT_DIR}/../../../../..
 GEN_FILES_DIR=${ROOT_DIR}/tensorflow/lite/micro/tools/make/gen/
-cd "${ROOT_DIR}"
 
+cd "${ROOT_DIR}"
 source tensorflow/lite/micro/tools/ci_build/helper_functions.sh
 
-START_HEADER="################## START ##############################"
-END_HEADER="#################### END ############################"
+CSV_HEADER="date, sha, text, data, bss, totoal"
 HEAD_SHA=`git rev-parse HEAD`
-SIZE_LOG=${SCRIPT_DIR}/size_data/size_linux_x86_64_release.csv
+BUILD_TIME=`date`
+TARGET="linux"
+TARGET_ARCH="x86_64"
+BUILD_TYPE="release"
+LOG_ROOT_DIR=${ROOT_DIR}/data/continuous_builds/size_profiling/${TARGET}_${TARGET_ARCH}_${BUILD_TYPE}
 
-# Clean previous build including downloads
-make -f tensorflow/lite/micro/tools/make/Makefile clean clean_downloads
+# Clean the own build and download third party
+readable_run make -f tensorflow/lite/micro/tools/make/Makefile clean clean_downloads
+readable_run make -f tensorflow/lite/micro/tools/make/Makefile third_party_downloads
 
-start_size_report ${SIZE_LOG}
-
-report_size ${SIZE_LOG} keyword_benchmark
+report_size keyword_benchmark ${BUILD_TYPE} ${TARGET} ${TARGET_ARCH}
 KEYWORD_BENCHMARK_STATUS=$?
-report_size ${SIZE_LOG} baseline_memory_footprint
-BASELINE_MEMORY_FOOTPRINT_STATUS=$?
-report_size ${SIZE_LOG} interpreter_memory_footprint
-INTERPRETER_MEMORY_FOOTPRINT_STATUS=$?
 
-end_size_report ${SIZE_LOG}
+report_size baseline_memory_footprint ${BUILD_TYPE} ${TARGET} ${TARGET_ARCH}
+BASELINE_MEMORY_FOOTPRINT_STATUS=$?
+
+report_size interpreter_memory_footprint ${BUILD_TYPE} ${TARGET} ${TARGET_ARCH}
+INTERPRETER_MEMORY_FOOTPRINT_STATUS=$?
 
 if [[ ${KEYWORD_BENCHMARK_STATUS} != 0 || ${BASELINE_MEMORY_FOOTPRINT_STATUS} != 0 || ${INTERPRETER_MEMORY_FOOTPRINT_STATUS} != 0 ]]
 then
@@ -135,5 +127,5 @@ then
   exit -1
 fi
 
-## TODO: run difference detection and also return error code if detecting large increase
+## TODO(b/213646558): run difference detection and also return error code if detecting large increase
 echo "Profiling succeed"
