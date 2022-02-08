d1a241a9f901c4fd21d2be74318c02cf027ea9d3
Jens Elofsson <jens.elofsson@arm.com>
2022-02-08 11:52:48 +0100

    Increase stack size for Arm Corstone-300 target.
    Increase stack size for to 0x50000 to make the micro_interpreter_test work.

--- tensorflow/lite/micro/tools/make/ethos_u_core_platform_download.sh
+++ tensorflow/lite/micro/tools/make/ethos_u_core_platform_download.sh
@@ -90,7 +90,8 @@ else
   sed -i 's/tensor_arena/\*tensor_arena\*/' ${LINKER_PATH}/platform_parsed.ld
 
   # Increase stack size - needed for some tests.
-  sed -i 's/__STACK_SIZE = 0x00008000/__STACK_SIZE = 0x00030000/' ${LINKER_PATH}/platform_parsed.ld
+  sed -i 's/__STACK_SIZE = 0x00008000/__STACK_SIZE = 0x00050000/' ${LINKER_PATH}/platform_parsed.ld
+  sed -i 's/STACK_SIZE 0x8000/STACK_SIZE 0x50000/' ${LINKER_PATH}/platform.scatter
 
   # Patch retarget.c so that g++ can find exit symbol.
   cat <<EOT >> ${DOWNLOADED_ETHOS_U_CORE_PLATFORM_PATH}/targets/corstone-300/retarget.c
