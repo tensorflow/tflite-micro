b44fb5ceff795ce889a6fc5787de997c96690ee7
Ting Yan <tingyan@google.com>
2022-04-29 15:51:38 -0700

    Correct LSTM test bug with wrong output dimension

--- tensorflow/lite/micro/kernels/unidirectional_sequence_lstm.cc
+++ tensorflow/lite/micro/kernels/unidirectional_sequence_lstm.cc
@@ -1266,6 +1266,14 @@ TfLiteStatus UnidirectionalSequenceLstmPrepare(TfLiteContext* context,
   TF_LITE_ENSURE_EQ(context, NumElements(output_state), n_batch * n_output);
   TF_LITE_ENSURE_EQ(context, NumElements(cell_state), n_batch * n_cell);
 
+  // Check the shape of output tensor against that of input tensor
+  TF_LITE_ENSURE_EQ(context, input->dims->size, output->dims->size);
+  for (int i = 0; i < input->dims->size - 1; ++i) {
+    TF_LITE_ENSURE_EQ(context, input->dims->data[i], output->dims->data[i]);
+  }
+  TF_LITE_ENSURE_EQ(context, output->dims->data[output->dims->size - 1],
+                    n_output);
+
   if (is_integer) {
     const int num_intermediate_tensors = node->intermediates->size;
     TF_LITE_ENSURE(context, num_intermediate_tensors == 5);
