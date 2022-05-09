c360771f1e1907ca74a83a81183bc0a4d597befe
Steven Toribio <toribiosteven@google.com>
2022-05-09 10:26:15 -0400

    trying typeid instead of decltype/is_same combo

--- tensorflow/lite/micro/testing/micro_test.h
+++ tensorflow/lite/micro/testing/micro_test.h
@@ -124,8 +124,8 @@ inline void InitializeTest() { InitializeTarget(); }
 // the printf specifier is %d.
 #define TF_LITE_MICRO_EXPECT_EQ(x, y)                                      \
   do {                                                                     \
-    bool checkIntX = (typeid(x) == typeid(0))                              \
-    bool checkIntY = (typeid(y) == typeid(0))                              \
+    bool checkIntX = std::is_same<decltype(x) , decltype(0)>::value        \
+    bool checkIntY = std::is_same<decltype(y) , decltype(0)>::value        \
     if (checkIntX && checkIntY) {                                          \
       if ((x) != (y)) {                                                    \
         MicroPrintf(#x " == " #y " failed at %s:%d (%d vs %d)", __FILE__,  \
@@ -135,23 +135,24 @@ inline void InitializeTest() { InitializeTarget(); }
     } else {                                                               \
            MicroPrintf("Ints should be used as arugments for"              \
       "TF_LITE_MICRO_EXPECT_EQ macro used at %s:%d", __FILE__, __LINE__);  \  
-        micro_test::did_test_fail = true;                                  \
+      micro_test::did_test_fail = true;                                    \
     }                                                                      \
   } while (false)
 
 #define TF_LITE_MICRO_EXPECT_NE(x, y)                                      \
   do {                                                                     \
-    bool checkIntX = (typeid(x) == typeid(0))                              \
-    bool checkIntY = (typeid(y) == typeid(0))                              \
+    bool checkIntX = std::is_same<decltype(x) , decltype(0)>::value        \
+    bool checkIntY = std::is_same<decltype(y) , decltype(0)>::value        \
     if (checkIntX && checkIntY) {                                          \
       if ((x) == (y)) {                                                    \
         MicroPrintf(#x " != " #y " failed at %s:%d", __FILE__, __LINE__);  \
         micro_test::did_test_fail = true;                                  \
       }                                                                    \
-    } else {                                                               \
+    }                                                                      \
+    else {                                                                 \
       MicroPrintf("Ints should be used as arugments for"                   \
       "TF_LITE_MICRO_EXPECT_NE macro used at %s:%d", __FILE__, __LINE__);  \                                           
-        micro_test::did_test_fail = true;                                  \
+      micro_test::did_test_fail = true;                                    \
     }
   } while (false)
 
