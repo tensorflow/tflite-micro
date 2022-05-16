b5d8c5a190c64b3c9d71035cd0b8d452484188f1
Steven Toribio <toribiosteven@google.com>
2022-05-16 11:41:13 +0000

    git presubmit style formatting changes

--- tensorflow/lite/micro/kernels/resize_nearest_neighbor_test.cc
+++ tensorflow/lite/micro/kernels/resize_nearest_neighbor_test.cc
@@ -67,7 +67,8 @@ void TestResizeNearestNeighbor(int* input_dims_data, const T* input_data,
 
   // compare results
   for (int i = 0; i < output_dims_count; ++i) {
-    TF_LITE_MICRO_EXPECT_NEAR(expected_output_data[i], output_data[i], 0.0000000001f);
+    TF_LITE_MICRO_EXPECT_NEAR(expected_output_data[i], output_data[i],
+                              0.0000000001f);
   }
 }
 
