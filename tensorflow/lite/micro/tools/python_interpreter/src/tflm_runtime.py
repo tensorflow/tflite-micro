ed6510d46e4a5889d7ebe35ecf1b79e85b99ce5d
Pauline Sho <psho@google.com>
2022-05-25 12:58:09 -0400

    Removed extra debug code

--- tensorflow/lite/micro/tools/python_interpreter/src/tflm_runtime.py
+++ tensorflow/lite/micro/tools/python_interpreter/src/tflm_runtime.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+"""Python package for TFLM Python Interpreter"""
 
 import os
 
