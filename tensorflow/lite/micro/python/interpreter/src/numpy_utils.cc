90d62ab628a256fd37d810173e81533e6d51d94b
Pauline Sho <psho@google.com>
2022-06-03 12:09:54 -0700

    Implement Python interpreter for TFLM (#1147)
    * First pass working with float input/output
    
    * Numpy buffer input works. Got a sine graph for hello world output.
    
    Copyright notice
    
    * Remove static references
    
    * Output tensors work
    
    * Output tensors work
    
    * bazel test works
    
    bazel test tensorflow/lite/micro/tools:interpreter_test
    --test_output=all
    
    * Successfully built extension with Python setuptools
    
    cd tensorflow/lite/micro/tools/interpreter_pypi
    python -m build
    pip install dist/example_tflm_interpreter_psho-0.0.3-cp39-cp39-linux_x86_64.whl --force-reinstall
    python tests/test-interpreter.py
    
    TestPyPi:
    pip uninstall example-tflm-interpreter-psho
    python -m twine upload --repository testpypi dist/example-tflm-interpreter-psho-0.0.3.tar.gz
    pip install -i https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple example-tflm-interpreter-psho==0.0.3
    python tests/test-interpreter.py
    
    * Refactored some
    
    * Cleaned for production and tested with conv model
    
    * Fixed formatting
    
    * Fixed numpy header issue for Docker. Need to fix double registration issue that's somehow only present in Docker.
    
    * Test fix with changes in ci.yml
    
    * Added sudo
    
    * Added nomsan and noasan tags to python test
    
    * Added -layering-check, used py_library instead of py_binary to generate conv model, added micro namespace
    
    * Removed extra debug code
    
    * Added new tests
    
    * Changed reinterpret_steal to cast
    
    * Addressed PR comments
    
    * size log refactor (#1156)
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    
    * Addressed PR and review comments
    
    * adding documentation to log_binary* workflows. (#1161)
    
    * Automated binary size log update (#1160)
    
    * Create an allocator to manage persistent arena (#1174)
    
    * Create an allocator to manage persistent arena
    
    This PR corresponds to internal cl/452380512.
    
    Create an allocator to manage persistent arena.
    This is another step towards the feature of enabling the client to
    have two separate memory arenas.
    
    BUG=https://b/226971240
    
    * Manually patch due to Makefile difference
    
    * Fix virtual environment when building Vela for Arm Ethos-U (#1177)
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    
    * Removed print statement
    
    Co-authored-by: jwithers <jpwithers@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: TFLM-bot <tflm-github-bot@google.com>
    Co-authored-by: deqiangc <86809673+deqiangc@users.noreply.github.com>
    Co-authored-by: Måns Nilsson <mans.nilsson@arm.com>

---
+++ tensorflow/lite/micro/python/interpreter/src/numpy_utils.cc
@@ -0,0 +1,121 @@
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/lite/micro/python/interpreter/src/numpy_utils.h"
+
+// Disallow Numpy 1.7 deprecated symbols.
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+// Since we are calling `import_array()` here, define PY_ARRAY_UNIQUE_SYMBOL
+// here and NO_IMPORT_ARRAY everywhere else arrayobject.h is included
+// See https://numpy.org/doc/1.16/reference/c-api.array.html#importing-the-api
+#define PY_ARRAY_UNIQUE_SYMBOL tflite_micro_python_interpreter_array_api
+#include <numpy/arrayobject.h>
+
+#include "tensorflow/lite/c/c_api_types.h"
+
+namespace tflite {
+
+void* ImportNumpy() {
+  // import_array() is actually a macro that returns NULL (in Python3), hence
+  // this wrapper function with a return type of void*.
+  import_array();
+  return nullptr;
+}
+
+int TfLiteTypeToPyArrayType(TfLiteType tf_lite_type) {
+  switch (tf_lite_type) {
+    case kTfLiteFloat32:
+      return NPY_FLOAT32;
+    case kTfLiteFloat16:
+      return NPY_FLOAT16;
+    case kTfLiteFloat64:
+      return NPY_FLOAT64;
+    case kTfLiteInt32:
+      return NPY_INT32;
+    case kTfLiteUInt32:
+      return NPY_UINT32;
+    case kTfLiteUInt16:
+      return NPY_UINT16;
+    case kTfLiteInt16:
+      return NPY_INT16;
+    case kTfLiteUInt8:
+      return NPY_UINT8;
+    case kTfLiteInt8:
+      return NPY_INT8;
+    case kTfLiteInt64:
+      return NPY_INT64;
+    case kTfLiteUInt64:
+      return NPY_UINT64;
+    case kTfLiteString:
+      return NPY_STRING;
+    case kTfLiteBool:
+      return NPY_BOOL;
+    case kTfLiteComplex64:
+      return NPY_COMPLEX64;
+    case kTfLiteComplex128:
+      return NPY_COMPLEX128;
+    case kTfLiteResource:
+    case kTfLiteVariant:
+      return NPY_OBJECT;
+    case kTfLiteNoType:
+      return NPY_NOTYPE;
+      // Avoid default so compiler errors created when new types are made.
+  }
+  return NPY_NOTYPE;
+}
+
+TfLiteType TfLiteTypeFromPyType(int py_type) {
+  switch (py_type) {
+    case NPY_FLOAT32:
+      return kTfLiteFloat32;
+    case NPY_FLOAT16:
+      return kTfLiteFloat16;
+    case NPY_FLOAT64:
+      return kTfLiteFloat64;
+    case NPY_INT32:
+      return kTfLiteInt32;
+    case NPY_UINT32:
+      return kTfLiteUInt32;
+    case NPY_INT16:
+      return kTfLiteInt16;
+    case NPY_UINT8:
+      return kTfLiteUInt8;
+    case NPY_INT8:
+      return kTfLiteInt8;
+    case NPY_INT64:
+      return kTfLiteInt64;
+    case NPY_UINT64:
+      return kTfLiteUInt64;
+    case NPY_BOOL:
+      return kTfLiteBool;
+    case NPY_OBJECT:
+    case NPY_STRING:
+    case NPY_UNICODE:
+      return kTfLiteString;
+    case NPY_COMPLEX64:
+      return kTfLiteComplex64;
+    case NPY_COMPLEX128:
+      return kTfLiteComplex128;
+      // Avoid default so compiler errors created when new types are made.
+  }
+  return kTfLiteNoType;
+}
+
+TfLiteType TfLiteTypeFromPyArray(const PyArrayObject* array) {
+  int pyarray_type = PyArray_TYPE(array);
+  return TfLiteTypeFromPyType(pyarray_type);
+}
+
+}  // namespace tflite
