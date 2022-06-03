d3825019a1d0118961a8b5ed65aee05b92194f06
Steven Toribio <toribiosteven@google.com>
2022-06-03 22:57:10 +0000

    fixing kernal runner

--- tensorflow/lite/micro/kernels/kernel_runner.cc
+++ tensorflow/lite/micro/kernels/kernel_runner.cc
@@ -49,6 +49,7 @@ KernelRunner::KernelRunner(const TfLiteRegistration& registration,
   context_.impl_ = static_cast<void*>(&fake_micro_context_);
   context_.ReportError = MicroContextReportOpError;
   context_.recommended_num_threads = 1;
+  context_->GetTensor = MicroContextGetTensor;
   context_.GetEvalTensor = MicroContextGetEvalTensor;
   tflite::micro:: clearBufferAPI(&context_);
   context_.AllocatePersistentBuffer = MicroContextAllocatePersistentBuffer;
