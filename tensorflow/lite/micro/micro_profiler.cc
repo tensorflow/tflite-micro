4444512fd30ef25d5bba70f665341e614eedc067
Volker Eckert <volker@plumerai.com>
2022-06-06 09:25:50 +0100

    #1151: fixup: PRI format

--- tensorflow/lite/micro/micro_profiler.cc
+++ tensorflow/lite/micro/micro_profiler.cc
@@ -51,7 +51,7 @@ void MicroProfiler::Log() const {
 #if !defined(TF_LITE_STRIP_ERROR_STRINGS)
   for (int i = 0; i < num_events_; ++i) {
     uint32_t ticks = end_ticks_[i] - start_ticks_[i];
-    MicroPrintf("%s took %"PRIu32" ticks (%d ms).", tags_[i], ticks, TicksToMs(ticks));
+    MicroPrintf("%s took %" PRIu32 " ticks (%d ms).", tags_[i], ticks, TicksToMs(ticks));
   }
 #endif
 }
@@ -61,7 +61,7 @@ void MicroProfiler::LogCsv() const {
   MicroPrintf("\"Event\",\"Tag\",\"Ticks\"");
   for (int i = 0; i < num_events_; ++i) {
     uint32_t ticks = end_ticks_[i] - start_ticks_[i];
-    MicroPrintf("%d,%s,%"PRIu32, i, tags_[i], ticks);
+    MicroPrintf("%d,%s,%" PRIu32, i, tags_[i], ticks);
   }
 #endif
 }
