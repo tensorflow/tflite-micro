de3f2b73b78b8dc24de94e75cce8ca8ecb0aad96
Steven Toribio <toribiosteven@google.com>
2022-07-06 21:05:03 +0000

    Changed micromutable op resovler so that it no longer uses error_reporter since the error returns didn't stop the program in favor of microprintf for error messages. Also used dcheck to crash/abort program is number of op used is greater then the maximum number allowed

--- tensorflow/lite/micro/micro_mutable_op_resolver.h
+++ tensorflow/lite/micro/micro_mutable_op_resolver.h
@@ -32,6 +32,7 @@ limitations under the License.
 #include "tensorflow/lite/micro/kernels/reduce.h"
 #include "tensorflow/lite/micro/kernels/softmax.h"
 #include "tensorflow/lite/micro/micro_op_resolver.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 
 namespace tflite {
@@ -85,22 +86,14 @@ class MicroMutableOpResolver : public MicroOpResolver {
   // kTfLiteError.
   TfLiteStatus AddCustom(const char* name, TfLiteRegistration* registration) {
     if (registrations_len_ >= tOpCount) {
-      if (error_reporter_) {
-        TF_LITE_REPORT_ERROR(
-            error_reporter_,
-            "Couldn't register custom op '%s', resolver size is too small (%d)",
-            name, tOpCount);
-      }
-      return kTfLiteError;
+      MicroPrintf("Couldn't register custom op '%s' ", name);
+      MicroPrintf("resolver size is too small (%d)", tOpCount);
+      TFLITE_ABORT;
     }
 
     if (FindOp(name) != nullptr) {
-      if (error_reporter_ != nullptr) {
-        TF_LITE_REPORT_ERROR(error_reporter_,
-                             "Calling AddCustom for the same op more than once "
-                             "is not supported (Op: %s).",
-                             name);
-      }
+      MicroPrintf("Calling AddCustom for the same op more than once ");
+      MicroPrintf("is not supported (Op: %s).", name);
       return kTfLiteError;
     }
 
@@ -582,32 +575,21 @@ class MicroMutableOpResolver : public MicroOpResolver {
                           const TfLiteRegistration& registration,
                           MicroOpResolver::BuiltinParseFunction parser) {
     if (op == BuiltinOperator_CUSTOM) {
-      if (error_reporter_ != nullptr) {
-        TF_LITE_REPORT_ERROR(error_reporter_,
-                             "Invalid parameter BuiltinOperator_CUSTOM to the "
-                             "AddBuiltin function.");
-      }
+      MicroPrintf("Invalid parameter BuiltinOperator_CUSTOM to the ");
+      MicroPrintf("AddBuiltin function.");
       return kTfLiteError;
     }
 
     if (FindOp(op) != nullptr) {
-      if (error_reporter_ != nullptr) {
-        TF_LITE_REPORT_ERROR(error_reporter_,
-                             "Calling AddBuiltin with the same op more than "
-                             "once is not supported (Op: #%d).",
-                             op);
-      }
+      MicroPrintf("Calling AddBuiltin with the same op more than ");
+      MicroPrintf("once is not supported (Op: #%d).", op);
       return kTfLiteError;
     }
 
     if (registrations_len_ >= tOpCount) {
-      if (error_reporter_) {
-        TF_LITE_REPORT_ERROR(error_reporter_,
-                             "Couldn't register builtin op #%d, resolver size "
-                             "is too small (%d).",
-                             op, tOpCount);
-      }
-      return kTfLiteError;
+      MicroPrintf("Couldn't register builtin op #%d, resolver size ", op);
+      MicroPrintf("is too small (%d).", tOpCount);
+      TFLITE_ABORT;
     }
 
     registrations_[registrations_len_] = registration;
