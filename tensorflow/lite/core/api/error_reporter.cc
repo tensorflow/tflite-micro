c610371fc07aa50946c6546fe889e4597af92b14
Advait Jain <advaitjain@google.com>
2022-07-21 10:52:38 -0700

    restore file whose source of truth is the upstream TF repo.

--- tensorflow/lite/core/api/error_reporter.cc
+++ tensorflow/lite/core/api/error_reporter.cc
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 #include "tensorflow/lite/core/api/error_reporter.h"
-
 #include <cstdarg>
 
 namespace tflite {
