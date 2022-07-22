67da610613e1a429e4c80e56b90b48dfa7282c4c
Steven Toribio <toribiosteven@google.com>
2022-07-22 18:12:48 +0000

    modified align tool made by rocky into a sort of template for my tool

--- tensorflow/lite/micro/tools/tflite_flatbuffer_transform_tool.cc
+++ tensorflow/lite/micro/tools/tflite_flatbuffer_transform_tool.cc
@@ -18,39 +18,9 @@ limitations under the License.
 #include "flatbuffers/util.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 
-const bool dump_buffers = false;
-
-void dump_model_buffers(const tflite::Model* model) {
-  fprintf(stderr, "model version: 0x%x\n", model->version());
-  // This is a pointer to a vector of offsets:
-  const flatbuffers::Vector<flatbuffers::Offset<tflite::Buffer>>* buffers =
-      model->buffers();
-  fprintf(stderr, "buffers: 0x%p\n", buffers);
-  const flatbuffers::Vector<flatbuffers::Offset<tflite::Buffer>>&
-      buffer_offsets = *buffers;
-  int number_of_buffers = buffer_offsets.size();
-  fprintf(stderr, "number of buffers: %d\n", buffer_offsets.size());
-  for (int i = 0; i < number_of_buffers; ++i) {
-    // C++ magic returns the actual buffer pointer here, rather than the
-    // expected Offset that the Vector seems to hold:
-    const tflite::Buffer* buffer = buffer_offsets[i];
-    const flatbuffers::Vector<uint8_t>* data = buffer->data();
-    // Only the weight buffers are allocated in the flatbuffer:
-    if (data) {
-      size_t buffer_size = data->size();
-      const uint8_t* buffer_addr = data->Data();
-      int buffer_offset = buffer_addr - reinterpret_cast<const uint8_t*>(model);
-      fprintf(stderr, "buffer %d size: %zu, addr: 0x%p, offset: 0x%x\n", i,
-              buffer_size, buffer_addr, buffer_offset);
-      fprintf(stderr, "buffer contents: %x %x %x %x %x %x %x %x\n",
-              buffer_addr[0], buffer_addr[1], buffer_addr[2], buffer_addr[3],
-              buffer_addr[4], buffer_addr[5], buffer_addr[6], buffer_addr[7]);
-    }
-  }
-}
 
 int main(int argc, char** argv) {
-  if (argc < 2) {
+  if (argc < 1) {
     fprintf(stderr, "usage: %s input_flatbuffer output_flatbuffer\n", argv[0]);
     exit(-1);
   }
@@ -61,7 +31,6 @@ int main(int argc, char** argv) {
   // Parse the string into a C++ class.  Model is the root object of a tflite
   // flatbuffer file.
   const tflite::Model* model = tflite::GetModel(model_file.c_str());
-  if (dump_buffers) dump_model_buffers(model);
   // A packed model is basically the file format mmaped into memory.
   // Unpacking it and then packing it with the C++ API should yield
   // a file with the force_align attributes respected.
@@ -71,11 +40,10 @@ int main(int argc, char** argv) {
   auto new_model = tflite::Model::Pack(fbb, unpacked_model);
   fbb.Finish(new_model, tflite::ModelIdentifier());
   const tflite::Model* aligned_model = tflite::GetModel(fbb.GetBufferPointer());
-  if (dump_buffers) dump_model_buffers(aligned_model);
-  flatbuffers::SaveFile(argv[2],
-                        reinterpret_cast<char*>(fbb.GetBufferPointer()),
-                        fbb.GetSize(), /*binary*/ true);
+  // flatbuffers::SaveFile(argv[2],
+  //                       reinterpret_cast<char*>(fbb.GetBufferPointer()),
+  //                       fbb.GetSize(), /*binary*/ true);
   int size = fbb.GetSize();
-  fprintf(stderr, "Aligned model size: %d\n", size);
+  fprintf(stderr, "New Optimized model size: %d\n", size);
   return 0;
 }
