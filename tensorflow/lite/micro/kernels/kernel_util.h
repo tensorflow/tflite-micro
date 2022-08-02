62422cb32b32edd3a4264c434ed9d7290a45817b
Steven Toribio <toribiosteven@google.com>
2022-08-02 01:01:03 +0000

    missing include in header

--- tensorflow/lite/micro/kernels/kernel_util.h
+++ tensorflow/lite/micro/kernels/kernel_util.h
@@ -21,6 +21,7 @@ limitations under the License.
 #include "tensorflow/lite/c/builtin_op_data.h"
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/kernels/internal/compatibility.h"
+#include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
 #include "tensorflow/lite/kernels/internal/types.h"
 #include "tensorflow/lite/micro/micro_context.h"
 
