11f06406faa080d1cd4f0628dd5b56b0a1109f5b
Steven Toribio <34755817+turbotoribio@users.noreply.github.com>
2022-08-02 20:31:48 -0700

    Update zeros_like.cc

--- tensorflow/lite/micro/kernels/zeros_like.cc
+++ tensorflow/lite/micro/kernels/zeros_like.cc
@@ -17,7 +17,7 @@ limitations under the License.
 #include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
 #include "tensorflow/lite/kernels/kernel_util.h"
 #include "tensorflow/lite/micro/kernels/kernel_util.h"
-#include "tensorflow/lite/micro/micro_error_reporter.h"
+
 
 namespace tflite {
 namespace {
