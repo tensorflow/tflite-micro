14d341b8c9b654bc4c4231a53ebc9549d96433f6
Steven Toribio <34755817+turbotoribio@users.noreply.github.com>
2022-08-02 20:26:50 -0700

    Update elu.cc

--- tensorflow/lite/micro/kernels/elu.cc
+++ tensorflow/lite/micro/kernels/elu.cc
@@ -25,7 +25,7 @@ limitations under the License.
 #include "tensorflow/lite/kernels/internal/types.h"
 #include "tensorflow/lite/kernels/kernel_util.h"
 #include "tensorflow/lite/micro/kernels/kernel_util.h"
-#include "tensorflow/lite/micro/micro_error_reporter.h"
+
 
 namespace tflite {
 namespace {
