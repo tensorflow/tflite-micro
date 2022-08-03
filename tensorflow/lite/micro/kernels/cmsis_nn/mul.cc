7a0b13466ef0edfe222e7b689e23bad922102dc1
Steven Toribio <34755817+turbotoribio@users.noreply.github.com>
2022-08-02 20:24:40 -0700

    Update mul.cc

--- tensorflow/lite/micro/kernels/cmsis_nn/mul.cc
+++ tensorflow/lite/micro/kernels/cmsis_nn/mul.cc
@@ -24,7 +24,6 @@ limitations under the License.
 #include "tensorflow/lite/micro/kernels/kernel_util.h"
 #include "tensorflow/lite/micro/kernels/mul.h"
 #include "tensorflow/lite/micro/memory_helpers.h"
-#include "tensorflow/lite/micro/micro_error_reporter.h"
 
 namespace tflite {
 namespace {
