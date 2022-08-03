d050d76ee56e8d565fe86f9e350d5f62b689deda
Steven Toribio <34755817+turbotoribio@users.noreply.github.com>
2022-08-02 20:23:55 -0700

    Update add.cc

--- tensorflow/lite/micro/kernels/cmsis_nn/add.cc
+++ tensorflow/lite/micro/kernels/cmsis_nn/add.cc
@@ -25,7 +25,7 @@ limitations under the License.
 #include "tensorflow/lite/kernels/op_macros.h"
 #include "tensorflow/lite/micro/kernels/kernel_util.h"
 #include "tensorflow/lite/micro/memory_helpers.h"
-#include "tensorflow/lite/micro/micro_error_reporter.h"
+
 
 namespace tflite {
 
