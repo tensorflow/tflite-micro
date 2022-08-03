4cafaa461ea96560ad82c6636bcec7655324e7c7
Steven Toribio <34755817+turbotoribio@users.noreply.github.com>
2022-08-02 20:30:22 -0700

    Update transpose_conv.cc

--- tensorflow/lite/micro/kernels/transpose_conv.cc
+++ tensorflow/lite/micro/kernels/transpose_conv.cc
@@ -24,7 +24,7 @@ limitations under the License.
 #include "tensorflow/lite/kernels/kernel_util.h"
 #include "tensorflow/lite/kernels/padding.h"
 #include "tensorflow/lite/micro/kernels/kernel_util.h"
-#include "tensorflow/lite/micro/micro_error_reporter.h"
+
 #include "tensorflow/lite/micro/micro_utils.h"
 
 namespace tflite {
