e4e543c2ecf04dfcbc7b7ea34f6ec985078e6ae8
Steven Toribio <34755817+turbotoribio@users.noreply.github.com>
2022-08-02 20:27:07 -0700

    Update gather_nd.cc

--- tensorflow/lite/micro/kernels/gather_nd.cc
+++ tensorflow/lite/micro/kernels/gather_nd.cc
@@ -17,7 +17,6 @@ limitations under the License.
 #include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
 #include "tensorflow/lite/kernels/kernel_util.h"
 #include "tensorflow/lite/micro/kernels/kernel_util.h"
-#include "tensorflow/lite/micro/micro_error_reporter.h"
 #include "tensorflow/lite/micro/micro_utils.h"
 
 namespace tflite {
