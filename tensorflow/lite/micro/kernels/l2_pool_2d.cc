8f195641c1d487ac986295197e2efc32956de904
Steven Toribio <34755817+turbotoribio@users.noreply.github.com>
2022-08-02 20:27:37 -0700

    Update l2_pool_2d.cc

--- tensorflow/lite/micro/kernels/l2_pool_2d.cc
+++ tensorflow/lite/micro/kernels/l2_pool_2d.cc
@@ -21,7 +21,7 @@ limitations under the License.
 #include "tensorflow/lite/kernels/kernel_util.h"
 #include "tensorflow/lite/kernels/padding.h"
 #include "tensorflow/lite/micro/kernels/kernel_util.h"
-#include "tensorflow/lite/micro/micro_error_reporter.h"
+
 
 namespace tflite {
 namespace {
