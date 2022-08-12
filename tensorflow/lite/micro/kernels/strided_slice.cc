a682c331b10903ac4960c2036a52762b0e038907
Ian McGraw <imcgraw@google.com>
2022-08-12 04:18:32 +0000

    Add bool to strided slice.

--- tensorflow/lite/micro/kernels/strided_slice.cc
+++ tensorflow/lite/micro/kernels/strided_slice.cc
@@ -12,17 +12,17 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include "tensorflow/lite/kernels/internal/reference/strided_slice.h"
+#include "third_party/tensorflow/lite/kernels/internal/reference/strided_slice.h"
 
 #include <cmath>
 #include <cstring>
 
-#include "tensorflow/lite/c/builtin_op_data.h"
-#include "tensorflow/lite/c/common.h"
-#include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
-#include "tensorflow/lite/kernels/kernel_util.h"
-#include "tensorflow/lite/kernels/op_macros.h"
-#include "tensorflow/lite/micro/kernels/kernel_util.h"
+#include "third_party/tensorflow/lite/c/builtin_op_data.h"
+#include "third_party/tensorflow/lite/c/common.h"
+#include "third_party/tensorflow/lite/kernels/internal/tensor_ctypes.h"
+#include "third_party/tensorflow/lite/kernels/kernel_util.h"
+#include "third_party/tensorflow/lite/kernels/op_macros.h"
+#include "third_party/tflite_micro/tensorflow/lite/micro/kernels/kernel_util.h"
 
 namespace tflite {
 namespace ops {
@@ -182,6 +182,13 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
           tflite::micro::GetTensorData<int32_t>(input),
           tflite::micro::GetTensorShape(output),
           tflite::micro::GetTensorData<int32_t>(output));
+      break;
+    case kTfLiteBool:
+      reference_ops::StridedSlice(op_params,
+                                  tflite::micro::GetTensorShape(input),
+                                  tflite::micro::GetTensorData<bool>(input),
+                                  tflite::micro::GetTensorShape(output),
+                                  tflite::micro::GetTensorData<bool>(output));
       break;
     default:
       MicroPrintf("Type %s (%d) not supported.", TfLiteTypeGetName(input->type),
