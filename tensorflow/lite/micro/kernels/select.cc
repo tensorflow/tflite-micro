46ea8669d674ca113a84c001597f30b13abbbe44
Ian McGraw <imcgraw@google.com>
2022-08-26 17:45:50 +0000

    Implement CheckBroadcastShape to avoid dynamic memory allocation.

--- tensorflow/lite/micro/kernels/select.cc
+++ tensorflow/lite/micro/kernels/select.cc
@@ -45,6 +45,40 @@ void* SelectInit(TfLiteContext* context, const char* buffer, size_t length) {
   return data;
 }
 
+TfLiteStatus CheckBroadcastShape(TfLiteContext* context,
+                                 const TfLiteTensor* input1,
+                                 const TfLiteTensor* input2,
+                                 const TfLiteTensor* input3,
+                                 TfLiteIntArray* output_shape) {
+  const int dims1 = NumDimensions(input1);
+  const int dims2 = NumDimensions(input2);
+  const int dims3 = NumDimensions(input3);
+  const int out_dims = std::max(std::max(dims1, dims2), dims3);
+  TF_LITE_ENSURE_EQ(context, out_dims, output_shape->size);
+
+  std::unique_ptr<TfLiteIntArray, void (*)(TfLiteIntArray*)> shape(
+      TfLiteIntArrayCreate(out_dims), TfLiteIntArrayFree);
+  for (int i = 0; i < out_dims; ++i) {
+    const int d1 = i >= dims1 ? 1 : SizeOfDimension(input1, dims1 - i - 1);
+    const int d2 = i >= dims2 ? 1 : SizeOfDimension(input2, dims2 - i - 1);
+    const int d3 = i >= dims3 ? 1 : SizeOfDimension(input3, dims3 - i - 1);
+    const int min_value = std::min(std::min(d1, d2), d3);
+    int max_value = std::max(std::max(d1, d2), d3);
+    // If one dimention is 0, others must be 0 or 1.
+    if (min_value == 0) max_value = 0;
+    if (!(d1 == 1 || d1 == max_value) || !(d2 == 1 || d2 == max_value) ||
+        !(d3 == 1 || d3 == max_value)) {
+      MicroPrintf("Given shapes, %s, %s and %s, are not broadcastable.",
+                  GetShapeDebugString(input1->dims).c_str(),
+                  GetShapeDebugString(input2->dims).c_str(),
+                  GetShapeDebugString(input3->dims).c_str());
+      return kTfLiteError;
+    }
+    TF_LITE_ENSURE_EQ(context, output_shape->data[out_dims -i - 1], max_value);
+  }
+  return kTfLiteOk;
+}
+
 TfLiteStatus SelectPrepare(TfLiteContext* context, TfLiteNode* node) {
   OpData* data = reinterpret_cast<OpData*>(node->user_data);
 
@@ -81,12 +115,9 @@ TfLiteStatus SelectPrepare(TfLiteContext* context, TfLiteNode* node) {
   bool same_shape = HaveSameShapes(input_condition, input_x) &&
                     HaveSameShapes(input_x, input_y);
   if (!same_shape) {
-    TfLiteIntArray* output_size;
-    TF_LITE_ENSURE_OK(context, CalculateShapeForBroadcast(
-        context, input_condition, input_x,
-        input_y, &output_size));
-    // Can only broadcast when the output shape doesn't change.
-    TF_LITE_ENSURE(context, TfLiteIntArrayEqual(output_size, output->dims));
+    TF_LITE_ENSURE_OK(
+        context, CheckBroadcastShape(context, input_condition, input_x, input_y,
+                                     output->dims));
     data->requires_broadcast = true;
   }
 
