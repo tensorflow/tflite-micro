868fb7e8af70a77fe3fc1a61d83757f51ce10f9e
suleshahid <110432064+suleshahid@users.noreply.github.com>
2022-08-29 17:09:39 +0000

    Nightly sync fix. (#1381)

--- ci/sync_from_upstream_tf.sh
+++ ci/sync_from_upstream_tf.sh
@@ -47,6 +47,8 @@ bazel build tensorflow/lite/python:schema_py
 bazel build tensorflow/lite/schema:schema_fbs_srcs
 /bin/cp ./bazel-bin/tensorflow/lite/schema/schema_generated.h tensorflow/lite/schema/schema_generated.h
 
+# Must clean the bazel directories out after building as we don't check these in.
+bazel clean
 
 # The shared TFL/TFLM python code uses a different bazel workspace in the two
 # repositories (TF and tflite-micro) which needs the import statements to be
