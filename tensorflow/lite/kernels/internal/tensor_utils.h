10250a2defaa8a3e43a7c494ee47844bb53a4e6e
Steven Toribio <toribiosteven@google.com>
2022-09-19 23:41:18 +0000

    style fix

--- tensorflow/lite/kernels/internal/tensor_utils.h
+++ tensorflow/lite/kernels/internal/tensor_utils.h
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================
 
-This is a fork from Tensorflow Lite for TensorflowLite Micro usage 
+This is a fork from Tensorflow Lite for TensorflowLite Micro usage
 */
 
 #include <algorithm>
