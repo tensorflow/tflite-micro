80740c3770189445b33112493d94076ed2572f6f
Steven Toribio <toribiosteven@google.com>
2022-09-19 23:26:30 +0000

    done

--- tensorflow/lite/kernels/internal/tensor_utils.h
+++ tensorflow/lite/kernels/internal/tensor_utils.h
@@ -12,8 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#ifndef TENSORFLOW_LITE_KERNELS_INTERNAL_TENSOR_UTILS_H_
-#define TENSORFLOW_LITE_KERNELS_INTERNAL_TENSOR_UTILS_H_
+
 
 #include <algorithm>
 #include <cmath>
