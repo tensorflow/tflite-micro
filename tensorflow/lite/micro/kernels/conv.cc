b82170ed306f232dd7c559397954a3a069ff8e0b
vamsimanchala <vamsimanchala@google.com>
2022-09-20 01:44:32 +0000

    examples, kernels and integration tests

--- tensorflow/lite/micro/kernels/conv.cc
+++ tensorflow/lite/micro/kernels/conv.cc
@@ -25,7 +25,7 @@ limitations under the License.
 #include "tensorflow/lite/kernels/kernel_util.h"
 #include "tensorflow/lite/kernels/padding.h"
 #include "tensorflow/lite/micro/kernels/kernel_util.h"
-#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_log.h"
 
 namespace tflite {
 namespace {
--- third_party/hexagon/svdf.cc
+++ third_party/hexagon/svdf.cc
@@ -53,6 +53,7 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "tensorflow/lite/kernels/op_macros.h"
 #include "tensorflow/lite/micro/kernels/activation_utils.h"
 #include "tensorflow/lite/micro/kernels/kernel_util.h"
+#include "tensorflow/lite/micro/micro_log.h"
 #include "tensorflow/lite/micro/micro_utils.h"
 #include "third_party/hexagon/hexagon_svdf.h"
 #include "third_party/hexagon/hexagon_tflm_translation_svdf.h"
