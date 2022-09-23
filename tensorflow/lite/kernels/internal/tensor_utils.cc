947e3327cfc1c7077e5d1857eee851131eeb84f7
Steven Toribio <34755817+turbotoribio@users.noreply.github.com>
2022-09-23 14:23:51 -0700

    fixing nits 1

--- tensorflow/lite/kernels/internal/tensor_utils.cc
+++ tensorflow/lite/kernels/internal/tensor_utils.cc
@@ -12,14 +12,14 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================
-
-internal/reference/portable_tensor_utils.h has the implementation of the
-functions dcelared in internal/portable_tensor_utils.h. This somewhat confusing
-setup is derived from how the code is organized in TfLite where it is used to
-select between NEON, SSE and portable implementaitons. See
-https://github.com/tensorflow/tensorflow/blob/d76c23975c4a3a0d7987cfe3f45c76566df06180/tensorflow/lite/kernels/internal/tensor_utils.cc
-for how the code is written in TfLite.
 */
+
+//internal/reference/portable_tensor_utils.h has the implementation of the
+//functions dcelared in internal/portable_tensor_utils.h. This somewhat confusing
+//setup is derived from how the code is organized in TfLite where it is used to
+//select between NEON, SSE and portable implementaitons. See
+//https://github.com/tensorflow/tensorflow/blob/d76c23975c4a3a0d7987cfe3f45c76566df06180/tensorflow/lite/kernels/internal/tensor_utils.cc
+//for how the code is written in TfLite.
 
 #include "tensorflow/lite/kernels/internal/portable_tensor_utils.h"
 #include "tensorflow/lite/kernels/internal/reference/portable_tensor_utils.h"
