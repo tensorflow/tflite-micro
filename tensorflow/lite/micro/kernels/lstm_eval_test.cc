6edd260948b6d4593e178bd8bafb156dbf691ab7
Renjie Wu <rewu@google.com>
2022-10-20 10:01:17 -0700

    make batch size variable

--- tensorflow/lite/micro/kernels/lstm_eval_test.cc
+++ tensorflow/lite/micro/kernels/lstm_eval_test.cc
@@ -30,14 +30,13 @@ namespace testing {
 namespace {
 constexpr int kInputDimension = 2;
 constexpr int kStateDimension = 2;
-constexpr int kBatchSize = 1;
 
 // Struct that holds the weight/bias information for a standard gate (i.e. no
 // modification such as layer normalization, peephole, etc.)
 struct GateParameters {
-  const float activation_weight[kInputDimension * kStateDimension * kBatchSize];
-  const float recurrent_weight[kInputDimension * kStateDimension * kBatchSize];
-  const float fused_bias[kStateDimension * kBatchSize];
+  const float activation_weight[kInputDimension * kStateDimension];
+  const float recurrent_weight[kInputDimension * kStateDimension];
+  const float fused_bias[kStateDimension];
 };
 
 // Set parameters for different gates
@@ -70,18 +69,19 @@ void ValidateResultGoldens(const T* golden, const T* output_data,
   }
 }
 
-void TestGateOutputFloat(const GateParameters& gate_params,
+void TestGateOutputFloat(const int batch_size,
+                         const GateParameters& gate_params,
                          TfLiteFusedActivation activation_type,
                          const float* input_dats, const float* recurrent_data,
                          const float* expected_vals) {
-  const int output_size = kBatchSize * kStateDimension;
+  const int output_size = batch_size * kStateDimension;
   float gate_output[output_size];
   tflite::lstm_internal::CalculateLstmGateFloat(
       input_dats, gate_params.activation_weight,
       /*aux_input=*/nullptr, /*aux_input_to_gate_weights*/ nullptr,
       recurrent_data, gate_params.recurrent_weight,
       /*cell_state=*/nullptr, /*cell_to_gate_weights=*/nullptr,
-      /*layer_norm_coefficients=*/nullptr, gate_params.fused_bias, kBatchSize,
+      /*layer_norm_coefficients=*/nullptr, gate_params.fused_bias, batch_size,
       kInputDimension, kInputDimension, kStateDimension, kStateDimension,
       /*activation=*/activation_type, gate_output,
       /*is_input_all_zeros=*/false,
@@ -95,6 +95,7 @@ void TestGateOutputFloat(const GateParameters& gate_params,
 
 TF_LITE_MICRO_TESTS_BEGIN
 TF_LITE_MICRO_TEST(CheckGateOutputFloat) {
+  const int batch_size = 1;
   const float input_data[] = {0.2, 0.3};
   const float recurrent_data[] = {-0.1, 0.2};
   // Use the forget gate parameters to test small gate outputs
@@ -103,8 +104,8 @@ TF_LITE_MICRO_TEST(CheckGateOutputFloat) {
   // [6.69285092e-03, 4.53978687e-05]
   const float expected_forget_gate_output[] = {6.69285092e-3f, 4.53978687e-5f};
   tflite::testing::TestGateOutputFloat(
-      tflite::testing::kForgetGateParameters, kTfLiteActSigmoid, input_data,
-      recurrent_data, expected_forget_gate_output);
+      batch_size, tflite::testing::kForgetGateParameters, kTfLiteActSigmoid,
+      input_data, recurrent_data, expected_forget_gate_output);
 
   // Use the input gate parameters to test small gate outputs
   // output = sigmoid(W_i*i+W_h*h+b) = sigmoid([[10,10],[20,20]][0.2, 0.3]
@@ -112,8 +113,8 @@ TF_LITE_MICRO_TEST(CheckGateOutputFloat) {
   // [0.99330715, 0.9999546]
   const float expected_input_gate_output[] = {0.99330715, 0.9999546};
   tflite::testing::TestGateOutputFloat(
-      tflite::testing::kInputGateParameters, kTfLiteActSigmoid, input_data,
-      recurrent_data, expected_input_gate_output);
+      batch_size, tflite::testing::kInputGateParameters, kTfLiteActSigmoid,
+      input_data, recurrent_data, expected_input_gate_output);
 
   // Use the output gate parameters to test normnal gate outputs
   // output = sigmoid(W_i*i+W_h*h+b) = sigmoid([[1,1],[1,1]][0.2, 0.3]
@@ -122,8 +123,8 @@ TF_LITE_MICRO_TEST(CheckGateOutputFloat) {
   const float expected_output_gate_output[] = {0.6456563062257954,
                                                0.6456563062257954};
   tflite::testing::TestGateOutputFloat(
-      tflite::testing::kOutputGateParameters, kTfLiteActSigmoid, input_data,
-      recurrent_data, expected_output_gate_output);
+      batch_size, tflite::testing::kOutputGateParameters, kTfLiteActSigmoid,
+      input_data, recurrent_data, expected_output_gate_output);
 
   // Use the modulation(cell) gate parameters to tanh output
   // output = tanh(W_i*i+W_h*h+b) = tanh([[1,1],[1,1]][0.2, 0.3]
@@ -132,40 +133,41 @@ TF_LITE_MICRO_TEST(CheckGateOutputFloat) {
   const float expected_modulation_gate_output[] = {0.5370495669980353,
                                                    0.5370495669980353};
   tflite::testing::TestGateOutputFloat(
-      tflite::testing::kModulationGateParameters, kTfLiteActTanh, input_data,
-      recurrent_data, expected_modulation_gate_output);
+      batch_size, tflite::testing::kModulationGateParameters, kTfLiteActTanh,
+      input_data, recurrent_data, expected_modulation_gate_output);
 }
 
 TF_LITE_MICRO_TEST(CheckCellUpdateFloat) {
+  const int batch_size = 1;
   float cell_data[] = {0.1, 0.2};
   float forget_gate[] = {0.2, 0.5};
   const float input_gate[] = {0.8, 0.9};
   const float cell_gate[] = {-0.3, 0.8};  // modulation gate
-  const int cell_size =
-      tflite::testing::kBatchSize * tflite::testing::kStateDimension;
+  const int cell_size = batch_size * tflite::testing::kStateDimension;
 
   // Cell = forget_gate*cell + input_gate*modulation_gate
   // = [0.02, 0.1] + [-0.24, 0.72] = [-0.22, 0.82]
   const float expected_cell_vals[] = {-0.22, 0.82};
 
   tflite::lstm_internal::UpdateLstmCellFloat(
-      tflite::testing::kBatchSize, tflite::testing::kStateDimension, cell_data,
-      input_gate, forget_gate, cell_gate, /*use_cifg=*/false, /*clip=*/10);
+      batch_size, tflite::testing::kStateDimension, cell_data, input_gate,
+      forget_gate, cell_gate, /*use_cifg=*/false, /*clip=*/10);
 
   tflite::testing::ValidateResultGoldens(expected_cell_vals, cell_data,
                                          cell_size, 1e-6f);
 }
 
 TF_LITE_MICRO_TEST(CheckOutputFloat) {
+  const int batch_size = 1;
+  // -1 and 5 for different tanh behavior (normal, saturated)
   const float cell_data[] = {-1, 5};
   const float output_gate[] = {0.2, 0.5};
-  const int output_size =
-      tflite::testing::kStateDimension * tflite::testing::kBatchSize;
+  const int output_size = tflite::testing::kStateDimension * batch_size;
   float output[output_size];
   float scratch[output_size];
 
   tflite::lstm_internal::CalculateLstmOutputFloat(
-      tflite::testing::kBatchSize, tflite::testing::kStateDimension,
+      batch_size, tflite::testing::kStateDimension,
       tflite::testing::kStateDimension, cell_data, output_gate, kTfLiteActTanh,
       nullptr, nullptr, 0, output, scratch);
 
