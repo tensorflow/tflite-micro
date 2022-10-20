3f5d76f347d9e8e687027bea8c7d742400b2680d
Renjie Wu <rewu@google.com>
2022-10-20 11:04:30 -0700

    global model setting

--- tensorflow/lite/micro/kernels/lstm_eval_test.cc
+++ tensorflow/lite/micro/kernels/lstm_eval_test.cc
@@ -34,6 +34,11 @@ constexpr int kBatchSize = 1;
 
 constexpr int kGateOutputSize = kBatchSize * kStateDimension;
 
+constexpr TfLiteLSTMParams kModelSettings = {
+    /*.activation=*/kTfLiteActTanh,
+    /*.cell_clip=*/10, /*.proj_clip=*/3, /*.kernel_type=*/kTfLiteLSTMFullKernel,
+    /*.asymmetric_quantize_inputs=*/true};
+
 // Struct that holds the weight/bias information for a standard gate (i.e. no
 // modification such as layer normalization, peephole, etc.)
 struct GateParameters {
@@ -44,22 +49,22 @@ struct GateParameters {
 
 // Set parameters for different gates
 // negative large weights for forget gate to make it really forget
-const GateParameters kForgetGateParameters = {
+constexpr GateParameters kForgetGateParameters = {
     /*.activation_weight=*/{-10, -10, -20, -20},
     /*.recurrent_weight=*/{-10, -10, -20, -20},
     /*.fused_bias=*/{1, 2}};
 // positive large weights for input gate to make it really remember
-const GateParameters kInputGateParameters = {
+constexpr GateParameters kInputGateParameters = {
     /*.activation_weight=*/{10, 10, 20, 20},
     /*.recurrent_weight=*/{10, 10, 20, 20},
     /*.fused_bias=*/{-1, -2}};
 // all ones to test the behavior of tanh at normal range (-1,1)
-const GateParameters kModulationGateParameters = {
+constexpr GateParameters kModulationGateParameters = {
     /*.activation_weight=*/{1, 1, 1, 1},
     /*.recurrent_weight=*/{1, 1, 1, 1},
     /*.fused_bias=*/{0, 0}};
 // all ones to test the behavior of sigmoid at normal range (-1. 1)
-const GateParameters kOutputGateParameters = {
+constexpr GateParameters kOutputGateParameters = {
     /*.activation_weight=*/{1, 1, 1, 1},
     /*.recurrent_weight=*/{1, 1, 1, 1},
     /*.fused_bias=*/{0, 0}};
@@ -74,13 +79,13 @@ void ValidateResultGoldens(const T* golden, const T* output_data,
 
 void TestGateOutputFloat(const GateParameters& gate_params,
                          TfLiteFusedActivation activation_type,
-                         const float* input_dats, const float* recurrent_data,
+                         const float* input_dats, const float* recurrent_state,
                          const float* expected_vals) {
   float gate_output[kGateOutputSize];
   tflite::lstm_internal::CalculateLstmGateFloat(
       input_dats, gate_params.activation_weight,
       /*aux_input=*/nullptr, /*aux_input_to_gate_weights*/ nullptr,
-      recurrent_data, gate_params.recurrent_weight,
+      recurrent_state, gate_params.recurrent_weight,
       /*cell_state=*/nullptr, /*cell_to_gate_weights=*/nullptr,
       /*layer_norm_coefficients=*/nullptr, gate_params.fused_bias, kBatchSize,
       kInputDimension, kInputDimension, kStateDimension, kStateDimension,
@@ -97,7 +102,7 @@ void TestGateOutputFloat(const GateParameters& gate_params,
 TF_LITE_MICRO_TESTS_BEGIN
 TF_LITE_MICRO_TEST(CheckGateOutputFloat) {
   const float input_data[] = {0.2, 0.3};
-  const float recurrent_data[] = {-0.1, 0.2};
+  const float recurrent_state[] = {-0.1, 0.2};
   // Use the forget gate parameters to test small gate outputs
   // output = sigmoid(W_i*i+W_h*h+b) = sigmoid([[-10,-10],[-20,-20]][0.2,
   // +[[-10,-10],[-20,-20]][-0.1, 0.2]+[1,2]) = sigmoid([-5,-10]) =
@@ -105,7 +110,7 @@ TF_LITE_MICRO_TEST(CheckGateOutputFloat) {
   const float expected_forget_gate_output[] = {6.69285092e-3f, 4.53978687e-5f};
   tflite::testing::TestGateOutputFloat(
       tflite::testing::kForgetGateParameters, kTfLiteActSigmoid, input_data,
-      recurrent_data, expected_forget_gate_output);
+      recurrent_state, expected_forget_gate_output);
 
   // Use the input gate parameters to test small gate outputs
   // output = sigmoid(W_i*i+W_h*h+b) = sigmoid([[10,10],[20,20]][0.2, 0.3]
@@ -114,7 +119,7 @@ TF_LITE_MICRO_TEST(CheckGateOutputFloat) {
   const float expected_input_gate_output[] = {0.99330715, 0.9999546};
   tflite::testing::TestGateOutputFloat(
       tflite::testing::kInputGateParameters, kTfLiteActSigmoid, input_data,
-      recurrent_data, expected_input_gate_output);
+      recurrent_state, expected_input_gate_output);
 
   // Use the output gate parameters to test normnal gate outputs
   // output = sigmoid(W_i*i+W_h*h+b) = sigmoid([[1,1],[1,1]][0.2, 0.3]
@@ -124,7 +129,7 @@ TF_LITE_MICRO_TEST(CheckGateOutputFloat) {
                                                0.6456563062257954};
   tflite::testing::TestGateOutputFloat(
       tflite::testing::kOutputGateParameters, kTfLiteActSigmoid, input_data,
-      recurrent_data, expected_output_gate_output);
+      recurrent_state, expected_output_gate_output);
 
   // Use the modulation(cell) gate parameters to tanh output
   // output = tanh(W_i*i+W_h*h+b) = tanh([[1,1],[1,1]][0.2, 0.3]
@@ -133,12 +138,13 @@ TF_LITE_MICRO_TEST(CheckGateOutputFloat) {
   const float expected_modulation_gate_output[] = {0.5370495669980353,
                                                    0.5370495669980353};
   tflite::testing::TestGateOutputFloat(
-      tflite::testing::kModulationGateParameters, kTfLiteActTanh, input_data,
-      recurrent_data, expected_modulation_gate_output);
+      tflite::testing::kModulationGateParameters,
+      tflite::testing::kModelSettings.activation, input_data, recurrent_state,
+      expected_modulation_gate_output);
 }
 
 TF_LITE_MICRO_TEST(CheckCellUpdateFloat) {
-  float cell_data[] = {0.1, 0.2};
+  float cell_state[] = {0.1, 0.2};
   float forget_gate[] = {0.2, 0.5};
   const float input_gate[] = {0.8, 0.9};
   const float cell_gate[] = {-0.3, 0.8};  // modulation gate
@@ -148,23 +154,26 @@ TF_LITE_MICRO_TEST(CheckCellUpdateFloat) {
   const float expected_cell_vals[] = {-0.22, 0.82};
 
   tflite::lstm_internal::UpdateLstmCellFloat(
-      tflite::testing::kBatchSize, tflite::testing::kStateDimension, cell_data,
-      input_gate, forget_gate, cell_gate, /*use_cifg=*/false, /*clip=*/10);
+      tflite::testing::kBatchSize, tflite::testing::kStateDimension, cell_state,
+      input_gate, forget_gate, cell_gate, /*use_cifg=*/false,
+      /*clip=*/tflite::testing::kModelSettings.cell_clip);
 
   tflite::testing::ValidateResultGoldens(
-      expected_cell_vals, cell_data, tflite::testing::kGateOutputSize, 1e-6f);
+      expected_cell_vals, cell_state, tflite::testing::kGateOutputSize, 1e-6f);
 }
 
-TF_LITE_MICRO_TEST(CheckOutputFloat) {
+TF_LITE_MICRO_TEST(CheckOutputCalculationFloat) {
   // -1 and 5 for different tanh behavior (normal, saturated)
-  const float cell_data[] = {-1, 5};
+  const float cell_state[] = {-1, 5};
   const float output_gate[] = {0.2, 0.5};
+  // If no projection layer, hidden state dimension == output dimension == cell
+  // state dimension
   float output[tflite::testing::kGateOutputSize];
   float scratch[tflite::testing::kGateOutputSize];
 
   tflite::lstm_internal::CalculateLstmOutputFloat(
       tflite::testing::kBatchSize, tflite::testing::kStateDimension,
-      tflite::testing::kStateDimension, cell_data, output_gate, kTfLiteActTanh,
+      tflite::testing::kStateDimension, cell_state, output_gate, kTfLiteActTanh,
       nullptr, nullptr, 0, output, scratch);
 
   // Output state generate the output and copy it to the hidden state
@@ -176,4 +185,47 @@ TF_LITE_MICRO_TEST(CheckOutputFloat) {
       expected_output_vals, output, tflite::testing::kGateOutputSize, 1e-6f);
 }
 
+// TF_LITE_MICRO_TEST(CheckOneStepLSTMFloat) {
+//   const float input_data[] = {0.2, 0.3};
+//   float recurrent_state[tflite::testing::kGateOutputSize];
+//   float cell_state[tflite::testing::kGateOutputSize];
+//   float output[tflite::testing::kGateOutputSize];
+
+//   float cell_gate_scratch[tflite::testing::kGateOutputSize];
+//   float forget_gate_scratch[tflite::testing::kGateOutputSize];
+//   float input_gate_scratch[tflite::testing::kGateOutputSize];
+//   float output_gate_scratch[tflite::testing::kGateOutputSize];
+
+//   LstmStepFloat(
+//       input_data, tflite::testing::kInputGateParameters.activation_weight,
+//       tflite::testing::kForgetGateParameters.activation_weight,
+//       tflite::testing::kModulationGateParameters.activation_weight,
+//       tflite::testing::kOutputGateParameters.activation_weight,
+//       /*aux_input_ptr=*/nullptr, /*aux_input_to_input_weights_ptr=*/nullptr,
+//       /*aux_input_to_forget_weights_ptr=*/nullptr,
+//       /*aux_input_to_cell_weights_ptr=*/nullptr,
+//       /*aux_input_to_output_weights_ptr=*/nullptr,
+//       tflite::testing::kInputGateParameters.recurrent_weight,
+//       tflite::testing::kForgetGateParameters.recurrent_weight,
+//       tflite::testing::kModulationGateParameters.recurrent_weight,
+//       tflite::testing::kOutputGateParameters.recurrent_weight,
+//       /*cell_to_input_weights_ptr=*/nullptr,
+//       /*cell_to_forget_weights_ptr=*/nullptr,
+//       /*cell_to_output_weights_ptr=*/nullptr,
+//       /*input_layer_norm_coefficients_ptr=*/nullptr,
+//       /*forget_layer_norm_coefficients_ptr=*/nullptr,
+//       /*cell_layer_norm_coefficients_ptr=*/nullptr,
+//       /*output_layer_norm_coefficients_ptr=*/nullptr,
+//       tflite::testing::kInputGateParameters.fused_bias,
+//       tflite::testing::kForgetGateParameters.fused_bias,
+//       tflite::testing::kModulationGateParameters.fused_bias,
+//       tflite::testing::kOutputGateParameters.fused_bias, ,
+//       /*projection_weights_ptr=*/nullptr, /*projection_bias_ptr=*/nullptr,
+//       const TfLiteLSTMParams* params, tflite::testing::kBatchSize,
+//       tflite::testing::kStateDimension, tflite::testing::kInputDimension,
+//       tflite::testing::kInputDimension, tflite::testing::kStateDimension,
+//       /*output_batch_leading_dim=*/0, recurrent_state, cell_state,
+//       input_gate_scratch forget_gate_scratch, cell_gate_scratch,
+//       output_gate_scratch, output)
+// }
 TF_LITE_MICRO_TESTS_END
