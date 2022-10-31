ffb8adc8912c7585f63a24447b6d2eca23eae524
Renjie Wu <rewu@google.com>
2022-10-31 13:55:46 -0700

    xtensa!!!

--- tensorflow/lite/micro/kernels/lstm_eval_test.cc
+++ tensorflow/lite/micro/kernels/lstm_eval_test.cc
@@ -707,8 +707,12 @@ const QuantizedModelContents<int8_t, int8_t, int32_t, int16_t>
 }  // namespace
 }  // namespace testing
 }  // namespace tflite
+#endif  // !defined(XTENSA)
 
 TF_LITE_MICRO_TESTS_BEGIN
+// TODO(b/230666079) enable below tests for xtensa when the xtensa
+// kernel is reconciled with reference kernel
+#if !defined(XTENSA)
 TF_LITE_MICRO_TEST(CheckGateOutputFloat) {
   // Forget gate
   tflite::testing::TestGateOutputFloat(
@@ -996,5 +1000,5 @@ TF_LITE_MICRO_TEST(TestLSTMEval) {
       tflite::testing::kExpectedOutput, float_model_contents.GetOutput(),
       tflite::testing::kOutputSize, tflite::testing::kTestFloatTolerance);
 }
-TF_LITE_MICRO_TESTS_END
 #endif  // !defined(XTENSA)
+TF_LITE_MICRO_TESTS_END
