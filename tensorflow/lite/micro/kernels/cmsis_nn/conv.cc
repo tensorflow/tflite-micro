9ff0689c5a96aab5c04503422ae4dc0311c740fc
Steven Toribio <toribiosteven@google.com>
2022-11-22 18:47:44 +0000

    formarting:

--- tensorflow/lite/micro/kernels/cmsis_nn/conv.cc
+++ tensorflow/lite/micro/kernels/cmsis_nn/conv.cc
@@ -455,7 +455,7 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
               tflite::micro::GetOptionalTensorData<int32_t>(bias),
               tflite::micro::GetTensorShape(output),
               tflite::micro::GetTensorData<int8_t>(output));
-              return kTfLiteOk;
+          return kTfLiteOk;
         }
         default: {
           MicroPrintf("Filter type %s (%d) not supported.",
