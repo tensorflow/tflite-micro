4e9076de3ebff4aa93e3c81e6a76938b0fd033b6
Måns Nilsson <mans.nilsson@arm.com>
2022-12-15 17:30:28 +0100

    Add option to verify generated micro mutable list
    If operator in model is not supported by TFLM script should fail with
    new option.
    
    Change-Id: Id03bf53188ba4c75f80243b3bb111661a388cbf5

--- tensorflow/lite/micro/tools/gen_micro_mutable_op_resolver/generate_micro_mutable_op_resolver_from_model.py
+++ tensorflow/lite/micro/tools/gen_micro_mutable_op_resolver/generate_micro_mutable_op_resolver_from_model.py
@@ -29,13 +29,21 @@ TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 TEMPLATE_DIR = os.path.abspath(TEMPLATE_DIR)
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string('common_tflite_path', None, 'Common path to tflite files. This need to be an absolute path.'
-                    'This would typically be the path to the directory where the models reside.')
-flags.DEFINE_list('input_tflite_files', None,
-                  'Relative path name list of the input TFLite files.'
-                  'This would be relative to the common path.'
-                  'This would typically be the name(s) of the tflite file(s).')
-flags.DEFINE_string('output_dir', None, 'directory to output generated files')
+flags.DEFINE_string(
+    'common_tflite_path', None,
+    'Common path to tflite files. This need to be an absolute path.'
+    'This would typically be the path to the directory where the models reside.'
+)
+flags.DEFINE_list(
+    'input_tflite_files', None,
+    'Relative path name list of the input TFLite files.'
+    'This would be relative to the common path.'
+    'This would typically be the name(s) of the tflite file(s).')
+flags.DEFINE_string('output_dir', None, 'Directory to output generated files.')
+flags.DEFINE_string(
+    'verify_op_list_against_header', None,
+    'Take micro_mutable_op_resolver.h as input and verifies that all generated operator calls are there.'
+)
 
 flags.mark_flag_as_required('common_tflite_path')
 flags.mark_flag_as_required('input_tflite_files')
@@ -113,6 +121,24 @@ def GetModelOperatorsAndActivation(model_path):
   return operators_and_activations
 
 
+def VerifyOpList(op_list, header):
+  """Make sure operators in list are not missing in header file ."""
+
+  supported_op_list = []
+  with open(header, 'r') as f:
+    for l in f.readlines():
+      if "TfLiteStatus Add" in l:
+        op = l.strip().split(' ')[1].split('(')[0]
+        supported_op_list.append(op)
+
+  for op in op_list:
+    if op not in supported_op_list:
+      print(f'{op} not supported by TFLM')
+      return True
+
+  return False
+
+
 def main(_):
   model_names = []
   final_operator_list = []
@@ -142,9 +168,14 @@ def main(_):
       if operator not in final_operator_list
   ]
 
+  if FLAGS.verify_op_list_against_header and VerifyOpList(
+      final_operator_list, FLAGS.verify_op_list_against_header):
+    return True
+
   os.makedirs(FLAGS.output_dir, exist_ok=True)
   GenerateMicroMutableOpsResolverHeaderFile(final_operator_list, model_name,
                                             FLAGS.output_dir)
+  return False
 
 
 if __name__ == '__main__':
