7cc2076d83b560d0e340f9b2bcbb2f63f29df500
Advait Jain <advaitjain@google.com>
2023-01-05 14:18:01 -0800

    bring back hifimini svdf that was removed with https://github.com/tensorflow/tflite-micro/pull/790

--- tensorflow/lite/micro/kernels/xtensa/xtensa.h
+++ tensorflow/lite/micro/kernels/xtensa/xtensa.h
@@ -16,6 +16,10 @@ limitations under the License.
 #ifndef TENSORFLOW_LITE_MICRO_KERNELS_XTENSA_XTENSA_H_
 #define TENSORFLOW_LITE_MICRO_KERNELS_XTENSA_XTENSA_H_
 
+#if defined(HIFMINI)
+#include <xtensa/tie/xt_hifi2.h>
+#endif  // defined(HIFMINI)
+
 #if defined(HIFI4) || defined(HIFI5)
 #include "include/nnlib/xa_nnlib_api.h"
 #include "include/nnlib/xa_nnlib_standards.h"
@@ -23,9 +27,11 @@ limitations under the License.
 #define ALIGNED_SIZE(x, bytes) (((x) + (bytes - 1)) & (~(bytes - 1)))
 #define ALIGN_PTR(x, bytes) ((((unsigned)(x)) + (bytes - 1)) & (~(bytes - 1)))
 #endif  // defined(HIFI4) || defined(HIFI5)
+
+
 #if defined(VISION_P6)
 #include "utils.h"
 #include "vision_api.h"
-#endif  // VISION_P6
+#endif  // defined(VISION_P6)
 
 #endif  // TENSORFLOW_LITE_MICRO_KERNELS_XTENSA_XTENSA_H_
