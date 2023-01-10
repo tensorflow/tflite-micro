36c01fa9da90d3df65f6b1f5bd34ee51fd069962
Måns Nilsson <mans.nilsson@arm.com>
2023-01-10 19:15:26 +0100

    Fix gen micro mutable script
    Solves LSTM operator corner case.
    
    BUG=script not working with LSTM operator

--- tensorflow/lite/micro/tools/gen_micro_mutable_op_resolver/generate_micro_mutable_op_resolver_from_model.py
+++ tensorflow/lite/micro/tools/gen_micro_mutable_op_resolver/generate_micro_mutable_op_resolver_from_model.py
@@ -1,4 +1,4 @@
-# Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2023 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -68,6 +68,10 @@ def ParseString(word):
         formated_op_string += part.upper()
     else:
       formated_op_string += part.upper()
+
+  # Edge case for AddUnidirectionalSequenceLSTM().
+  formated_op_string = formated_op_string.replace('Lstm', 'LSTM')
+
   return 'Add' + formated_op_string
 
 
