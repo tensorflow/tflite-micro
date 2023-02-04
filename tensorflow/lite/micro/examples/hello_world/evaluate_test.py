b2a9a0f7a26f7b461c3afac7f3d69a41920c9274
Tanmay Das <dtanmay@google.com>
2023-02-04 13:27:27 +0000

    Important changes
    - evaluate.py and evaluate_test.py is now using floating point model.
    
    - Have the option to run tflite interpreter as well using use_tflite flag.
    
    - By default the hello_world_no_quant.tflite model is run; no flag is required.
    
    - Updated the README file accordingly.

--- tensorflow/lite/micro/examples/hello_world/evaluate_test.py
+++ tensorflow/lite/micro/examples/hello_world/evaluate_test.py
@@ -14,7 +14,6 @@
 
 import os
 import numpy as np
-import tensorflow as tf
 
 from tensorflow.python.framework import test_util
 from tensorflow.python.platform import resource_loader
@@ -27,7 +26,7 @@ PREFIX_PATH = resource_loader.get_path_to_datafile('')
 
 
 class HelloWorldQuantModelTest(test_util.TensorFlowTestCase):
-  model_path = os.path.join(PREFIX_PATH, 'hello_world.tflite')
+  model_path = os.path.join(PREFIX_PATH, 'hello_world_no_quant.tflite')
   input_shape = (1, 1)
   output_shape = (1, 1)
   # Create the tflm interpreter
@@ -40,11 +39,12 @@ class HelloWorldQuantModelTest(test_util.TensorFlowTestCase):
 
   def test_input(self):
     input_details = self.tflm_interpreter.get_input_details(0)
-    input_scale, input_zero_point = evaluate.get_metadata(input_details)
+    input_scale, input_zero_point = evaluate.get_quantization_params(
+        input_details)
 
     self.assertAllEqual(input_details['shape'], self.input_shape)
-    self.assertEqual(input_details['dtype'], np.int8)
-    self.assertEqual(len(input_scale), 1)
+    self.assertEqual(input_details['dtype'], np.float32)
+    self.assertEqual(len(input_scale), 0)
     self.assertEqual(
         input_details['quantization_parameters']['quantized_dimension'], 0)
     self.assertEqual(input_scale.dtype, np.float32)
@@ -52,56 +52,43 @@ class HelloWorldQuantModelTest(test_util.TensorFlowTestCase):
 
   def test_output(self):
     output_details = self.tflm_interpreter.get_output_details(0)
-    output_scale, output_zero_point = evaluate.get_metadata(output_details)
+    output_scale, output_zero_point = evaluate.get_quantization_params(
+        output_details)
 
     self.assertAllEqual(output_details['shape'], self.output_shape)
-    self.assertEqual(output_details['dtype'], np.int8)
-    self.assertEqual(len(output_scale), 1)
+    self.assertEqual(output_details['dtype'], np.float32)
+    self.assertEqual(len(output_scale), 0)
     self.assertEqual(
         output_details['quantization_parameters']['quantized_dimension'], 0)
     self.assertEqual(output_scale.dtype, np.float32)
     self.assertEqual(output_zero_point.dtype, np.int32)
 
   def test_interpreter_prediction(self):
-    x_value = 0.0
+    x_value = np.float32(0.0)
     # Calculate the corresponding sine values
     y_true = np.sin(x_value).astype(np.float32)
 
-    input_details = self.tflm_interpreter.get_input_details(0)
-    input_scale, input_zero_point = evaluate.get_metadata(input_details)
+    input_shape = np.array(
+        self.tflm_interpreter.get_input_details(0).get('shape'))
 
-    output_details = self.tflm_interpreter.get_output_details(0)
-    output_scale, output_zero_point = evaluate.get_metadata(output_details)
-
-    input_shape = np.array(input_details.get('shape'))
-
-    x_quantized = np.int8((x_value / input_scale[0]) + input_zero_point[0])
-    y_quantized = evaluate.invoke_tflm_interpreter(input_shape,
-                                                   self.tflm_interpreter,
-                                                   x_quantized,
-                                                   input_index=0,
-                                                   output_index=0)
-    y_pred = float((y_quantized - output_zero_point[0]) * output_scale[0])
+    y_pred = evaluate.invoke_tflm_interpreter(
+        input_shape,
+        self.tflm_interpreter,
+        x_value,
+        input_index=0,
+        output_index=0)
     epsilon = 0.05
     self.assertNear(
         y_true, y_pred, epsilon,
         'hello_world model prediction is not close enough to numpy.sin value')
 
   def test_compare_with_tflite(self):
-    # TFLite interpreter
-    tflite_interpreter = tf.lite.Interpreter(
-        model_path=self.model_path,
-        experimental_op_resolver_type= \
-          tf.lite.experimental.OpResolverType.BUILTIN_REF)
-    tflite_interpreter.allocate_tensors()
-
     x_values = evaluate.generate_random_input()
 
-    tflm_y_predictions = evaluate.get_tflm_prediction(self.tflm_interpreter,
-                                                      x_values)
+    tflm_y_predictions = evaluate.get_tflm_prediction(self.model_path, x_values)
 
     tflite_y_predictions = evaluate.get_tflite_prediction(
-        tflite_interpreter, x_values)
+        self.model_path, x_values)
 
     self.assertAllEqual(tflm_y_predictions, tflite_y_predictions)
 
