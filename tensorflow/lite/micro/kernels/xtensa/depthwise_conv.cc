1dadf08ba1b33c3b0ad5a8694a65733178491e3a
Steven Toribio <34755817+turbotoribio@users.noreply.github.com>
2023-02-09 10:28:48 -0800

    Refactors unpacking outside of deptwise_conv and fully_connected reference kernel (#1714)
    -Refactors unpacking outside of deptwise_conv and fully_connected reference kernel for 4bit test case
    -allows 4 bit filters to be used with optimized kernels in TfLite for depthwise_conv and fully_connected ops
    -test for correctness by running fully_connected and depthwise_conv  in TfLite and TFLM
    
    Corresponding g3 cl - [cl/504384976](https://critique.corp.google.com/cl/504384976)
    BUG=[265045979](https://b.corp.google.com/issues/265045979)

--- tensorflow/lite/micro/kernels/xtensa/depthwise_conv.cc
+++ tensorflow/lite/micro/kernels/xtensa/depthwise_conv.cc
@@ -19,6 +19,7 @@ limitations under the License.
 #include "tensorflow/lite/c/builtin_op_data.h"
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/kernels/internal/common.h"
+#include "tensorflow/lite/kernels/internal/portable_tensor_utils.h"
 #include "tensorflow/lite/kernels/internal/quantization_util.h"
 #include "tensorflow/lite/kernels/internal/reference/depthwiseconv_float.h"
 #include "tensorflow/lite/kernels/internal/reference/depthwiseconv_uint8.h"
@@ -78,16 +79,33 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
           ? tflite::micro::GetEvalInput(context, node, kDepthwiseConvBiasTensor)
           : nullptr;
 
+  TfLiteEvalTensor filter_int8;
+
+  if (filter->type == kTfLiteInt4) {
+    filter_int8.data.data = static_cast<int8_t*>(context->GetScratchBuffer(
+        context, op_data.reference_op_data.filter_buffer_index));
+
+    filter_int8.dims = filter->dims;
+    filter_int8.type = kTfLiteInt8;
+    tflite::tensor_utils::UnpackDenseInt4IntoInt8(
+        tflite::micro::GetTensorData<int8_t>(filter),
+        tflite::micro::GetTensorShape(filter).FlatSize(),
+        tflite::micro::GetTensorData<int8_t>(&filter_int8));
+
+  } else {
+    filter_int8 = *filter;
+  }
+
   switch (input->type) {  // Already know in/out types are same.
     case kTfLiteInt8: {
-      switch (filter->type) {
+      switch (filter_int8.type) {
         case kTfLiteInt8: {
 #if defined(HIFI4) || defined(HIFI5)
-          DepthwiseConvEvalHifi(context, node, params, op_data, input, filter,
-                                bias, output);
+          DepthwiseConvEvalHifi(context, node, params, op_data, input,
+                                &filter_int8, bias, output);
 #elif defined(VISION_P6)
-          DepthwiseConvEvalVision(context, node, params, op_data, input, filter,
-                                  bias, output);
+          DepthwiseConvEvalVision(context, node, params, op_data, input,
+                                  &filter_int8, bias, output);
 #else
           reference_integer_ops::DepthwiseConvPerChannel(
               DepthwiseConvParamsQuantized(params, op_data.reference_op_data),
@@ -96,30 +114,12 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
               tflite::micro::GetTensorShape(input),
               tflite::micro::GetTensorData<int8_t>(input),
               tflite::micro::GetTensorShape(filter),
-              tflite::micro::GetTensorData<int8_t>(filter),
+              tflite::micro::GetTensorData<int8_t>(&filter_int8),
               tflite::micro::GetTensorShape(bias),
-              tflite::micro::GetTensorData<int32_t>(bias),
-              tflite::micro::GetTensorShape(output),
-              tflite::micro::GetTensorData<int8_t>(output));
-#endif  // defined(HIFI4) || defined(HIFI5)
-          break;
-        }
-        case kTfLiteInt4: {
-          int8_t* unpacked_filter_data =
-              static_cast<int8_t*>(context->GetScratchBuffer(
-                  context, op_data.reference_op_data.filter_buffer_index));
-          reference_integer_ops::DepthwiseConvPerChannelWithPackedInt4Weights(
-              DepthwiseConvParamsQuantized(params, op_data.reference_op_data),
-              op_data.reference_op_data.per_channel_output_multiplier,
-              op_data.reference_op_data.per_channel_output_shift,
-              tflite::micro::GetTensorShape(input),
-              tflite::micro::GetTensorData<int8_t>(input),
-              tflite::micro::GetTensorShape(filter),
-              tflite::micro::GetTensorData<int8_t>(filter),
-              unpacked_filter_data, tflite::micro::GetTensorShape(bias),
               tflite::micro::GetOptionalTensorData<int32_t>(bias),
               tflite::micro::GetTensorShape(output),
               tflite::micro::GetTensorData<int8_t>(output));
+#endif  // defined(HIFI4) || defined(HIFI5)
           break;
         }
         default:
