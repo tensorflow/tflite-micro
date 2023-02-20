f88632dc2d4d54a3ba25b52fbbe6f400e790d203
Renjie Wu <rewu@google.com>
2023-02-20 11:43:06 -0800

    print the scale information for the test

--- tensorflow/lite/micro/kernels/testdata/lstm_test_data_generator.py
+++ tensorflow/lite/micro/kernels/testdata/lstm_test_data_generator.py
@@ -1,3 +1,18 @@
+# Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+
 import tensorflow as tf
 from absl import app
 import numpy as np
@@ -59,16 +74,30 @@ _MULTISTEP_TEST_DATA = {
 }
 
 
-def one_step():
-  lstm_debugger = QuantizedLSTMDebugger(
-      _KERNEL_CONFIG,
-      _KERNEL_PARAMETERS,
-      _GATE_TEST_DATA['init_hidden_state_vals'],
-      _GATE_TEST_DATA['hidden_state_range'],
-      _GATE_TEST_DATA['init_cell_state_vals'],
-      _GATE_TEST_DATA['cell_state_range'],
-  )
+def print_tensor_quantization_params(tensor_name, tensor):
+  print(f"{tensor_name}, scale: {tensor.scale}, zero_point:"
+        f" {tensor.zero_point}")
 
+
+def print_gate_tensor_params(gate_name, gate):
+  print(f"###### Quantization settings for {gate_name} ######")
+  print_tensor_quantization_params("activation weight", gate.activation_weight)
+  print_tensor_quantization_params("recurrent weight", gate.activation_weight)
+
+
+def print_quantization_settings(lstm_debugger):
+  print_gate_tensor_params("forget gate", lstm_debugger.forget_gate_params)
+  print_gate_tensor_params("input gate", lstm_debugger.input_gate_params)
+  print_gate_tensor_params("cell gate", lstm_debugger.modulation_gate_params)
+  print_gate_tensor_params("output gate", lstm_debugger.output_gate_params)
+  print("###### State Tensors ######")
+  print_tensor_quantization_params("Hidden State Tensor",
+                                   lstm_debugger.hidden_state_tensor)
+  print_tensor_quantization_params("Cell State Tensor",
+                                   lstm_debugger.cell_state_tensor)
+
+
+def print_one_step(lstm_debugger):
   test_data = np.array(_GATE_TEST_DATA['input_data']).reshape((-1, 1))
   input_data_range = _GATE_TEST_DATA['input_data_range']
   input_tensor = assemble_quantized_tensor(test_data, input_data_range[0],
@@ -76,8 +105,45 @@ def one_step():
   lstm_debugger.invoke(input_tensor, debug=True)
 
 
-def multi_step():
-  lstm_debugger = QuantizedLSTMDebugger(
+def print_multi_step(lstm_debugger, debug=False):
+  input_data = _MULTISTEP_TEST_DATA['input_data']
+  input_data_range = _MULTISTEP_TEST_DATA['input_data_range']
+  input_data_size = _KERNEL_CONFIG['shape_info']['input_dim']
+  input_start_pos = 0
+  steps = 0
+  while input_start_pos < len(input_data):
+    one_step_data = np.array(input_data[input_start_pos:input_start_pos +
+                                        input_data_size]).reshape((-1, 1))
+    input_tensor = assemble_quantized_tensor(one_step_data,
+                                             input_data_range[0],
+                                             input_data_range[1], False)
+    output_quant, output_float = lstm_debugger.invoke(input_tensor,
+                                                      debug=debug)
+    print(f"##### Step: {steps} #####")
+    print(f"Quantized Output: {output_quant.flatten()}")
+    print(
+        f"Dequantized Output: {lstm_debugger.hidden_state_tensor.dequantized_data.flatten().flatten()}"
+    )
+    print(f"Float Output: {output_float.flatten()}")
+    input_start_pos += input_data_size
+    steps += 1
+
+
+def main(_):
+  one_step_lstm_debugger = QuantizedLSTMDebugger(
+      _KERNEL_CONFIG,
+      _KERNEL_PARAMETERS,
+      _GATE_TEST_DATA['init_hidden_state_vals'],
+      _GATE_TEST_DATA['hidden_state_range'],
+      _GATE_TEST_DATA['init_cell_state_vals'],
+      _GATE_TEST_DATA['cell_state_range'],
+  )
+  print("========== Quantization Settings for the Test Kernal ========== ")
+  print_quantization_settings(one_step_lstm_debugger)
+  print("========== Single Step Invocation Intermediates  ========== ")
+  print_one_step(one_step_lstm_debugger)
+
+  multi_step_lstm_debugger = QuantizedLSTMDebugger(
       _KERNEL_CONFIG,
       _KERNEL_PARAMETERS,
       _MULTISTEP_TEST_DATA['init_hidden_state_vals'],
@@ -85,23 +151,8 @@ def multi_step():
       _MULTISTEP_TEST_DATA['init_cell_state_vals'],
       _MULTISTEP_TEST_DATA['cell_state_range'],
   )
-  input_data = _MULTISTEP_TEST_DATA['input_data']
-  input_data_range = _MULTISTEP_TEST_DATA['input_data_range']
-  input_data_size = _KERNEL_CONFIG['shape_info']['input_dim']
-  input_start_pos = 0
-  while input_start_pos < len(input_data):
-    one_step_data = np.array(input_data[input_start_pos:input_start_pos +
-                                        input_data_size]).reshape((-1, 1))
-    input_tensor = assemble_quantized_tensor(one_step_data,
-                                             input_data_range[0],
-                                             input_data_range[1], False)
-    lstm_debugger.invoke(input_tensor, debug=True)
-    input_start_pos += input_data_size
-
-
-def main(_):
-  one_step()
-  multi_step()
+  print("========== Multi Step Invocation Intermediates  ========== ")
+  print_multi_step(multi_step_lstm_debugger)
 
 
 if __name__ == "__main__":
