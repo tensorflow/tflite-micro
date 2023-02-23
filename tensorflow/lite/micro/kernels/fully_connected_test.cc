a5ef12c8589ca5ff45644f89bad6d7523e0f02c1
Steven Toribio <toribiosteven@google.com>
2023-02-23 00:23:34 +0000

    fixing merge conflict

--- tensorflow/lite/micro/kernels/fully_connected_test.cc
+++ tensorflow/lite/micro/kernels/fully_connected_test.cc
@@ -42,11 +42,8 @@ const float simple_weights_data[] = {
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10,  // u = 1
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10,  // u = 2
 };
-// TODO(b/258710417): INT4 isn't currently supported on Hexagon.
-// TODO(b/265349042): INT4 isn't currently supported on Hifimini.
-// TODO(b/268384678): xtensa vision p6 kernels break
-// this test, will if def till properly investigated.
-#if !defined(HEXAGON) && !defined(HIFIMINI)
+
+#if !defined(HEXAGON)
 const float simple_int4_weights_data[] = {
     -2, -1, 0, 1, 2, 3, 4, 5, 6, 7,  // u = 0
     -2, -1, 0, 1, 2, 3, 4, 5, 6, 7,  // u = 1
@@ -635,11 +632,7 @@ TF_LITE_MICRO_TEST(SimpleTestQuantizedInt8NullBias) {
       kTfLiteOk);
 }
 
-// TODO(b/258710417): INT4 isn't currently supported on Hexagon.
-// TODO(b/265349042): INT4 isn't currently supported on Hifimini.
-// TODO(b/268384678): xtensa vision p6 kernels break
-// this test, will if def till properly investigated.
-#if !defined(HEXAGON) && !defined(HIFIMINI)
+#if !defined(HEXAGON)
 // This test was created by handcrafting simple_int4_weights_data, and
 // simple_golden_null_bias_int4_weights was obtained by running
 // TestFullyConnectedQuantized() with int8 quantization, and ensuring that int4
