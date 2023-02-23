ab9f8bcdf6f64d11df2f637d58f7c07c45cd27a8
Tanmay Das <dtanmay@google.com>
2023-02-23 03:09:20 +0000

    Added train.py script to create, train and optimize the model.

---
+++ tensorflow/lite/micro/examples/hello_world/train.py
@@ -0,0 +1,197 @@
+# Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+"""hellow_world model training for sinwave recognition
+
+Run:
+`bazel build tensorflow/lite/micro/examples/hello_world:train`
+`bazel-bin/tensorflow/lite/micro/examples/hello_world/train --quantize --save_tf_model --save_dir=/tmp/model_created/`
+"""
+import math
+import os
+
+from absl import app
+from absl import flags
+from absl import logging
+import numpy as np
+import tensorflow as tf
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_integer("epochs", 500, "number of epochs to train the model.")
+flags.DEFINE_string("save_dir", "/tmp/hello_world_models",
+                    "the directory to save the trained model.")
+flags.DEFINE_boolean("save_tf_model", False,
+                     "store the original unconverted tf model.")
+flags.DEFINE_boolean(
+    "quantize", False,
+    "convert and save the full integer (int8) quantized model.")
+
+def get_train_data():
+  """
+  ### 1. Generate Data
+  The code in the following cell will generate a set of random `x` values,
+  calculate their sine values, and display them on a graph.
+
+  Throws: AssertionError
+  """
+  # Generate a uniformly distributed set of random numbers in the range from
+  # 0 to 2π, which covers a complete sine wave oscillation
+  x_values = np.random.uniform(low=0, high=2 * math.pi,
+                               size=1000).astype(np.float32)
+
+  # Shuffle the values to guarantee they're not in order
+  np.random.shuffle(x_values)
+
+  # We'll use 60% of our data for training
+  train_split = int(0.6 * 1000)
+  test_split = int(0.2 * 1000 + train_split)
+
+  # Use np.split to chop our data into three parts.
+  # The second argument to np.split is an array of indices where the data will
+  # be split. We provide two indices, so the data will be divided into three
+  # chunks.
+  splits = np.split(x_values, [train_split, test_split])
+  x_train, x_test, x_validate = splits[0], splits[1], splits[2]
+  y_train, y_test, y_validate = splits[0], splits[1], splits[2]
+
+  return (x_train, y_train)
+
+def create_model() -> tf.keras.Model:
+  """
+  ## Training
+  ## Training a Larger Model
+
+  ### 1. Design the Model
+  To make our model bigger, let's add an additional layer of neurons.
+  The following cell redefines our model in the same way as earlier, but with
+  16 neurons in the first layer and an additional layer of 16 neurons in the
+  middle:
+  """
+
+  model = tf.keras.Sequential()
+
+  # First layer takes a scalar input and feeds it through 16 "neurons". The
+  # neurons decide whether to activate based on the 'relu' activation function.
+  model.add(tf.keras.layers.Dense(16, activation='relu', input_shape=(1, )))
+
+  # The new second and third layer will help the network learn more complex
+  # representations
+  model.add(tf.keras.layers.Dense(16, activation='relu'))
+
+  # Final layer is a single neuron, since we want to output a single value
+  model.add(tf.keras.layers.Dense(1))
+
+  # Compile the model using the standard 'adam' optimizer and the mean squared
+  # error or 'mse' loss function for regression.
+  model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+
+  return model
+
+def convert_tflite_model(model):
+  """Convert the save TF model to tflite model, then save it as .tflite flatbuffer format
+    Args:
+        model (tf.keras.Model): the trained hello_world Model
+    Returns:
+        The converted model in serialized format.
+  """
+  converter = tf.lite.TFLiteConverter.from_keras_model(model)
+  tflite_model = converter.convert()
+  return tflite_model
+
+
+def save_tflite_model(tflite_model, save_dir, model_name):
+  """save the converted tflite model
+  Args:
+      tflite_model (binary): the converted model in serialized format.
+      save_dir (str): the save directory
+      model_name (str): model name to be saved
+  """
+  if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+  save_path = os.path.join(save_dir, model_name)
+  with open(save_path, "wb") as f:
+    f.write(tflite_model)
+  logging.info("Tflite model saved to %s", save_dir)
+
+
+def train_model(epochs, x_train, y_train):
+  """Train keras hello_world model
+    Args: epochs (int) : number of epochs to train the model
+        x_train (numpy.array): list of the training data
+        y_train (numpy.array): list of the corresponding array
+    Returns:
+        tf.keras.Model: A trained keras hello_world model
+  """
+  model = create_model()
+  model.fit(x_train,
+            y_train,
+            epochs=epochs,
+            validation_split=0.2,
+            batch_size=64)
+
+  if FLAGS.save_tf_model:
+    model.save(FLAGS.save_dir, save_format="tf")
+    logging.info("TF model saved to %s", FLAGS.save_dir)
+
+  return model
+
+def convert_quantized_tflite_model(model, x_train):
+  """Convert the save TF model to tflite model, then save it as .tflite flatbuffer format
+
+    See
+    https://www.tensorflow.org/lite/performance/post_training_integer_quant#convert_using_integer-only_quantization
+
+    Args:
+        model (tf.keras.Model): the trained hello_world Model
+        x_train (numpy.array): list of the training data
+
+    Returns:
+        The converted model in serialized format.
+  """
+  # Convert the model to the TensorFlow Lite format with quantization
+  def representative_dataset(num_samples=500):
+    for i in range(num_samples):
+      yield [x_train[i].reshape(1, 1)]
+
+  converter = tf.lite.TFLiteConverter.from_keras_model(model)
+  converter.optimizations = [tf.lite.Optimize.DEFAULT]
+  converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+  converter.inference_input_type = tf.int8
+  converter.inference_output_type = tf.int8
+  converter.representative_dataset = representative_dataset
+  tflite_model = converter.convert()
+  return tflite_model
+
+def main(_):
+  x_train, y_train = get_train_data()
+  trained_model = train_model(FLAGS.epochs, x_train, y_train)
+
+  # Convert and save the model to .tflite
+  tflite_model = convert_tflite_model(trained_model)
+  save_tflite_model(tflite_model,
+                    FLAGS.save_dir,
+                    model_name="hello_world_float.tflite")
+
+  # Convert and save the quantized model
+  if FLAGS.quantize:
+    quantized_tflite_model = convert_quantized_tflite_model(trained_model,
+                                                            x_train)
+    save_tflite_model(quantized_tflite_model,
+                      FLAGS.save_dir,
+                      model_name="hello_world_int8.tflite")
+
+
+if __name__ == "__main__":
+  app.run(main)
