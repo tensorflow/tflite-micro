4085b33d842842fbf323d2cecee3abc475601e47
Tanmay Das <dtanmay@google.com>
2023-02-27 22:21:24 +0000

    Fix linting

--- tensorflow/lite/micro/examples/hello_world/main.cc
+++ tensorflow/lite/micro/examples/hello_world/main.cc
@@ -10,6 +10,4 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-int main(int argc, char* argv[]) {
-
-}
+int main(int argc, char* argv[]) {}
