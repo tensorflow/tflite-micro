207dde4da9e3263c0b868a6fbd1bf2f7f83b5770
Steven Toribio <34755817+turbotoribio@users.noreply.github.com>
2023-05-12 11:29:53 -0700

    adds PythonOpsResolver  (#1965)
    This PR does the following: 
    
    1. Introduces PythonOpsResolver Target 
    2. removes a reference to AllOpsResolver in remaing places in TFLM repo outside of integration_tests
    
    [doc for changes](https://docs.google.com/document/d/1vafCGaFGGBauAssAdFABtmgpiRju2V3isioZfLMgeEc/edit?resourcekey=0-pc-aJl3TPZ7NcO9RxrNsrA#)
    
    [google3 cl](https://critique.corp.google.com/cl/531013669)
    
    BUG=[b/276918689](https://b.corp.google.com/issues/276918689)

--- tensorflow/lite/micro/test_helpers.cc
+++ tensorflow/lite/micro/test_helpers.cc
@@ -26,7 +26,6 @@ limitations under the License.
 #include "tensorflow/lite/kernels/internal/compatibility.h"
 #include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
 #include "tensorflow/lite/kernels/kernel_util.h"
-#include "tensorflow/lite/micro/all_ops_resolver.h"
 #include "tensorflow/lite/micro/kernels/kernel_util.h"
 #include "tensorflow/lite/micro/memory_helpers.h"
 #include "tensorflow/lite/micro/micro_arena_constants.h"
