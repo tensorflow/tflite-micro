a5ef6a115b7d0c252719509e4b17ad16e8a605a6
Steven Toribio <toribiosteven@google.com>
2023-05-18 19:46:31 +0000

    sync g3

---
+++ tensorflow/lite/micro/micro_common.h
@@ -0,0 +1,19 @@
+#ifndef THIRD_PARTY_TFLITE_MICRO_TENSORFLOW_LITE_MICRO_MICRO_COMMON_H_
+#define THIRD_PARTY_TFLITE_MICRO_TENSORFLOW_LITE_MICRO_MICRO_COMMON_H_
+
+#include "tensorflow/lite/c/common.h"
+
+// TFLMRegistration defines the API that TFLM kernels need to implement.
+// This will be replacing the current TfLiteRegistration_V1 struct with 
+// something more compatible Embedded enviroment TFLM is used in. 
+struct TFLMRegistration {
+  void* (*init)(TfLiteContext* context, const char* buffer, size_t length);
+  void (*free)(TfLiteContext* context, void* buffer);
+  TfLiteStatus (*prepare)(TfLiteContext* context, TfLiteNode* node);
+  TfLiteStatus (*invoke)(TfLiteContext* context, TfLiteNode* node);
+  void (*reset)(TfLiteContext* context, void* buffer);
+  int32_t builtin_code;
+  const char* custom_name;
+};
+
+#endif  // THIRD_PARTY_TFLITE_MICRO_TENSORFLOW_LITE_MICRO_MICRO_COMMON_H_
