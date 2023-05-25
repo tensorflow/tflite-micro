6128af793b60bb78123762a3ac50da6d7123f580
Tanmay Das <dtanmay@google.com>
2023-05-25 21:14:33 +0000

    Revert change.

--- tensorflow/lite/core/c/common.h
+++ tensorflow/lite/core/c/common.h
@@ -1014,7 +1014,7 @@ typedef struct TfLiteRegistration_V2 {
 // type.
 // WARNING: This structure is deprecated / not an official part of the
 // API. It should be only used for binary backward compatibility.
-typedef struct TfLiteRegistration_V1 {
+typedef struct TFLMRegistration {
   void* (*init)(TfLiteContext* context, const char* buffer, size_t length);
   void (*free)(TfLiteContext* context, void* buffer);
   TfLiteStatus (*prepare)(TfLiteContext* context, TfLiteNode* node);
@@ -1024,7 +1024,7 @@ typedef struct TfLiteRegistration_V1 {
   int32_t builtin_code;
   const char* custom_name;
   int version;
-} TfLiteRegistration_V1;
+} TFLMRegistration;
 
 // The flags used in `TfLiteDelegate`. Note that this is a bitmask, so the
 // values should be 1, 2, 4, 8, ...etc.
