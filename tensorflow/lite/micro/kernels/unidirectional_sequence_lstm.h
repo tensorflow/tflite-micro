50b8e5dd3c814681af54eb5aa2e9ae9ada872882
ddavis-2015 <ddavis@bdti.com>
2023-05-28 14:58:59 -0700

    Updated to use new registration.

--- tensorflow/lite/micro/kernels/unidirectional_sequence_lstm.h
+++ tensorflow/lite/micro/kernels/unidirectional_sequence_lstm.h
@@ -21,6 +21,7 @@ limitations under the License.
 #include "tensorflow/lite/c/builtin_op_data.h"
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/kernels/internal/types.h"
+#include "tensorflow/lite/micro/micro_common.h"
 
 namespace tflite {
 
