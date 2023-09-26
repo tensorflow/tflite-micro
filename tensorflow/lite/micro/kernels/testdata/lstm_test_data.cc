0fb58056fdee83f0f3691107b977f20b9361ca2f
Advait Jain <advaitjain@users.noreply.github.com>
2023-09-26 08:57:01 -0700

    Restore cell_state scale changed with #2171 (#2245)
    See http://b/296130372#comment3 for additional context.
    
    BUG=http://b/296130372

--- tensorflow/lite/micro/kernels/testdata/lstm_test_data.cc
+++ tensorflow/lite/micro/kernels/testdata/lstm_test_data.cc
@@ -257,8 +257,7 @@ NodeQuantizationParameters Get2X2Int16LstmQuantizationSettings() {
   quantization_settings.hidden_state = {/*scale=*/2.1362956633198035e-05,
                                         /*zp=*/0,
                                         /*symmetry=*/true};
-  quantization_settings.cell_state = {/*scale=*/0.00024414807580797754,
-                                      /*zp=*/0,
+  quantization_settings.cell_state = {/*scale=*/0.00024414062, /*zp=*/0,
                                       /*symmetry=*/true};
 
   // gate quantization parameters
