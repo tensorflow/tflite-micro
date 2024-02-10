aa76695453cf6369b2b398a9765c4cf9015a2e91
ddavis-2015 <ddavis@bdti.com>
2024-02-09 17:23:08 -0800

    Generic Benchmark Tool changes
    @tensorflow/micro
    
    Add ability to build with a model chosen at compile time.
    Add README with build instructions.
    
    bug=fixes #2450

--- tensorflow/lite/micro/tools/benchmarking/generic_model_benchmark.cc
+++ tensorflow/lite/micro/tools/benchmarking/generic_model_benchmark.cc
@@ -1,4 +1,4 @@
-/* Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2024 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@ limitations under the License.
 
 #include "tensorflow/lite/c/c_api_types.h"
 #include "tensorflow/lite/c/common.h"
+#include "tensorflow/lite/micro/micro_context.h"
 #include "tensorflow/lite/micro/micro_log.h"
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
 #include "tensorflow/lite/micro/micro_op_resolver.h"
@@ -33,6 +34,18 @@ limitations under the License.
 #include "tensorflow/lite/micro/tools/benchmarking/metrics.h"
 #include "tensorflow/lite/micro/tools/benchmarking/op_resolver.h"
 #include "tensorflow/lite/schema/schema_generated.h"
+
+#if defined(MODEL_HEADER_PATH)
+#if !defined(MODEL_NAME)
+#error "MODEL_NAME missing from CCFLAGS"
+#endif  // !defined(MODEL_NAME)
+
+#include MODEL_HEADER_PATH
+
+#define __MODEL_DATA(x) g_##x##_model_data
+#define _MODEL_DATA(x) __MODEL_DATA(x)
+#define MODEL_DATA _MODEL_DATA(MODEL_NAME)
+#endif  // defind(MODEL_HEADER_PATH)
 
 /*
  * Generic model benchmark.  Evaluates runtime performance of a provided model
@@ -45,15 +58,17 @@ namespace {
 
 using Profiler = ::tflite::MicroProfiler;
 
-constexpr int kTfLiteAbort = -9;
-
 // Seed used for the random input. Input data shouldn't affect invocation timing
 // so randomness isn't really needed.
 constexpr uint32_t kRandomSeed = 0xFB;
 
+#if !defined(MODEL_HEADER_PATH)
 constexpr size_t kTensorArenaSize = 3e6;
-constexpr int kNumResourceVariable = 100;
 constexpr size_t kModelSize = 2e6;
+#else
+constexpr size_t kTensorArenaSize = TENSOR_ARENA_SIZE;
+#endif  // !defined(MODEL_HEADER_PATH)
+constexpr int kNumResourceVariable = 100;
 
 void SetRandomInput(const uint32_t random_seed,
                     tflite::MicroInterpreter& interpreter) {
@@ -71,6 +86,7 @@ void SetRandomInput(const uint32_t random_seed,
   }
 }
 
+#if !defined(MODEL_HEADER_PATH)
 bool ReadFile(const char* file_name, void* buffer, size_t buffer_size) {
   std::unique_ptr<FILE, decltype(&fclose)> file(fopen(file_name, "rb"), fclose);
 
@@ -95,17 +111,14 @@ bool ReadFile(const char* file_name, void* buffer, size_t buffer_size) {
 
   return true;
 }
+#endif  // !defined(MODEL_HEADER_PATH)
 
-int Benchmark(const char* model_file_name, tflite::PrettyPrintType print_type) {
+int Benchmark(const uint8_t* model_data, tflite::PrettyPrintType print_type) {
   Profiler profiler;
   alignas(16) static uint8_t tensor_arena[kTensorArenaSize];
-  alignas(16) static uint8_t model_file_content[kModelSize];
 
-  if (!ReadFile(model_file_name, model_file_content, kModelSize)) {
-    return -1;
-  }
   uint32_t event_handle = profiler.BeginEvent("TfliteGetModel");
-  const tflite::Model* model = tflite::GetModel(model_file_content);
+  const tflite::Model* model = tflite::GetModel(model_data);
   profiler.EndEvent(event_handle);
 
   TflmOpResolver op_resolver;
@@ -164,6 +177,7 @@ int main(int argc, char** argv) {
   // Which format should be used to output debug information.
   tflite::PrettyPrintType print_type = tflite::PrettyPrintType::kTable;
 
+#if !defined(MODEL_HEADER_PATH)
   if (argc < 2 || argc > 3) {
     usage(argv[0]);
     return -1;
@@ -179,5 +193,14 @@ int main(int argc, char** argv) {
     }
   }
 
-  return tflite::Benchmark(model_filename, print_type);
+  alignas(16) static uint8_t model_data[tflite::kModelSize];
+
+  if (!tflite::ReadFile(model_filename, model_data, tflite::kModelSize)) {
+    return -1;
+  }
+#else
+  const uint8_t* model_data = MODEL_DATA;
+#endif  // !defined(MODEL_HEADER_PATH)
+
+  return tflite::Benchmark(model_data, print_type);
 }
