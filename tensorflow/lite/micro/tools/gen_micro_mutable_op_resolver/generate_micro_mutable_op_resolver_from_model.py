72188c478194aeaaf369614aa1b96f46613800d4
Måns Nilsson <mans.nilsson@arm.com>
2024-05-15 09:13:37 +0200

    Workaround for micro mutable generate script
    BUG=https://github.com/tensorflow/tflite-micro/issues/2564
    
    Change-Id: Id37ba1ef827eb1da8836dd8e7ea440e7e9918828

--- tensorflow/lite/micro/tools/gen_micro_mutable_op_resolver/generate_micro_mutable_op_resolver_from_model.py
+++ tensorflow/lite/micro/tools/gen_micro_mutable_op_resolver/generate_micro_mutable_op_resolver_from_model.py
@@ -1,4 +1,4 @@
-# Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2024 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,7 +23,11 @@ from absl import app
 from absl import flags
 from mako import template
 
-from tflite_micro.tensorflow.lite.tools import visualize as visualize
+# TODO(#2564): Resolve import issue
+try:
+  from tflite_micro.tensorflow.lite.tools import visualize as visualize
+except ImportError:
+  from tensorflow.lite.tools import visualize as visualize
 
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 TEMPLATE_DIR = os.path.abspath(TEMPLATE_DIR)
