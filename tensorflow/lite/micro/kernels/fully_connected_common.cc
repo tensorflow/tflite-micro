47b54507432b1ad6e30eda513e07593cb4065632
Måns Nilsson <mans.nilsson@arm.com>
2024-09-27 18:13:11 +0200

    Add per channel quantization support for fully connnected (#2673)
    Adds per channel quantization support for fully connnected reference and cmsis-nn kernel.
    
    BUG=missing support for FC per channel quantization

--- tensorflow/lite/micro/kernels/fully_connected_common.cc
+++ tensorflow/lite/micro/kernels/fully_connected_common.cc
@@ -1,4 +1,4 @@
-/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2024 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -57,46 +57,98 @@ TfLiteStatus CalculateOpDataFullyConnected(
     TfLiteType data_type, const TfLiteTensor* input, const TfLiteTensor* filter,
     const TfLiteTensor* bias, TfLiteTensor* output,
     OpDataFullyConnected* data) {
-  // TODO(b/324385802): Support per-channel quantization for FullyConnected.
-  // If you have hit this failure message, you will need to disable this
-  // behavior. This can be done by setting the following flag to true:
-  // TfLiteConverter._experimental_disable_per_channel_quantization_for_dense_layers
-  // https://github.com/tensorflow/tensorflow/blob/377f47694fa790e98db6665b9adecde00b5e0d68/tensorflow/lite/python/lite.py#L674
-  if (filter->quantization.type == kTfLiteAffineQuantization &&
-      filter->quantization.params != nullptr) {
-    TfLiteAffineQuantization* affine_quantization =
-        reinterpret_cast<TfLiteAffineQuantization*>(
-            filter->quantization.params);
-    TF_LITE_ENSURE(context, affine_quantization->scale);
-    TF_LITE_ENSURE_MSG(
-        context, affine_quantization->scale->size == 1,
-        "FullyConnected per-channel quantization not yet supported. Please set "
-        "converter._experimental_disable_per_channel_quantization_for_dense_"
-        "layers = True.");
+#ifndef HEXAGON
+  data->is_per_channel = false;
+#endif
+
+  if (data_type == kTfLiteFloat32) {
+    return kTfLiteOk;
   }
 
-  if (data_type != kTfLiteFloat32) {
+  bool is_per_channel = false;
+  if (filter->quantization.type == kTfLiteAffineQuantization &&
+      filter->quantization.params != nullptr) {
+    const auto* affine_quantization =
+        reinterpret_cast<TfLiteAffineQuantization*>(
+            filter->quantization.params);
+    TF_LITE_ENSURE(context, affine_quantization);
+    TF_LITE_ENSURE(context, affine_quantization->scale);
+    is_per_channel = affine_quantization->scale->size > 1;
+  }
+
+  if (is_per_channel) {
+// Hexagon currently does not support per-channel fully connected, and the
+// existing hexagon support library is intolerant of data members being added to
+// OpDataFullyConnected. As such, we have to be careful not to reference newer
+// data members. This is why we use a local variable is_per_channel in common
+// code, and only reference the data->is_per_channel in non-HEXAGON code.
+#ifdef HEXAGON
+    TF_LITE_ENSURE_MSG(
+        context, !is_per_channel,
+        "FullyConnected per-channel quantization not yet supported on Hexagon. "
+        "Please set converter._experimental_disable_per_channel_quantization_"
+        "for_dense_layers = True.");
+#else
+    data->is_per_channel = is_per_channel;
+    const auto* affine_quantization =
+        reinterpret_cast<TfLiteAffineQuantization*>(
+            filter->quantization.params);
+    const int per_channel_quantization_size = affine_quantization->scale->size;
+
+    //  Currently only Int8 is supported for per channel quantization.
+    TF_LITE_ENSURE(context,
+                   input->type == kTfLiteInt8 && filter->type != kTfLiteInt4);
+
+    TF_LITE_ENSURE_EQ(
+        context, per_channel_quantization_size,
+        filter->dims->data[affine_quantization->quantized_dimension]);
+
+    data->per_channel_output_multiplier =
+        static_cast<int32_t*>(context->AllocatePersistentBuffer(
+            context, per_channel_quantization_size * sizeof(int32_t)));
+    data->per_channel_output_shift =
+        static_cast<int32_t*>(context->AllocatePersistentBuffer(
+            context, per_channel_quantization_size * sizeof(int32_t)));
+
+    // Populate multiplier and shift using affine quantization.
+    const float input_scale = input->params.scale;
+    const float output_scale = output->params.scale;
+    const float* filter_scales = affine_quantization->scale->data;
+
+    for (int i = 0; i < per_channel_quantization_size; ++i) {
+      const float scale = filter_scales[i];
+      const double filter_scale = static_cast<double>(scale);
+      const double effective_output_scale = static_cast<double>(input_scale) *
+                                            filter_scale /
+                                            static_cast<double>(output_scale);
+      int32_t significand;
+      int channel_shift;
+      QuantizeMultiplier(effective_output_scale, &significand, &channel_shift);
+      data->per_channel_output_multiplier[i] = significand;
+      data->per_channel_output_shift[i] = channel_shift;
+    }
+#endif
+  } else {
     double real_multiplier = 0.0;
     TF_LITE_ENSURE_STATUS(GetQuantizedConvolutionMultipler(
         context, input, filter, bias, output, &real_multiplier));
     QuantizeMultiplier(real_multiplier, &data->output_multiplier,
                        &data->output_shift);
-
-    // Filter weights will always be symmetric quantized since we only support
-    // int8 quantization. See
-    // https://github.com/tensorflow/tensorflow/issues/44912 for additional
-    // context.
-    TFLITE_DCHECK(filter->params.zero_point == 0);
-
-    data->input_zero_point = input->params.zero_point;
-    data->filter_zero_point = filter->params.zero_point;
-    data->output_zero_point = output->params.zero_point;
-
-    return CalculateActivationRangeQuantized(context, activation, output,
-                                             &data->output_activation_min,
-                                             &data->output_activation_max);
   }
-  return kTfLiteOk;
+
+  // Filter weights will always be symmetric quantized since we only support
+  // int8 quantization. See
+  // https://github.com/tensorflow/tensorflow/issues/44912 for additional
+  // context.
+  TFLITE_DCHECK(filter->params.zero_point == 0);
+
+  data->input_zero_point = input->params.zero_point;
+  data->filter_zero_point = filter->params.zero_point;
+  data->output_zero_point = output->params.zero_point;
+
+  return CalculateActivationRangeQuantized(context, activation, output,
+                                           &data->output_activation_min,
+                                           &data->output_activation_max);
 }
 
 }  // namespace tflite
