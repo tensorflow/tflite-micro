d4194775828601d4e01a45f2b852a28bd2b7d0cc
Ryan Kuester <kuester@bdti.com>
2024-11-18 18:56:21 -0600

    refactor(metadata_test_cc): eliminate iostream, extract expected values
    Refactor away use of <iostream>, factor out expected values and
    setup code.

--- tensorflow/lite/micro/compression/metadata_test.cc
+++ tensorflow/lite/micro/compression/metadata_test.cc
@@ -15,11 +15,10 @@
 // Test validity of the flatbuffer schema and illustrate use of the flatbuffer
 // machinery with C++.
 
-#include <iostream>
-#include <vector>
-
 #include "tensorflow/lite/micro/compression/metadata_saved.h"
 #include "tensorflow/lite/micro/hexdump.h"
+#include "tensorflow/lite/micro/micro_log.h"
+#include "tensorflow/lite/micro/span.h"
 #include "tensorflow/lite/micro/testing/micro_test.h"
 
 using tflite::micro::compression::LutTensor;
@@ -31,51 +30,76 @@ using tflite::micro::compression::SubgraphT;
 
 namespace {
 
-bool operator==(const LutTensorT& a, const LutTensor& b) {
+struct ExpectedLutTensor {
+  int tensor;
+  unsigned value_buffer;
+  int index_bitwidth;
+};
+
+constexpr ExpectedLutTensor kExpected0 = {
+    .tensor = 63,
+    .value_buffer = 128,
+    .index_bitwidth = 2,
+};
+
+constexpr ExpectedLutTensor kExpected1 = {
+    .tensor = 64,
+    .value_buffer = 129,
+    .index_bitwidth = 4,
+};
+
+bool operator==(const ExpectedLutTensor& a, const LutTensor& b) {
   return a.tensor == b.tensor() && a.value_buffer == b.value_buffer() &&
          a.index_bitwidth == b.index_bitwidth();
 }
+
+constexpr unsigned kExpectedSchemaVersion = 1;
 
 }  // end anonymous namespace
 
 TF_LITE_MICRO_TESTS_BEGIN
 
-TF_LITE_MICRO_TEST(ReadTest) {
-  // Create these objects on the stack and copy them into the subgraph's vector,
-  // so they can be compared later to what is read from the flatbuffer.
-  LutTensorT lut_tensor0;
-  lut_tensor0.tensor = 63;
-  lut_tensor0.value_buffer = 128;
-  lut_tensor0.index_bitwidth = 2;
+// Create these objects on the stack and copy them into the subgraph's vector,
+// so they can be compared later to what is read from the flatbuffer.
+LutTensorT lut_tensor0;
+lut_tensor0.tensor = kExpected0.tensor;
+lut_tensor0.value_buffer = kExpected0.value_buffer;
+lut_tensor0.index_bitwidth = kExpected0.index_bitwidth;
 
-  LutTensorT lut_tensor1;
-  lut_tensor1.tensor = 64;
-  lut_tensor1.value_buffer = 129;
-  lut_tensor1.index_bitwidth = 4;
+LutTensorT lut_tensor1;
+lut_tensor1.tensor = kExpected1.tensor;
+lut_tensor1.value_buffer = kExpected1.value_buffer;
+lut_tensor1.index_bitwidth = kExpected1.index_bitwidth;
 
-  auto subgraph0 = std::make_unique<SubgraphT>();
-  subgraph0->lut_tensors.push_back(std::make_unique<LutTensorT>(lut_tensor0));
-  subgraph0->lut_tensors.push_back(std::make_unique<LutTensorT>(lut_tensor1));
+auto subgraph0 = std::make_unique<SubgraphT>();
+subgraph0->lut_tensors.push_back(std::make_unique<LutTensorT>(lut_tensor0));
+subgraph0->lut_tensors.push_back(std::make_unique<LutTensorT>(lut_tensor1));
 
-  auto metadata = std::make_unique<MetadataT>();
-  metadata->subgraphs.push_back(std::move(subgraph0));
+auto metadata = std::make_unique<MetadataT>();
+metadata->subgraphs.push_back(std::move(subgraph0));
 
-  flatbuffers::FlatBufferBuilder builder;
-  auto root = Metadata::Pack(builder, metadata.get());
-  builder.Finish(root);
-  const uint8_t* buffer = builder.GetBufferPointer();
-  const size_t buffer_size = builder.GetSize();
+flatbuffers::FlatBufferBuilder builder;
+auto root = Metadata::Pack(builder, metadata.get());
+builder.Finish(root);
+auto flatbuffer =
+    tflite::Span{reinterpret_cast<const std::byte*>(builder.GetBufferPointer()),
+                 builder.GetSize()};
 
-  tflite::hexdump({reinterpret_cast<const std::byte*>(buffer), buffer_size});
-  std::cout << "length: " << buffer_size << "\n";
-
+TF_LITE_MICRO_TEST(ReadbackEqualsWrite) {
   const Metadata* read_metadata =
-      tflite::micro::compression::GetMetadata(buffer);
+      tflite::micro::compression::GetMetadata(flatbuffer.data());
   const Subgraph* read_subgraph0 = read_metadata->subgraphs()->Get(0);
   const LutTensor* read_lut_tensor0 = read_subgraph0->lut_tensors()->Get(0);
   const LutTensor* read_lut_tensor1 = read_subgraph0->lut_tensors()->Get(1);
-  TF_LITE_MICRO_EXPECT(lut_tensor0 == *read_lut_tensor0);
-  TF_LITE_MICRO_EXPECT(lut_tensor1 == *read_lut_tensor1);
+  TF_LITE_MICRO_EXPECT(kExpected0 == *read_lut_tensor0);
+  TF_LITE_MICRO_EXPECT(kExpected1 == *read_lut_tensor1);
+
+  TF_LITE_MICRO_EXPECT(read_metadata->schema_version() ==
+                       kExpectedSchemaVersion);
+
+  // Print representation of the binary flatbuffer for debugging purposes.
+  tflite::hexdump(flatbuffer);
+  MicroPrintf("length: %i", flatbuffer.size());
 }
 
 TF_LITE_MICRO_TESTS_END
