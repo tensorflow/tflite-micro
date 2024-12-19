e3ac890725b22a37293f64203b5270fbefebdfe1
suleshahid <110432064+suleshahid@users.noreply.github.com>
2024-12-18 16:21:00 -0800

    Fix missing includes for printf (#3023)
    BUG=quick fix for missing includes

--- tensorflow/lite/micro/kernels/depthwise_conv_common.cc
+++ tensorflow/lite/micro/kernels/depthwise_conv_common.cc
@@ -24,6 +24,7 @@ limitations under the License.
 #include "tensorflow/lite/kernels/padding.h"
 #include "tensorflow/lite/micro/kernels/depthwise_conv.h"
 #include "tensorflow/lite/micro/kernels/kernel_util.h"
+#include "tensorflow/lite/micro/micro_log.h"
 
 namespace tflite {
 
