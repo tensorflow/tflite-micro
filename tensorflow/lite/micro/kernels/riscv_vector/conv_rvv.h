54e272ad5cb898eb56780578714c99b7ba21f324
JaimeHW <jameswhitfield65@gmail.com>
2025-04-14 12:43:40 -0500

    Add placeholder file until we adapt the convolution implementation to tflite-micro

---
+++ tensorflow/lite/micro/kernels/riscv_vector/conv_rvv.h
@@ -0,0 +1,10 @@
+// tensorflow/lite/micro/kernels/riscv_vector/conv_rvv.h
+#ifndef TENSORFLOW_LITE_MICRO_KERNELS_RISCV_VECTOR_CONV_RVV_H_
+#define TENSORFLOW_LITE_MICRO_KERNELS_RISCV_VECTOR_CONV_RVV_H_
+
+#include <cstdint>
+#include <cstddef>
+
+// #ifdef __cplusplus
+// extern "C" {
+// #endif
