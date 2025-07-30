67ff9bf55e5e45c1241ea8f07081a4e00d1cb184
ddavis-2015 <ddavis@bdti.com>
2025-07-30 09:45:06 -0700

    xtensa per-channel int8 normal axis working

--- tensorflow/lite/micro/kernels/xtensa/xtensa_decode_state_prune.cc
+++ tensorflow/lite/micro/kernels/xtensa/xtensa_decode_state_prune.cc
@@ -153,7 +153,7 @@ void XtensaDecodeStatePrune::DecompressToBufferPerChannelInt8_Xtensa(
   ScopedMicroProfiler scoped_profiler(__func__, micro_profiler_);
 
   ae_int8x16* p_weights = (ae_int8x16*)value_table_;
-  int* __restrict p_mask32 = (int*)compressed_indices_;
+  short* __restrict p_stream = (short*)compressed_indices_;
   ae_valign align = AE_LA64_PP(p_weights);
   ae_int8x8 data0, data1, data2, data3;
   ae_int8x8 shfl0, shfl1, shfl2, shfl3;
@@ -161,26 +161,35 @@ void XtensaDecodeStatePrune::DecompressToBufferPerChannelInt8_Xtensa(
   int8_t* __restrict pCoeff = static_cast<int8_t*>(buffer);
   ae_int8x8 discarded;
 
+  WUR_AE_BITPTR(0);
+  WUR_AE_BITHEAD(0);
+
+  AE_DBI_IP((const unsigned short*)p_stream, 16);
+  AE_DBI_IP((const unsigned short*)p_stream, 16);
+
   for (size_t channel = 0; channel < num_channels_; channel++) {
     ae_int8x8 zero = zero_points_[channel];
+    uint32_t mask_low, mask_high;
 
     for (int i = 0; i < count >> 5; i++) {
       // unpack elements
-      int mask = *p_mask32++;
-      AE_LAVUNSQZ8X8_XP(data0, shfl0, align, p_weights, mask, 0);
-      AE_LAVUNSQZ8X8_XP(data1, shfl1, align, p_weights, mask, 1);
-      AE_LAVUNSQZ8X8_XP(data2, shfl2, align, p_weights, mask, 2);
-      AE_LAVUNSQZ8X8_XP(data3, shfl3, align, p_weights, mask, 3);
+      AE_LBI_DBI_IP((unsigned short*)p_stream, mask_high, 16);
+      AE_LBI_DBI_IP((unsigned short*)p_stream, mask_low, 16);
+      int mask = (mask_high << 16) | mask_low;
+      AE_LAVUNSQZ8X8_XP(data0, shfl0, align, p_weights, mask, 3);
+      AE_LAVUNSQZ8X8_XP(data1, shfl1, align, p_weights, mask, 2);
+      AE_LAVUNSQZ8X8_XP(data2, shfl2, align, p_weights, mask, 1);
+      AE_LAVUNSQZ8X8_XP(data3, shfl3, align, p_weights, mask, 0);
       data0 = AE_SHFL8X8(data0, shfl0);
       data1 = AE_SHFL8X8(data1, shfl1);
       data2 = AE_SHFL8X8(data2, shfl2);
       data3 = AE_SHFL8X8(data3, shfl3);
 
       // merge <zero> into elements
-      AE_MOVT8X16_L(discarded, data0, zero, data0, mask);
-      AE_MOVT8X16_L(discarded, data1, zero, data1, mask >> 8);
-      AE_MOVT8X16_H(discarded, data2, zero, data2, mask);
-      AE_MOVT8X16_H(discarded, data3, zero, data3, mask >> 8);
+      AE_MOVT8X16_H(discarded, data0, zero, data0, mask >> 8);
+      AE_MOVT8X16_H(discarded, data1, zero, data1, mask);
+      AE_MOVT8X16_L(discarded, data2, zero, data2, mask >> 8);
+      AE_MOVT8X16_L(discarded, data3, zero, data3, mask);
 
       // move merged elements to output
       AE_S8X8X2_IP(data0, data1, (ae_int8x16*)pCoeff, 16);
@@ -190,31 +199,31 @@ void XtensaDecodeStatePrune::DecompressToBufferPerChannelInt8_Xtensa(
     const int count_rem = count & 0x1F;
     if (count_rem) {
       ae_valignx2 align2 = AE_ZALIGN128();
-      int8_t* __restrict p_mask8 = reinterpret_cast<int8_t*>(p_mask32);
+
+      if (count_rem > 16) {
+        AE_LBI_DBI_IP((unsigned short*)p_stream, mask_high, 16);
+        AE_LB_DB_IP((unsigned short*)p_stream, mask_low, count_rem - 16);
+        mask_low <<= 32 - count_rem;
+      } else {
+        AE_LB_DB_IP((unsigned short*)p_stream, mask_high, count_rem);
+        mask_high <<= 16 - count_rem;
+        mask_low = 0;
+      }
+      int mask = (mask_high << 16) | mask_low;
 
       // unpack and merge <zero> into remaining elements
-      int mask = *p_mask8++;
-      AE_LAVUNSQZ8X8_XP(data0, shfl0, align, p_weights, mask, 0);
+      AE_LAVUNSQZ8X8_XP(data0, shfl0, align, p_weights, mask, 3);
       data0 = AE_SHFL8X8(data0, shfl0);
-      AE_MOVT8X16_L(discarded, data0, zero, data0, mask);
-      if (count_rem > 8) {
-        mask = *p_mask8++;
-        AE_LAVUNSQZ8X8_XP(data1, shfl1, align, p_weights, mask, 0);
-        data1 = AE_SHFL8X8(data1, shfl1);
-        AE_MOVT8X16_L(discarded, data1, zero, data1, mask);
-      }
-      if (count_rem > 16) {
-        mask = *p_mask8++;
-        AE_LAVUNSQZ8X8_XP(data2, shfl2, align, p_weights, mask, 0);
-        data2 = AE_SHFL8X8(data2, shfl2);
-        AE_MOVT8X16_L(discarded, data2, zero, data2, mask);
-      }
-      if (count_rem > 24) {
-        mask = *p_mask8++;
-        AE_LAVUNSQZ8X8_XP(data3, shfl3, align, p_weights, mask, 0);
-        data3 = AE_SHFL8X8(data3, shfl3);
-        AE_MOVT8X16_L(discarded, data3, zero, data3, mask);
-      }
+      AE_MOVT8X16_H(discarded, data0, zero, data0, mask >> 8);
+      AE_LAVUNSQZ8X8_XP(data1, shfl1, align, p_weights, mask, 2);
+      data1 = AE_SHFL8X8(data1, shfl1);
+      AE_MOVT8X16_H(discarded, data1, zero, data1, mask);
+      AE_LAVUNSQZ8X8_XP(data2, shfl2, align, p_weights, mask, 1);
+      data2 = AE_SHFL8X8(data2, shfl2);
+      AE_MOVT8X16_L(discarded, data2, zero, data2, mask >> 8);
+      AE_LAVUNSQZ8X8_XP(data3, shfl3, align, p_weights, mask, 0);
+      data3 = AE_SHFL8X8(data3, shfl3);
+      AE_MOVT8X16_L(discarded, data3, zero, data3, mask);
 
       // move merged elements to output
       if (count_rem <= 16) {
