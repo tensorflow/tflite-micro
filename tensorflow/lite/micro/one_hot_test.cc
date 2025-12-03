61aa04bb4cafa2a8c8669c1fa1d34c77795bd803
junseokShim <tjdrbsrhk123@gmail.com>
2025-12-03 20:41:01 +0900

    refactoring one_hot

--- tensorflow/lite/micro/one_hot_test.cc
+++ tensorflow/lite/micro/one_hot_test.cc
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "tensorflow/lite/micro/kernels/one_hot.cc"
+#include "tensorflow/lite/micro/kernels/one_hot.h"
 
 #include <stdint.h>
 
