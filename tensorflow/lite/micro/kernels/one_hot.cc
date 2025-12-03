2795ddfcba560d356c0ab459b648cf68047c3ccd
junseokShim <tjdrbsrhk123@gmail.com>
2025-12-03 20:41:00 +0900

    Test Passed version

--- tensorflow/lite/micro/kernels/one_hot.cc
+++ tensorflow/lite/micro/kernels/one_hot.cc
@@ -12,213 +12,266 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+
 #include <stdint.h>
 
-#include "tensorflow/lite/core/c/builtin_op_data.h"
-#include "tensorflow/lite/core/c/common.h"
+#include "tensorflow/lite/c/builtin_op_data.h"
+#include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
-#include "tensorflow/lite/kernels/kernel_util.h"
+#include "tensorflow/lite/micro/kernels/kernel_util.h"
 
 namespace tflite {
 namespace ops {
-namespace builtin {
+namespace micro {
 namespace one_hot {
 
+constexpr int kIndicesTensor = 0;
+constexpr int kDepthTensor = 1;
+constexpr int kOnValueTensor = 2;
+constexpr int kOffValueTensor = 3;
+constexpr int kOutputTensor = 0;
+
+namespace {  // 로컬 유틸 함수들
+
+inline const TfLiteTensor* GetInput(TfLiteContext* context,
+                                    const TfLiteNode* node, int index) {
+  return &context->tensors[node->inputs->data[index]];
+}
+
+inline TfLiteTensor* GetOutput(TfLiteContext* context, const TfLiteNode* node,
+                               int index) {
+  return &context->tensors[node->outputs->data[index]];
+}
+
+inline int NumInputs(const TfLiteNode* node) { return node->inputs->size; }
+
+inline int NumOutputs(const TfLiteNode* node) { return node->outputs->size; }
+
+// Tensor 전체 element 개수 계산
+inline int NumElements(const TfLiteTensor* t) {
+  int count = 1;
+  for (int i = 0; i < t->dims->size; ++i) {
+    count *= t->dims->data[i];
+  }
+  return count;
+}
+
+// 동적 텐서인지 확인
+inline bool IsDynamicTensor(const TfLiteTensor* tensor) {
+  return tensor->allocation_type == kTfLiteDynamic;
+}
+
+// 상수/퍼시스턴트 텐서인지 확인
+inline bool IsConstantOrPersistentTensor(const TfLiteTensor* tensor) {
+  return tensor->allocation_type == kTfLiteMmapRo ||
+         tensor->allocation_type == kTfLiteArenaRwPersistent;
+}
+
+// 텐서를 동적 텐서로 마킹
+inline void SetTensorToDynamic(TfLiteTensor* tensor) {
+  tensor->allocation_type = kTfLiteDynamic;
+}
+
+}  // namespace
+
+// Convenience utility for destructuring a node into the appropriate tensors and
+// data for the op. Note that this destructuring is quite cheap, so we can avoid
+// allocating op-specific, persistent data on the heap.
+struct OneHotContext {
+  OneHotContext(TfLiteContext* context, TfLiteNode* node) {
+    indices = GetInput(context, node, kIndicesTensor);
+    depth = GetInput(context, node, kDepthTensor);
+    on_value = GetInput(context, node, kOnValueTensor);
+    off_value = GetInput(context, node, kOffValueTensor);
+    output = GetOutput(context, node, kOutputTensor);
+
+    const auto* params =
+        reinterpret_cast<TfLiteOneHotParams*>(node->builtin_data);
+    const int indices_dims = indices->dims->size;
+    axis = (params->axis == -1) ? indices_dims : params->axis;
+    output_dims = indices_dims + 1;
+    dtype = on_value->type;
+  }
+
+  const TfLiteTensor* indices;
+  const TfLiteTensor* depth;
+  const TfLiteTensor* on_value;
+  const TfLiteTensor* off_value;
+  TfLiteTensor* output;
+  int axis;
+  int output_dims;
+  TfLiteType dtype;
+};
+
+template <typename T, typename TI>
+void OneHotComputeImpl(const OneHotContext& op_context) {
+  // prefix_dim_size == # of elements before the axis
+  // depth == # of elements per axis
+  // suffix_dim_size == # of elements after the axis
+  int prefix_dim_size = 1;
+  for (int i = 0; i < op_context.axis; ++i) {
+    prefix_dim_size *= op_context.indices->dims->data[i];
+  }
+  if (prefix_dim_size == 0) {
+    // If indices tensor is degenerate, return a degenerate tensor, just like
+    // TensorFlow does.
+    return;
+  }
+
+  const int suffix_dim_size = NumElements(op_context.indices) / prefix_dim_size;
+  const int depth = *op_context.depth->data.i32;
+
+  const T on_value = *tflite::GetTensorData<T>(op_context.on_value);
+  const T off_value = *tflite::GetTensorData<T>(op_context.off_value);
+
+  // View the indices as a matrix of size:
+  //     prefix_dim_size x suffix_dim_size
+  // View the output as a matrix of size:
+  //     prefix_dim_size x depth x suffix_dim_size
+  // Then the output is:
+  //     output(i, j, k) == (indices(i, k) == j) ? on : off
+  T* output = tflite::GetTensorData<T>(op_context.output);
+  const TI* indices = tflite::GetTensorData<TI>(op_context.indices);
+
+  for (int i = 0; i < prefix_dim_size; ++i) {
+    for (int j = 0; j < depth; ++j) {
+      for (int k = 0; k < suffix_dim_size; ++k, ++output) {
+        *output = static_cast<int>(indices[i * suffix_dim_size + k]) == j
+                      ? on_value
+                      : off_value;
+      }
+    }
+  }
+}
+
+template <typename T>
+void OneHotCompute(const OneHotContext& op_context) {
+  if (op_context.indices->type == kTfLiteInt64) {
+    OneHotComputeImpl<T, int64_t>(op_context);
+  } else {
+    OneHotComputeImpl<T, int32_t>(op_context);
+  }
+}
+
+TfLiteStatus ResizeOutputTensor(TfLiteContext* context,
+                                const OneHotContext& op_context) {
+  TF_LITE_ENSURE(context, *op_context.depth->data.i32 >= 0);
+
+  // TfLiteIntArrayCreate 대신, TFLM 스타일로 직접 AllocatePersistentBuffer 사용
+  const int dims = op_context.output_dims;
+  // TfLiteIntArray 구조체 전체 크기 계산
+  size_t bytes = sizeof(TfLiteIntArray) + sizeof(int) * (dims - 1);
+
+  // Micro 환경에서는 malloc이 아니라 context->AllocatePersistentBuffer 써야 함
+  TfLiteIntArray* output_size = reinterpret_cast<TfLiteIntArray*>(
+      context->AllocatePersistentBuffer(context, bytes));
+  TF_LITE_ENSURE(context, output_size != nullptr);
+
+  output_size->size = dims;
+  for (int i = 0; i < dims; ++i) {
+    if (i < op_context.axis) {
+      output_size->data[i] = op_context.indices->dims->data[i];
+    } else if (i == op_context.axis) {
+      output_size->data[i] = *op_context.depth->data.i32;
+    } else {
+      output_size->data[i] = op_context.indices->dims->data[i - 1];
+    }
+  }
+
+  return context->ResizeTensor(context, op_context.output, output_size);
+}
+
+TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
+  TF_LITE_ENSURE_EQ(context, NumInputs(node), 4);
+  TF_LITE_ENSURE_EQ(context, NumOutputs(node), 1);
+
+  OneHotContext op_context{context, node};
+  TF_LITE_ENSURE(context, op_context.output != nullptr);
+
+  switch (op_context.dtype) {
+    // TODO(b/111744875): Support uint8 and quantization.
+    case kTfLiteFloat32:
+    case kTfLiteInt16:
+    case kTfLiteInt32:
+    case kTfLiteInt64:
+    case kTfLiteInt8:
+    case kTfLiteUInt8:
+    case kTfLiteBool:
+      op_context.output->type = op_context.dtype;
+      break;
+    default:
+      TF_LITE_KERNEL_LOG(context, "Unknown output data type: %s",
+                         TfLiteTypeGetName(op_context.dtype));
+      return kTfLiteError;
+  }
+
+  TF_LITE_ENSURE(context, op_context.indices->type == kTfLiteInt32 ||
+                              op_context.indices->type == kTfLiteInt64);
+  TF_LITE_ENSURE(context, op_context.axis >= 0 &&
+                              op_context.axis < op_context.output_dims);
+  TF_LITE_ENSURE_EQ(context, NumElements(op_context.depth), 1);
+  TF_LITE_ENSURE_EQ(context, NumElements(op_context.on_value), 1);
+  TF_LITE_ENSURE_EQ(context, NumElements(op_context.off_value), 1);
+  TF_LITE_ENSURE_TYPES_EQ(context, op_context.on_value->type, op_context.dtype);
+  TF_LITE_ENSURE_TYPES_EQ(context, op_context.off_value->type,
+                          op_context.dtype);
+
+  if (!IsConstantOrPersistentTensor(op_context.depth)) {
+    SetTensorToDynamic(op_context.output);
+    return kTfLiteOk;
+  }
+
+  return ResizeOutputTensor(context, op_context);
+}
+
+TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
+  OneHotContext op_context{context, node};
+
+  if (IsDynamicTensor(op_context.output)) {
+    TF_LITE_ENSURE_OK(context, ResizeOutputTensor(context, op_context));
+  }
+
+  switch (op_context.output->type) {
+    case kTfLiteFloat32:
+      OneHotCompute<float>(op_context);
+      break;
+    case kTfLiteInt32:
+      OneHotCompute<int32_t>(op_context);
+      break;
+    case kTfLiteInt64:
+      OneHotCompute<int64_t>(op_context);
+      break;
+    case kTfLiteInt8:
+      OneHotCompute<int8_t>(op_context);
+      break;
+    case kTfLiteUInt8:
+      OneHotCompute<uint8_t>(op_context);
+      break;
+    case kTfLiteBool:
+      OneHotCompute<bool>(op_context);
+      break;
+    default:
+      return kTfLiteError;
+  }
+
+  return kTfLiteOk;
+}
+
+}  // namespace one_hot
+
+// TFLM 쪽에서 사용할 등록 함수
 TfLiteRegistration_V1* Register_ONE_HOT() {
-  static TfLiteRegistration_V1 r = {/*init, free, prepare, eval*/};
+  static TfLiteRegistration_V1 r = {};  // 모든 필드를 0 / nullptr로 초기화
+
+  r.init = nullptr;
+  r.free = nullptr;
+  r.prepare = one_hot::Prepare;
+  r.invoke = one_hot::Eval;
+  // 나머지 custom_name, version, profiling_string 등은 0 / nullptr 유지
+
   return &r;
 }
 
-// constexpr int kIndicesTensor = 0;
-// constexpr int kDepthTensor = 1;
-// constexpr int kOnValueTensor = 2;
-// constexpr int kOffValueTensor = 3;
-// constexpr int kOutputTensor = 0;
-
-// // Convenience utility for destructuring a node into the appropriate tensors
-// and
-// // data for the op. Note that this destructuring is quite cheap, so we can
-// avoid
-// // allocating op-specific, persistent data on the heap.
-// struct OneHotContext {
-//   OneHotContext(TfLiteContext* context, TfLiteNode* node) {
-//     indices = GetInput(context, node, kIndicesTensor);
-//     depth = GetInput(context, node, kDepthTensor);
-//     on_value = GetInput(context, node, kOnValueTensor);
-//     off_value = GetInput(context, node, kOffValueTensor);
-//     output = GetOutput(context, node, kOutputTensor);
-
-//     const auto* params =
-//         reinterpret_cast<TfLiteOneHotParams*>(node->builtin_data);
-//     const int indices_dims = indices->dims->size;
-//     axis = (params->axis == -1) ? indices_dims : params->axis;
-//     output_dims = indices_dims + 1;
-//     dtype = on_value->type;
-//   }
-
-//   const TfLiteTensor* indices;
-//   const TfLiteTensor* depth;
-//   const TfLiteTensor* on_value;
-//   const TfLiteTensor* off_value;
-//   TfLiteTensor* output;
-//   int axis;
-//   int output_dims;
-//   TfLiteType dtype;
-// };
-
-// template <typename T, typename TI>
-// void OneHotComputeImpl(const OneHotContext& op_context) {
-//   // prefix_dim_size == # of elements before the axis
-//   // depth == # of elements per axis
-//   // suffix_dim_size == # of elements after the axis
-//   int prefix_dim_size = 1;
-//   for (int i = 0; i < op_context.axis; ++i) {
-//     prefix_dim_size *= op_context.indices->dims->data[i];
-//   }
-//   if (prefix_dim_size == 0) {
-//     // If indices tensor is degenerate, return a degenerate tensor, just like
-//     // TensorFlow does.
-//     return;
-//   }
-//   const int suffix_dim_size = NumElements(op_context.indices) /
-//   prefix_dim_size; const int depth = *op_context.depth->data.i32;
-
-//   const T on_value = *GetTensorData<T>(op_context.on_value);
-//   const T off_value = *GetTensorData<T>(op_context.off_value);
-
-//   // View the indices as a matrix of size:
-//   //     prefix_dim_size x suffix_dim_size
-//   // View the output as a matrix of size:
-//   //     prefix_dim_size x depth x suffix_dim_size
-//   // Then the output is:
-//   //     output(i, j, k) == (indices(i, k) == j) ? on : off
-//   T* output = GetTensorData<T>(op_context.output);
-//   const TI* indices = GetTensorData<TI>(op_context.indices);
-//   for (int i = 0; i < prefix_dim_size; ++i) {
-//     for (int j = 0; j < depth; ++j) {
-//       for (int k = 0; k < suffix_dim_size; ++k, ++output) {
-//         *output = static_cast<int>(indices[i * suffix_dim_size + k]) == j
-//                       ? on_value
-//                       : off_value;
-//       }
-//     }
-//   }
-// }
-
-// template <typename T>
-// void OneHotCompute(const OneHotContext& op_context) {
-//   if (op_context.indices->type == kTfLiteInt64) {
-//     OneHotComputeImpl<T, int64_t>(op_context);
-//   } else {
-//     OneHotComputeImpl<T, int>(op_context);
-//   }
-// }
-
-// TfLiteStatus ResizeOutputTensor(TfLiteContext* context,
-//                                 const OneHotContext& op_context) {
-//   TF_LITE_ENSURE(context, *op_context.depth->data.i32 >= 0);
-//   TfLiteIntArray* output_size = TfLiteIntArrayCreate(op_context.output_dims);
-//   for (int i = 0; i < op_context.output_dims; ++i) {
-//     if (i < op_context.axis) {
-//       output_size->data[i] = op_context.indices->dims->data[i];
-//     } else if (i == op_context.axis) {
-//       output_size->data[i] = *op_context.depth->data.i32;
-//     } else {
-//       output_size->data[i] = op_context.indices->dims->data[i - 1];
-//     }
-//   }
-//   return context->ResizeTensor(context, op_context.output, output_size);
-// }
-
-// TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
-//   TF_LITE_ENSURE_EQ(context, NumInputs(node), 4);
-//   TF_LITE_ENSURE_EQ(context, NumOutputs(node), 1);
-
-//   OneHotContext op_context{context, node};
-//   TF_LITE_ENSURE(context, op_context.output != nullptr);
-//   switch (op_context.dtype) {
-//     // TODO(b/111744875): Support uint8 and quantization.
-//     case kTfLiteFloat32:
-//     case kTfLiteInt16:
-//     case kTfLiteInt32:
-//     case kTfLiteInt64:
-//     case kTfLiteInt8:
-//     case kTfLiteUInt8:
-//     case kTfLiteBool:
-//       op_context.output->type = op_context.dtype;
-//       break;
-//     default:
-//       TF_LITE_KERNEL_LOG(context, "Unknown output data type: %s",
-//                          TfLiteTypeGetName(op_context.dtype));
-//       return kTfLiteError;
-//   }
-
-//   TF_LITE_ENSURE(context, op_context.indices->type == kTfLiteInt32 ||
-//                               op_context.indices->type == kTfLiteInt64);
-//   TF_LITE_ENSURE(context, op_context.axis >= 0 &&
-//                               op_context.axis < op_context.output_dims);
-//   TF_LITE_ENSURE_EQ(context, NumElements(op_context.depth), 1);
-//   TF_LITE_ENSURE_EQ(context, NumElements(op_context.on_value), 1);
-//   TF_LITE_ENSURE_EQ(context, NumElements(op_context.off_value), 1);
-//   TF_LITE_ENSURE_TYPES_EQ(context, op_context.on_value->type,
-//   op_context.dtype); TF_LITE_ENSURE_TYPES_EQ(context,
-//   op_context.off_value->type,
-//                           op_context.dtype);
-
-//   if (!IsConstantOrPersistentTensor(op_context.depth)) {
-//     SetTensorToDynamic(op_context.output);
-//     return kTfLiteOk;
-//   }
-
-//   return ResizeOutputTensor(context, op_context);
-// }
-
-// TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
-//   OneHotContext op_context{context, node};
-
-//   if (IsDynamicTensor(op_context.output)) {
-//     TF_LITE_ENSURE_OK(context, ResizeOutputTensor(context, op_context));
-//   }
-
-//   switch (op_context.output->type) {
-//     case kTfLiteFloat32:
-//       OneHotCompute<float>(op_context);
-//       break;
-//     case kTfLiteInt32:
-//       OneHotCompute<int>(op_context);
-//       break;
-//     case kTfLiteInt64:
-//       OneHotCompute<int64_t>(op_context);
-//       break;
-//     case kTfLiteInt8:
-//       OneHotCompute<int8_t>(op_context);
-//       break;
-//     case kTfLiteUInt8:
-//       OneHotCompute<uint8_t>(op_context);
-//       break;
-//     case kTfLiteBool:
-//       OneHotCompute<bool>(op_context);
-//       break;
-//     default:
-//       return kTfLiteError;
-//   }
-
-//   return kTfLiteOk;
-// }
-
-// }  // namespace one_hot
-
-// TfLiteRegistration* Register_ONE_HOT() {
-//   static TfLiteRegistration r = {
-//       nullptr,
-//       nullptr,
-//       one_hot::Prepare,
-//       one_hot::Eval,
-//   };
-//   return &r;
-// }
-
-}  // namespace one_hot
-}  // namespace builtin
+}  // namespace micro
 }  // namespace ops
 }  // namespace tflite
