bbeb38f58530b245d559b3ff38140574a1730a94
Shlomi Regev <shlmregev@users.noreply.github.com>
2026-01-12 07:50:34 +0000

    Solve compiler errors in decode op (#3284)
    1. When building with old Xtensa toolchains, the compiler throw an unused variable warning
    which is treated as error (-Werror is defined by default).
    The cause is ScopedMicroProfile instantiation in the decoder op. Added a dummy reference to mute the warning.
    
    2. Old pre C++14 Xtensa compilers don't support ticks in preprocessor constants, e.g. 0x8000'0000
    Removed the ticks.
    
    BUG=451462435

--- tensorflow/lite/micro/kernels/decode_state_lut.cc
+++ tensorflow/lite/micro/kernels/decode_state_lut.cc
@@ -126,6 +126,7 @@ template int64_t* DecodeStateLut::DecompressToBuffer<int64_t>(void*);
 
 void DecodeStateLut::DecompressToBufferWidth4_16(int8_t* buffer) {
   ScopedMicroProfiler scoped_profiler(__func__, micro_profiler_);
+  (void)scoped_profiler;
 
   const size_t stride = value_table_channel_stride_;
   const uint8_t* value_table = static_cast<const uint8_t*>(value_table_);
@@ -202,6 +203,7 @@ void DecodeStateLut::DecompressToBufferWidth4_16(int8_t* buffer) {
 
 void DecodeStateLut::DecompressToBufferWidth2_16(int8_t* buffer) {
   ScopedMicroProfiler scoped_profiler(__func__, micro_profiler_);
+  (void)scoped_profiler;
 
   const size_t stride = value_table_channel_stride_;
   const uint8_t* value_table = static_cast<const uint8_t*>(value_table_);
@@ -278,6 +280,7 @@ void DecodeStateLut::DecompressToBufferWidth2_16(int8_t* buffer) {
 
 void DecodeStateLut::DecompressToBufferWidth3_32(int8_t* buffer) {
   ScopedMicroProfiler scoped_profiler(__func__, micro_profiler_);
+  (void)scoped_profiler;
 
   const size_t stride = value_table_channel_stride_;
   const uint8_t* value_table = static_cast<const uint8_t*>(value_table_);
@@ -411,6 +414,7 @@ void DecodeStateLut::DecompressToBufferWidth3_32(int8_t* buffer) {
 template <typename T>
 void DecodeStateLut::DecompressToBufferWidthAny(T* buffer) {
   ScopedMicroProfiler scoped_profiler(__func__, micro_profiler_);
+  (void)scoped_profiler;
 
   if (use_alternate_axis_) {
     const size_t stride = value_table_channel_stride_;
