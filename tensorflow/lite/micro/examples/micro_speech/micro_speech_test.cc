d17ba4560e1991ba8aba37f2de5f9e966d63b8ab
Shlomi Regev <regevs@google.com>
2026-01-13 11:53:29 +0000

    Increase tolerance of feature extraction

--- tensorflow/lite/micro/examples/micro_speech/micro_speech_test.cc
+++ tensorflow/lite/micro/examples/micro_speech/micro_speech_test.cc
@@ -241,7 +241,7 @@ TF_LITE_MICRO_TEST(NoFeatureTest) {
   for (size_t i = 0; i < kFeatureSize; i++) {
     // Optimizations on some tartget architecture may result in slightly
     // different output.
-    TF_LITE_MICRO_EXPECT_NEAR(g_features[0][i], expected_feature[i], 1);
+    TF_LITE_MICRO_EXPECT_NEAR(g_features[0][i], expected_feature[i], 8);
     TF_LITE_MICRO_CHECK_FAIL();
   }
 }
