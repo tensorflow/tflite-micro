66fae7cc6f96611213d984f809a6b744fb538b8d
Ryan Kuester <kuester@bdti.com>
2026-02-02 19:56:37 -0600

    refactor(compression): compressors inherit from Compressor protocol
    Explicit inheritance from Protocol enables static type checking at
    definition time and makes the interface self-documenting.
    
    BUG=implements #3256

--- tensorflow/lite/micro/compression/huffman.py
+++ tensorflow/lite/micro/compression/huffman.py
@@ -25,7 +25,7 @@ from tflite_micro.tensorflow.lite.micro.compression import model_editor
 from tflite_micro.tensorflow.lite.micro.compression import spec
 
 
-class HuffmanCompressor:
+class HuffmanCompressor(compressor.Compressor):
   """Huffman compression plugin (stub).
 
   This stub exists to validate the plugin architecture. The actual Huffman
