62ad0c002e36175ac058f399d68789657627b0a6
Esun Kim <veblush@google.com>
2026-08-13 15:57:33 -0700

    Updated by review

--- tensorflow/lite/micro/kernels/cast_test.cc
+++ tensorflow/lite/micro/kernels/cast_test.cc
@@ -13,9 +13,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include <initializer_list>
+#include <cstddef>
+#include <cstdint>
 #include <limits>
-#include <memory>
 
 #include "tensorflow/lite/c/builtin_op_data.h"
 #include "tensorflow/lite/c/common.h"
@@ -27,20 +27,17 @@ namespace tflite {
 namespace testing {
 namespace {
 
-template <typename InputT, typename OutputT>
-void TestCast(std::initializer_list<InputT> input,
-              std::initializer_list<OutputT> golden) {
-  auto input_data = std::make_unique<InputT[]>(input.size());
-  auto output_data = std::make_unique<OutputT[]>(golden.size());
-  std::copy(input.begin(), input.end(), input_data.get());
+template <typename InputT, typename OutputT, size_t N>
+void TestCast(const InputT (&input)[N], const OutputT (&golden)[N]) {
+  OutputT output_data[N];
 
-  int dims_data[] = {1, static_cast<int>(input.size())};
+  int dims_data[] = {1, static_cast<int>(N)};
   TfLiteIntArray* dims = IntArrayFromInts(dims_data);
 
   constexpr int tensors_size = 2;
   TfLiteTensor tensors[tensors_size] = {
-      CreateTensor(input_data.get(), dims),
-      CreateTensor(output_data.get(), dims),
+      CreateTensor(input, dims),
+      CreateTensor(output_data, dims),
   };
 
   int inputs_array_data[] = {1, 0};
@@ -55,24 +52,41 @@ void TestCast(std::initializer_list<InputT> input,
   EXPECT_EQ(kTfLiteOk, runner.InitAndPrepare());
   EXPECT_EQ(kTfLiteOk, runner.Invoke());
 
-  size_t i = 0;
-  for (OutputT val : golden) {
-    EXPECT_EQ(val, output_data[i++]);
+  for (size_t i = 0; i < N; ++i) {
+    EXPECT_EQ(golden[i], output_data[i]);
   }
 }
 
 template <typename IntT>
 void TestFloatToInt(float pos_overflow, float neg_overflow) {
   constexpr bool is_signed = std::numeric_limits<IntT>::is_signed;
-  TestCast<float, IntT>(
-      {100.f, 1.0f, 0.f, 0.4f, 1.999f, 1.1f, -1.0f, -100.f, pos_overflow,
-       neg_overflow, std::numeric_limits<float>::infinity(),
-       -std::numeric_limits<float>::infinity(),
-       std::numeric_limits<float>::quiet_NaN()},
-      {100, 1, 0, 0, 1, 1, is_signed ? static_cast<IntT>(-1) : IntT{0},
-       is_signed ? static_cast<IntT>(-100) : IntT{0},
-       std::numeric_limits<IntT>::max(), std::numeric_limits<IntT>::min(),
-       std::numeric_limits<IntT>::max(), std::numeric_limits<IntT>::min(), 0});
+  const float input[] = {100.f,
+                         1.0f,
+                         0.f,
+                         0.4f,
+                         1.999f,
+                         1.1f,
+                         -1.0f,
+                         -100.f,
+                         pos_overflow,
+                         neg_overflow,
+                         std::numeric_limits<float>::infinity(),
+                         -std::numeric_limits<float>::infinity(),
+                         std::numeric_limits<float>::quiet_NaN()};
+  const IntT golden[] = {100,
+                         1,
+                         0,
+                         0,
+                         1,
+                         1,
+                         is_signed ? static_cast<IntT>(-1) : IntT{0},
+                         is_signed ? static_cast<IntT>(-100) : IntT{0},
+                         std::numeric_limits<IntT>::max(),
+                         std::numeric_limits<IntT>::min(),
+                         std::numeric_limits<IntT>::max(),
+                         std::numeric_limits<IntT>::min(),
+                         0};
+  TestCast(input, golden);
 }
 
 }  // namespace
@@ -96,45 +110,56 @@ TEST(CastTest, CastFloatToUInt32) {
 }
 
 TEST(CastTest, CastFloatToBool) {
-  tflite::testing::TestCast<float, bool>(
-      {1.0f, 0.0f, -1.0f, 0.001f, std::numeric_limits<float>::infinity(),
-       std::numeric_limits<float>::quiet_NaN()},
-      {true, false, true, true, true, true});
+  const float input[] = {1.0f,
+                         0.0f,
+                         -1.0f,
+                         0.001f,
+                         std::numeric_limits<float>::infinity(),
+                         std::numeric_limits<float>::quiet_NaN()};
+  const bool golden[] = {true, false, true, true, true, true};
+  tflite::testing::TestCast(input, golden);
 }
 
 TEST(CastTest, CastInt8ToFloat) {
-  tflite::testing::TestCast<int8_t, float>({123, 0, 1, 2, 3, 4},
-                                           {123.f, 0.f, 1.f, 2.f, 3.f, 4.f});
+  const int8_t input[] = {123, 0, 1, 2, 3, 4};
+  const float golden[] = {123.f, 0.f, 1.f, 2.f, 3.f, 4.f};
+  tflite::testing::TestCast(input, golden);
 }
 
 TEST(CastTest, CastInt16ToFloat) {
-  tflite::testing::TestCast<int16_t, float>({123, 0, 1, 2, 3, 4},
-                                            {123.f, 0.f, 1.f, 2.f, 3.f, 4.f});
+  const int16_t input[] = {123, 0, 1, 2, 3, 4};
+  const float golden[] = {123.f, 0.f, 1.f, 2.f, 3.f, 4.f};
+  tflite::testing::TestCast(input, golden);
 }
 
 TEST(CastTest, CastInt16ToInt32) {
-  tflite::testing::TestCast<int16_t, int32_t>({123, 0, 1, 2, 3, 4},
-                                              {123, 0, 1, 2, 3, 4});
+  const int16_t input[] = {123, 0, 1, 2, 3, 4};
+  const int32_t golden[] = {123, 0, 1, 2, 3, 4};
+  tflite::testing::TestCast(input, golden);
 }
 
 TEST(CastTest, CastInt32ToInt16) {
-  tflite::testing::TestCast<int32_t, int16_t>({123, 0, 1, 2, 3, 4},
-                                              {123, 0, 1, 2, 3, 4});
+  const int32_t input[] = {123, 0, 1, 2, 3, 4};
+  const int16_t golden[] = {123, 0, 1, 2, 3, 4};
+  tflite::testing::TestCast(input, golden);
 }
 
 TEST(CastTest, CastUInt32ToInt32) {
-  tflite::testing::TestCast<uint32_t, int32_t>({100, 200, 300, 400, 500, 600},
-                                               {100, 200, 300, 400, 500, 600});
+  const uint32_t input[] = {100, 200, 300, 400, 500, 600};
+  const int32_t golden[] = {100, 200, 300, 400, 500, 600};
+  tflite::testing::TestCast(input, golden);
 }
 
 TEST(CastTest, CastInt32ToUInt32) {
-  tflite::testing::TestCast<int32_t, uint32_t>({100, 200, 300, 400, 500, 600},
-                                               {100, 200, 300, 400, 500, 600});
+  const int32_t input[] = {100, 200, 300, 400, 500, 600};
+  const uint32_t golden[] = {100, 200, 300, 400, 500, 600};
+  tflite::testing::TestCast(input, golden);
 }
 
 TEST(CastTest, CastBoolToFloat) {
-  tflite::testing::TestCast<bool, float>({true, true, false, true, false, true},
-                                         {1.f, 1.0f, 0.f, 1.0f, 0.0f, 1.0f});
+  const bool input[] = {true, true, false, true, false, true};
+  const float golden[] = {1.f, 1.0f, 0.f, 1.0f, 0.0f, 1.0f};
+  tflite::testing::TestCast(input, golden);
 }
 
 TF_LITE_MICRO_TESTS_MAIN
