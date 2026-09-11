a80982869243cb4a145953506f0c27de17ef7ce8
Esun Kim <veblush@google.com>
2026-09-11 14:59:30 -0700

    Revert changes to tensorflow/lite/{tools,python} files synced from upstream

--- tensorflow/lite/python/schema_util.py
+++ tensorflow/lite/python/schema_util.py
@@ -14,9 +14,7 @@
 # ==============================================================================
 """Schema utilities to get builtin code from operator code."""
 
-__all__ = [
-    'get_builtin_code_from_operator_code',
-]
+from tensorflow.python.util import all_util
 
 
 def get_builtin_code_from_operator_code(opcode):
@@ -38,3 +36,10 @@ def get_builtin_code_from_operator_code(opcode):
     return max(opcode.BuiltinCode(), opcode.DeprecatedBuiltinCode())
 
   return max(opcode.builtinCode, opcode.deprecatedBuiltinCode)
+
+
+_allowed_symbols = [
+    'get_builtin_code_from_operator_code',
+]
+
+all_util.remove_undocumented(__name__, _allowed_symbols)
