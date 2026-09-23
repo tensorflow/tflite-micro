2118ae70fb51d3cef78831b11600e4a5923276db
Esun Kim <veblush@google.com>
2026-09-23 14:56:48 -0700

    Replace tensorflow in generate_test_models with static fixture

--- tensorflow/lite/micro/testing/generate_test_models.py
+++ tensorflow/lite/micro/testing/generate_test_models.py
@@ -12,75 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Python utility script to generate unit test model data."""
+"""Python utility script to provide unit test model data."""
 
-# Steps to regenerate model test data:
-# TODO(b/158011574): Do these steps in the script here instead of manually.
-# 1.) Run this script
-# 2.) Hexdump the model into a .h/.cc file:
-#       xxd -i /tmp/tf_micro_conv_test_model.tflite > /tmp/temp.cc
-# 3.) Copy/replace contents of temp.cc into desired header/source files (e.g.
-#     test_conv_model.h/.cc
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
+import os
 import sys
-import numpy as np
-import tensorflow as tf
 
 
 def generate_conv_model(
-  write_to_file=True, filename="/tmp/tf_micro_conv_test_model.int8.tflite"
+    write_to_file=True, filename="/tmp/tf_micro_conv_test_model.int8.tflite"
 ):
-  """Creates a basic Keras model and converts to tflite.
-
-  This model does not make any relevant classifications. It only exists to
-  generate a model that is designed to run on embedded devices.
-  """
-  np.random.seed(0)
-  input_shape = (16, 16, 1)
-
-  model = tf.keras.models.Sequential()
-  model.add(
-    tf.keras.layers.Conv2D(16, 3, activation="relu", input_shape=input_shape)
+  """Loads the pregenerated conv int8 model and optionally writes it to file."""
+  model_path = os.path.join(
+      os.path.dirname(__file__), "conv_test_model.tflite"
   )
-  model.add(tf.keras.layers.Conv2D(32, 3, activation="relu"))
-  model.add(tf.keras.layers.MaxPooling2D(2))
-  model.add(tf.keras.layers.Flatten())
-  model.add(tf.keras.layers.Dense(10))
-  model.compile(
-    optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
-  )
-  model.summary()
+  with open(model_path, "rb") as f:
+    tflite_model = f.read()
 
-  # Test with random data
-  data_x = np.random.rand(12, 16, 16, 1)
-  data_y = np.random.randint(2, size=(12, 10))
-  model.fit(data_x, data_y, epochs=5)
-
-  def representative_dataset_gen():
-    np.random.seed(0)
-    for _ in range(12):
-      yield [np.random.rand(16, 16).reshape(1, 16, 16, 1).astype(np.float32)]
-
-  # Now convert to a TFLite model with full int8 quantization:
-  converter = tf.lite.TFLiteConverter.from_keras_model(model)
-  converter.optimizations = [tf.lite.Optimize.DEFAULT]
-  converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-  converter.inference_input_type = tf.int8
-  converter.inference_output_type = tf.int8
-  converter.representative_dataset = representative_dataset_gen
-  # TODO(b/324385802): Disable per channel quantization in FC layers (currently
-  # default behaviour) since it's not yet supported in TFLM.
-  converter._experimental_disable_per_channel_quantization_for_dense_layers = (  # pylint: disable=protected-access
-    True
-  )
-
-  tflite_model = converter.convert()
   if write_to_file:
-    open(filename, "wb").write(tflite_model)
+    with open(filename, "wb") as f:
+      f.write(tflite_model)
 
   return tflite_model
 
